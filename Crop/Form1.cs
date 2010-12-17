50b68eef572b2513141f0ec3d39671df87a0aa90
John Hatton <hattonjohn@gmail.com>
2010-12-17 17:05:28 +1000

    Work on a cropping control

---
+++ Crop/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Crop
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void imageCropper1_Load(object sender, EventArgs e)
+		{
+			imageCropper1.Image = Image.FromFile(@"C:\Art of Reading\Images\Brazil\B-NA-6.tif");
+		}
+	}
+}
