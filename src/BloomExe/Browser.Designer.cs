c1d0db5505a1e6a90af449e3aba2834d4821c234
John Hatton <hattonjohn@gmail.com>
2011-02-22 16:26:14 +1000

    Add splash screen and delayed collection display.

--- src/BloomExe/Browser.Designer.cs
+++ src/BloomExe/Browser.Designer.cs
@@ -17,10 +17,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Browser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Browser";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
