5e87722b527eb2f4fab848665a453f8de5e5d24e
John Hatton <hattonjohn@gmail.com>
2011-02-22 14:16:45 +1000

    Installer stuff

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,6 +1,6 @@
 call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 pushd .
-MSbuild /target:installer /property:teamcity_build_checkoutDir=..\ /verbosity:detailed /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.6.789" /property:Minor="1"
+MSbuild /target:installer /property:teamcity_build_checkoutDir=..\ /verbosity:detailed /property:XulRunnerZipLocation="c:\dev" /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.6.789" /property:Minor="1"
 popd
 PAUSE
