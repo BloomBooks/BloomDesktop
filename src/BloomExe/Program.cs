d123bd7400b999cfa8fbb44f399d1cd6c78a2c24
John Hatton <hattonjohn@gmail.com>
2011-04-02 15:47:20 +1000

    remove duplicate settings creation.

--- src/BloomExe/Program.cs
+++ src/BloomExe/Program.cs
@@ -25,10 +25,7 @@ namespace Bloom
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
-			if(Settings.Default.Reporting == null)
-			{
-				Settings.Default.Reporting = new ReportingSettings();
-			}
+
 			//bring in settings from any previous version
 			if (Settings.Default.NeedUpgrade)
 			{
