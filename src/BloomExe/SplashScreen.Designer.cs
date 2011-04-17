462523a74dbb18e7f01e1a068821b4fe2cd533f7
John Hatton <hattonjohn@gmail.com>
2011-04-17 19:42:05 +1000

    update splash screen

--- src/BloomExe/SplashScreen.Designer.cs
+++ src/BloomExe/SplashScreen.Designer.cs
@@ -48,13 +48,13 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 134);
-            this.label1.MaximumSize = new System.Drawing.Size(300, 0);
+            this.label1.Location = new System.Drawing.Point(12, 102);
+            this.label1.MaximumSize = new System.Drawing.Size(400, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(299, 96);
+            this.label1.Size = new System.Drawing.Size(386, 168);
             this.label1.TabIndex = 1;
-            this.label1.Text = "This is a development version. Not only should you not use it, to do work, it\'s n" +
-                "ot worth the effort to report bugs.";
+            this.label1.Text = "This is an experimental version of Bloom.\r\n\r\nPlease click the ? icon and read the" +
+                " Release Notes.\r\n\r\nThanks for reporting any positive or negative results.";
             // 
             // _fadeOutTimer
             // 
@@ -65,7 +65,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(548, 251);
+            this.ClientSize = new System.Drawing.Size(548, 309);
             this.ControlBox = false;
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
