c66d1d8c9b1f6ed5c969f3e0c98c08dfd73599f7
John Hatton <hattonjohn@gmail.com>
2011-11-22 08:31:50 -0700

    TeamCity Build work

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,6 +1,6 @@
 call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 pushd .
-MSbuild /target:installer /property:teamcity_build_checkoutDir=..\ /verbosity:detailed  /property:BigFilesLocation="C:\dev\teamcitybuilddownloads" /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.36.999" /property:Minor="1"
+MSbuild /target:installer /property:teamcity_build_checkoutDir=..\ /verbosity:detailed  /property:LargeFilesDir="C:\dev\teamcitybuilddownloads" /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.36.999" /property:Minor="1"
 popd
 PAUSE
