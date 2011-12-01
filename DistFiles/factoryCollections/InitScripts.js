e8de9c7916e121cad3a0bbb11ad3c3dcc57291b7
John Hatton <hattonjohn@gmail.com>
2011-12-01 08:48:48 -0700

    Move Blur handling into InitScripts.js

--- DistFiles/factoryCollections/InitScripts.js
+++ DistFiles/factoryCollections/InitScripts.js
@@ -1,9 +1,13 @@
 //Run any needed functions here:
 jQuery(document).ready(function () {
-  //Apply overflow handling to all textareas
-  jQuery("textarea").each(function(){ jQuery(this).SignalOverflow() } );
 
-  //Other needed functions:
-  //...
-  //...
+
+    //Apply overflow handling to all textareas
+    jQuery("textarea").each(function () { jQuery(this).SignalOverflow() });
+
+    //make textarea edits go back into the dom (they were designed to be POST'ed via forms)
+    jQuery("textarea").blur(function () { this.innerHTML = this.value; });
+    //Other needed functions:
+    //...
+    //...
 });
