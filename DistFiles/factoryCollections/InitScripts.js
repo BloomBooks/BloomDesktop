30d735c6b9c4b7b2a096b1210b9c3d14d3e7ad66
John Hatton <hattonjohn@gmail.com>
2011-12-08 13:13:12 -0700

    Remove need for ids from <p> elements.

--- DistFiles/factoryCollections/InitScripts.js
+++ DistFiles/factoryCollections/InitScripts.js
@@ -1,13 +1,23 @@
 //Run any needed functions here:
 jQuery(document).ready(function () {
-
-
     //Apply overflow handling to all textareas
-    jQuery("textarea").each(function () { jQuery(this).SignalOverflow() });
+//    jQuery("textarea").each(function () { jQuery(this).SignalOverflow() });
 
     //make textarea edits go back into the dom (they were designed to be POST'ed via forms)
     jQuery("textarea").blur(function () { this.innerHTML = this.value; });
-    //Other needed functions:
-    //...
-    //...
+
+    //when a textarea gets focus, send Bloom a dictionary of all the translations found within
+    //the same parent element
+    jQuery("textarea").focus( function() {
+        event = document.createEvent('MessageEvent');
+        var origin = window.location.protocol + '//' + window.location.host;
+        var obj = {};
+        $(this).parent().find("textarea").each( function(){
+                obj[$(this).attr("lang")] = $(this).text();
+            })
+            var json = obj;//.get();
+            json = JSON.stringify(json);
+            event.initMessageEvent ('textGroupFocussed', true, true, json, origin, 1234, window, null);
+            document.dispatchEvent (event);
+        });
 });
