93711f2b25904e52614fadc71a87ad81ab8bc076
John Hatton <hattonjohn@gmail.com>
2012-04-20 14:27:01 -0600

    Tweak trilingual layout for basicBook-imageOnTop

--- src/BloomExe/Edit/PageListView.Designer.cs
+++ src/BloomExe/Edit/PageListView.Designer.cs
@@ -32,10 +32,7 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageListView));
 			this._pageThumbnails = new System.Windows.Forms.ImageList(this.components);
 			this.label1 = new System.Windows.Forms.Label();
-			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.deletePageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this._thumbNailList = new Bloom.Edit.ThumbNailList();
-			this.contextMenuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// _pageThumbnails
@@ -49,20 +46,12 @@
 			this.label1.AutoSize = true;
 			this.label1.BackColor = System.Drawing.Color.Transparent;
 			this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.ForeColor = Palette.TextAgainstDarkBackground;
+			this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
 			this.label1.Location = new System.Drawing.Point(18, 3);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(50, 20);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Pages";
-			// 
-			// contextMenuStrip1
-			// 
-			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.deletePageToolStripMenuItem});
-			this.contextMenuStrip1.Name = "contextMenuStrip1";
-			this.contextMenuStrip1.Size = new System.Drawing.Size(137, 26);
-			this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
 			// 
 			// _thumbNailList
 			// 
@@ -70,7 +59,6 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this._thumbNailList.BackColor = System.Drawing.SystemColors.Control;
-			this._thumbNailList.ContextMenuStrip = this.contextMenuStrip1;
 			this._thumbNailList.Font = new System.Drawing.Font("Tahoma", 9F);
 			this._thumbNailList.ForeColor = System.Drawing.SystemColors.WindowText;
 			this._thumbNailList.ItemWhichWouldPrecedeANewPageInsertion = null;
@@ -92,7 +80,6 @@
 			this.Name = "PageListView";
 			this.Size = new System.Drawing.Size(116, 216);
 			this.BackColorChanged += new System.EventHandler(this.PageListView_BackColorChanged);
-			this.contextMenuStrip1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -102,8 +89,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ImageList _pageThumbnails;
-        private Bloom.Edit.ThumbNailList _thumbNailList;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem deletePageToolStripMenuItem;
+		private Bloom.Edit.ThumbNailList _thumbNailList;
     }
 }
