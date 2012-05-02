0309e76e03baab73bc32e5d05f6a1767567da99d
John Hatton <hattonjohn@gmail.com>
2012-05-02 09:02:27 -0600

    Help wiring

--- src/BloomExe/Library/SettingsDialog.Designer.cs
+++ src/BloomExe/Library/SettingsDialog.Designer.cs
@@ -31,17 +31,19 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsDialog));
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this._aboutLanguageSettingsButton = new System.Windows.Forms.Button();
 			this._removeSecondNationalLanguageButton = new System.Windows.Forms.LinkLabel();
 			this._national2ChangeLink = new System.Windows.Forms.LinkLabel();
 			this._national1ChangeLink = new System.Windows.Forms.LinkLabel();
 			this._vernacularChangeLink = new System.Windows.Forms.LinkLabel();
 			this._nationalLanguage2Label = new System.Windows.Forms.Label();
-			this.label6 = new System.Windows.Forms.Label();
+			this._language3Label = new System.Windows.Forms.Label();
 			this._nationalLanguage1Label = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
+			this._language2Label = new System.Windows.Forms.Label();
 			this._vernacularLanguageName = new System.Windows.Forms.Label();
-			this._vernacularOrShellLanguageLabel = new System.Windows.Forms.Label();
+			this._language1Label = new System.Windows.Forms.Label();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this._aboutBookMakingSettingsButton = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
 			this._xmatterPackCombo = new System.Windows.Forms.ComboBox();
 			this.tabPage3 = new System.Windows.Forms.TabPage();
@@ -54,8 +56,7 @@
 			this._okButton = new System.Windows.Forms.Button();
 			this._restartMessage = new System.Windows.Forms.Label();
 			this.settingsProtectionLauncherButton1 = new Palaso.UI.WindowsForms.SettingProtection.SettingsProtectionLauncherButton();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
+			this._aboutProjectInformationSetingsButton = new System.Windows.Forms.Button();
 			this.tabControl1.SuspendLayout();
 			this.tabPage1.SuspendLayout();
 			this.tabPage2.SuspendLayout();
@@ -79,17 +80,17 @@
 			// 
 			// tabPage1
 			// 
-			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this._aboutLanguageSettingsButton);
 			this.tabPage1.Controls.Add(this._removeSecondNationalLanguageButton);
 			this.tabPage1.Controls.Add(this._national2ChangeLink);
 			this.tabPage1.Controls.Add(this._national1ChangeLink);
 			this.tabPage1.Controls.Add(this._vernacularChangeLink);
 			this.tabPage1.Controls.Add(this._nationalLanguage2Label);
-			this.tabPage1.Controls.Add(this.label6);
+			this.tabPage1.Controls.Add(this._language3Label);
 			this.tabPage1.Controls.Add(this._nationalLanguage1Label);
-			this.tabPage1.Controls.Add(this.label4);
+			this.tabPage1.Controls.Add(this._language2Label);
 			this.tabPage1.Controls.Add(this._vernacularLanguageName);
-			this.tabPage1.Controls.Add(this._vernacularOrShellLanguageLabel);
+			this.tabPage1.Controls.Add(this._language1Label);
 			this.tabPage1.Location = new System.Drawing.Point(4, 26);
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -97,6 +98,22 @@
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "Languages";
 			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// _aboutLanguageSettingsButton
+			// 
+			this._aboutLanguageSettingsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this._aboutLanguageSettingsButton.FlatAppearance.BorderSize = 0;
+			this._aboutLanguageSettingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this._aboutLanguageSettingsButton.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._aboutLanguageSettingsButton.Image = global::Bloom.Properties.Resources.help24x24;
+			this._aboutLanguageSettingsButton.Location = new System.Drawing.Point(478, 24);
+			this._aboutLanguageSettingsButton.Name = "_aboutLanguageSettingsButton";
+			this._aboutLanguageSettingsButton.Size = new System.Drawing.Size(113, 73);
+			this._aboutLanguageSettingsButton.TabIndex = 19;
+			this._aboutLanguageSettingsButton.Text = "About These Settings";
+			this._aboutLanguageSettingsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+			this._aboutLanguageSettingsButton.UseVisualStyleBackColor = true;
+			this._aboutLanguageSettingsButton.Click += new System.EventHandler(this.OnAboutLanguageSettings);
 			// 
 			// _removeSecondNationalLanguageButton
 			// 
@@ -152,15 +169,15 @@
 			this._nationalLanguage2Label.TabIndex = 14;
 			this._nationalLanguage2Label.Text = "foobar";
 			// 
-			// label6
+			// _language3Label
 			// 
-			this.label6.AutoSize = true;
-			this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label6.Location = new System.Drawing.Point(26, 198);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(308, 19);
-			this.label6.TabIndex = 13;
-			this.label6.Text = "Language 2 (Optional) (e.g. Regional Language)";
+			this._language3Label.AutoSize = true;
+			this._language3Label.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._language3Label.Location = new System.Drawing.Point(26, 198);
+			this._language3Label.Name = "_language3Label";
+			this._language3Label.Size = new System.Drawing.Size(316, 19);
+			this._language3Label.TabIndex = 13;
+			this._language3Label.Text = "Language 3 (e.g. Regional Language)   (Optional)";
 			// 
 			// _nationalLanguage1Label
 			// 
@@ -172,16 +189,16 @@
 			this._nationalLanguage1Label.TabIndex = 11;
 			this._nationalLanguage1Label.Text = "foobar";
 			// 
-			// label4
+			// _language2Label
 			// 
-			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(26, 113);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(227, 19);
-			this.label4.TabIndex = 10;
-			this.label4.Text = "Language 1 (e.g. Nation Language)";
-			this.label4.Click += new System.EventHandler(this.label4_Click);
+			this._language2Label.AutoSize = true;
+			this._language2Label.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._language2Label.Location = new System.Drawing.Point(26, 113);
+			this._language2Label.Name = "_language2Label";
+			this._language2Label.Size = new System.Drawing.Size(238, 19);
+			this._language2Label.TabIndex = 10;
+			this._language2Label.Text = "Language 2 (e.g. National Language)";
+			this._language2Label.Click += new System.EventHandler(this.label4_Click);
 			// 
 			// _vernacularLanguageName
 			// 
@@ -193,19 +210,19 @@
 			this._vernacularLanguageName.TabIndex = 8;
 			this._vernacularLanguageName.Text = "foobar";
 			// 
-			// _vernacularOrShellLanguageLabel
+			// _language1Label
 			// 
-			this._vernacularOrShellLanguageLabel.AutoSize = true;
-			this._vernacularOrShellLanguageLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this._vernacularOrShellLanguageLabel.Location = new System.Drawing.Point(26, 24);
-			this._vernacularOrShellLanguageLabel.Name = "_vernacularOrShellLanguageLabel";
-			this._vernacularOrShellLanguageLabel.Size = new System.Drawing.Size(140, 19);
-			this._vernacularOrShellLanguageLabel.TabIndex = 7;
-			this._vernacularOrShellLanguageLabel.Text = "Vernacular Language";
+			this._language1Label.AutoSize = true;
+			this._language1Label.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._language1Label.Location = new System.Drawing.Point(26, 24);
+			this._language1Label.Name = "_language1Label";
+			this._language1Label.Size = new System.Drawing.Size(140, 19);
+			this._language1Label.TabIndex = 7;
+			this._language1Label.Text = "Vernacular Language";
 			// 
 			// tabPage2
 			// 
-			this.tabPage2.Controls.Add(this.button2);
+			this.tabPage2.Controls.Add(this._aboutBookMakingSettingsButton);
 			this.tabPage2.Controls.Add(this.label1);
 			this.tabPage2.Controls.Add(this._xmatterPackCombo);
 			this.tabPage2.Location = new System.Drawing.Point(4, 26);
@@ -215,6 +232,22 @@
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "Book Making";
 			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// _aboutBookMakingSettingsButton
+			// 
+			this._aboutBookMakingSettingsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this._aboutBookMakingSettingsButton.FlatAppearance.BorderSize = 0;
+			this._aboutBookMakingSettingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this._aboutBookMakingSettingsButton.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._aboutBookMakingSettingsButton.Image = global::Bloom.Properties.Resources.help24x24;
+			this._aboutBookMakingSettingsButton.Location = new System.Drawing.Point(468, 30);
+			this._aboutBookMakingSettingsButton.Name = "_aboutBookMakingSettingsButton";
+			this._aboutBookMakingSettingsButton.Size = new System.Drawing.Size(113, 73);
+			this._aboutBookMakingSettingsButton.TabIndex = 20;
+			this._aboutBookMakingSettingsButton.Text = "About These Settings";
+			this._aboutBookMakingSettingsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+			this._aboutBookMakingSettingsButton.UseVisualStyleBackColor = true;
+			this._aboutBookMakingSettingsButton.Click += new System.EventHandler(this.OnAboutBookMakingSettings);
 			// 
 			// label1
 			// 
@@ -237,6 +270,7 @@
 			// 
 			// tabPage3
 			// 
+			this.tabPage3.Controls.Add(this._aboutProjectInformationSetingsButton);
 			this.tabPage3.Controls.Add(this._districtText);
 			this.tabPage3.Controls.Add(this._provinceText);
 			this.tabPage3.Controls.Add(this._countryText);
@@ -335,33 +369,21 @@
 			this.settingsProtectionLauncherButton1.Size = new System.Drawing.Size(257, 37);
 			this.settingsProtectionLauncherButton1.TabIndex = 20;
 			// 
-			// button1
+			// _aboutProjectInformationSetingsButton
 			// 
-			this.button1.FlatAppearance.BorderSize = 0;
-			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Image = global::Bloom.Properties.Resources.help24x24;
-			this.button1.Location = new System.Drawing.Point(478, 24);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(113, 73);
-			this.button1.TabIndex = 19;
-			this.button1.Text = "Help With Language Settings";
-			this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this.button2.FlatAppearance.BorderSize = 0;
-			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button2.Image = global::Bloom.Properties.Resources.help24x24;
-			this.button2.Location = new System.Drawing.Point(468, 30);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(113, 73);
-			this.button2.TabIndex = 20;
-			this.button2.Text = "Help With Book Making Settings";
-			this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-			this.button2.UseVisualStyleBackColor = true;
+			this._aboutProjectInformationSetingsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this._aboutProjectInformationSetingsButton.FlatAppearance.BorderSize = 0;
+			this._aboutProjectInformationSetingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this._aboutProjectInformationSetingsButton.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._aboutProjectInformationSetingsButton.Image = global::Bloom.Properties.Resources.help24x24;
+			this._aboutProjectInformationSetingsButton.Location = new System.Drawing.Point(479, 21);
+			this._aboutProjectInformationSetingsButton.Name = "_aboutProjectInformationSetingsButton";
+			this._aboutProjectInformationSetingsButton.Size = new System.Drawing.Size(113, 88);
+			this._aboutProjectInformationSetingsButton.TabIndex = 20;
+			this._aboutProjectInformationSetingsButton.Text = "About These Settings";
+			this._aboutProjectInformationSetingsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+			this._aboutProjectInformationSetingsButton.UseVisualStyleBackColor = true;
+			this._aboutProjectInformationSetingsButton.Click += new System.EventHandler(this.OnAboutProjectInformationSetingsButton_Click);
 			// 
 			// SettingsDialog
 			// 
@@ -397,14 +419,14 @@
 		private System.Windows.Forms.TabPage tabPage2;
 		private System.Windows.Forms.TabPage tabPage3;
 		private System.Windows.Forms.Button _okButton;
-		protected System.Windows.Forms.Label _vernacularOrShellLanguageLabel;
+		protected System.Windows.Forms.Label _language1Label;
 		private System.Windows.Forms.LinkLabel _national2ChangeLink;
 		private System.Windows.Forms.LinkLabel _national1ChangeLink;
 		private System.Windows.Forms.LinkLabel _vernacularChangeLink;
 		protected System.Windows.Forms.Label _nationalLanguage2Label;
-		protected System.Windows.Forms.Label label6;
+		protected System.Windows.Forms.Label _language3Label;
 		protected System.Windows.Forms.Label _nationalLanguage1Label;
-		protected System.Windows.Forms.Label label4;
+		protected System.Windows.Forms.Label _language2Label;
 		protected System.Windows.Forms.Label _vernacularLanguageName;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.ComboBox _xmatterPackCombo;
@@ -417,7 +439,8 @@
 		private System.Windows.Forms.LinkLabel _removeSecondNationalLanguageButton;
 		private System.Windows.Forms.Label _restartMessage;
 		private Palaso.UI.WindowsForms.SettingProtection.SettingsProtectionLauncherButton settingsProtectionLauncherButton1;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button _aboutLanguageSettingsButton;
+		private System.Windows.Forms.Button _aboutBookMakingSettingsButton;
+		private System.Windows.Forms.Button _aboutProjectInformationSetingsButton;
 	}
 }
