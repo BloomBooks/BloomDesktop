440242a1c41d74d60224136584c37db654004a28
John Hatton <hattonjohn@gmail.com>
2012-07-26 17:26:43 +1000

    Fix ImageServer hanlding of license.pg url (which had a dummy query to avoid getting a stale img)

--- src/BloomExe/ImageProcessing/ImageServer.cs
+++ src/BloomExe/ImageProcessing/ImageServer.cs
@@ -158,13 +158,14 @@ namespace Bloom.ImageProcessing
 		/// </summary>
 		/// <param name="info"></param>
 		public void MakeReply(IRequestInfo info)
-		{
-			if(info.RawUrl.EndsWith("testconnection"))
+		{	
+			if(info.LocalPathWithoutQuery.EndsWith("testconnection"))
 			{
 				info.WriteCompleteOutput("OK");
 				return;
 			}
-			var r = info.RawUrl.Replace("/bloom/", "");
+
+			var r = info.LocalPathWithoutQuery.Replace("/bloom/", "");
 			r = r.Replace("%3A", ":");
 			r = r.Replace("%20", " ");
 			r = r.Replace("%27", "'");
