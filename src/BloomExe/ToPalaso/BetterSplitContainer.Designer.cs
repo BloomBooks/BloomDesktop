5c425e44876052fa113e182d745d766bd2fdfa44
John Hatton <hattonjohn@gmail.com>
2012-09-06 10:58:59 +1000

    Some work on splitters

---
+++ src/BloomExe/ToPalaso/BetterSplitContainer.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Bloom.ToPalaso
+{
+	partial class BetterSplitContainer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// BetterSplitContainer
+			// 
+			this.TabStop = false;
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+			this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnMouseUp);
+			((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
