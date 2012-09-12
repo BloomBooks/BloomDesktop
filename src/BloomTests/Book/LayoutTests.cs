31fe7b38f3b379ae959255493c18ba678dd89cb5
John Hatton <hattonjohn@gmail.com>
2012-09-13 09:57:22 +1000

    Backed up & incrementall added all recent changes becuase of an unexplained slowdown. The slowdown didn't come back. Arrgggh.

---
+++ src/BloomTests/Book/LayoutTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Xml;
+using Bloom.Book;
+using NUnit.Framework;
+using Palaso.TestUtilities;
+
+namespace BloomTests.Book
+{
+	[TestFixture]
+	public class LayoutTests
+	{
+		[Test]
+		public void UpdatePageSplitMode_WasCombinedAndShouldStayThatWay_PageUntouched()
+		{
+			var dom = new XmlDocument();
+			dom.LoadXml(@"<html ><body><div id='foo'></div><div class='bloom-page A5Landscape bloom-combinedPage'></div></body></html>");
+			var layout = new Layout() { ElementDistribution = Layout.ElementDistributionChoices.CombinedPages };
+			layout.UpdatePageSplitMode(dom);
+
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-page')]", 1);
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-combinedPage')]", 1);
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-leadingPage')]", 0);
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-trailingPage')]", 0);
+		}
+
+		[Test]
+		public void UpdatePageSplitMode_WasCombined_IsNowSplitIntoTwoPages()
+		{
+			var dom = new XmlDocument();
+			dom.LoadXml(@"<html ><body><div id='foo'></div><div class='bloom-page A5Landscape bloom-combinedPage'></div></body></html>");
+			var layout = new Layout() {ElementDistribution = Layout.ElementDistributionChoices.SplitAcrossPages};
+			layout.UpdatePageSplitMode(dom);
+
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-page')]", 2);
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-combinedPage')]", 0);
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-leadingPage')]", 1);
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-trailingPage')]", 1);
+		}
+
+		[Test]
+		public void UpdatePageSplitMode_WasCombined_IndividualPagesHaveOwnIds()
+		{
+			var dom = new XmlDocument();
+			dom.LoadXml(@"<html ><body><div id='foo'></div><div id='1' class='bloom-page A5Landscape bloom-combinedPage'></div></body></html>");
+			var layout = new Layout() { ElementDistribution = Layout.ElementDistributionChoices.SplitAcrossPages };
+			layout.UpdatePageSplitMode(dom);
+
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[@id=1]", 0);
+		}
+
+
+		[Test]
+		public void UpdatePageSplitMode_WasCombined_ElementNowDividedBetweenTwoPages()
+		{
+			var dom = new XmlDocument();
+			dom.LoadXml(@"<html ><body><div id='foo'></div><div class='bloom-page A5Landscape bloom-combinedPage'>
+					<div class='bloom-leadingElement'>top1</div>
+					<div class='bloom-trailingElement'>bottom1</div>
+					<div class='bloom-leadingElement'>top2</div>
+					<div class='bloom-trailingElement'>bottom2</div>
+				</div></body></html>");
+			var layout = new Layout() { ElementDistribution = Layout.ElementDistributionChoices.SplitAcrossPages };
+			layout.UpdatePageSplitMode(dom);
+
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-leadingPage')]//div", 2);
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-trailingPage')]//div", 2);
+
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-leadingPage')]//div[contains(@class,'bloom-leadingElement')]", 2);
+			AssertThatXmlIn.Dom(dom).HasSpecifiedNumberOfMatchesForXpath("//div[contains(@class,'bloom-trailingPage')]//div[contains(@class,'bloom-trailingElement')]", 2);
+		}
+	}
+}
