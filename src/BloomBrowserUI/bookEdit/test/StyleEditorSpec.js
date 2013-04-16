6f25029b8afd153c15ced62d2f36fd6373a9ca19
John Hatton <hattonjohn@gmail.com>
2013-04-16 09:16:11 -0700

    Got first Typscript-Jasmine tests running with new organization

--- src/BloomBrowserUI/bookEdit/test/StyleEditorSpec.js
+++ src/BloomBrowserUI/bookEdit/test/StyleEditorSpec.js
@@ -1,6 +1,12 @@
+/// <reference path="../js/StyleEditor.ts" />
+/// <reference path="../../lib/jquery.d.ts" />
+/// <reference path="../../lib/jasmine/jasmine.d.ts"/>
+/*
+/// <reference path="../../lib/jquery-1.9.1.js"/>
+*/
 function GetStylesAfterMakeBigger() {
     var target = $(document).find('.fooStyle');
-    var editor = new Bloom.StyleEditor(document);
+    var editor = new StyleEditor(document);
     editor.MakeBigger(target);
     return $(document).find('.styleEditorStuff').html();
 }
@@ -8,11 +14,11 @@ describe("StyleEditor", function () {
     it("constructor does not make a documentStyles style if one already exists", function () {
         var s = $('<style id="documentStyles" class="styleEditorStuff" type="text/css"></style>');
         $(document).find("head").append(s);
-        var editor = new Bloom.StyleEditor(document);
+        var editor = new StyleEditor(document);
         expect($(document).find('.styleEditorStuff').length).toEqual(1);
     });
     it("constructor adds a style with id documentStyles", function () {
-        var editor = new Bloom.StyleEditor(document);
+        var editor = new StyleEditor(document);
         expect($(document).find('.styleEditorStuff').length).toEqual(1);
     });
     it("MakeBigger creates a style for the correct class if it is missing", function () {
--- src/BloomBrowserUI/bookEdit/test/StyleEditorSpec.ts
+++ src/BloomBrowserUI/bookEdit/test/StyleEditorSpec.ts
@@ -1,27 +1,35 @@
+/// <reference path="../js/StyleEditor.ts" />
+/// <reference path="../../lib/jquery.d.ts" />
+/// <reference path="../../lib/jasmine/jasmine.d.ts"/>
+
+/*
+/// <reference path="../../lib/jquery-1.9.1.js"/>
+*/
+
 function GetStylesAfterMakeBigger() {
 	var target = $(document).find('.fooStyle');
-	var editor = new Bloom.StyleEditor(document);
-	editor.MakeBigger(target);
+	var editor = new StyleEditor(<HTMLElement><any>document);
+	editor.MakeBigger(<HTMLElement><any>target);
 	return $(document).find('.styleEditorStuff').html();
 }
 
-describe("StyleEditor", function() {
-	it("constructor does not make a documentStyles style if one already exists", function() {
-		var s=$('<style id="documentStyles" class="styleEditorStuff" type="text/css"></style>')
+describe("StyleEditor", function () {
+	it("constructor does not make a documentStyles style if one already exists", function () {
+		var s = $('<style id="documentStyles" class="styleEditorStuff" type="text/css"></style>');
 		$(document).find("head").append(s);
-		var editor = new Bloom.StyleEditor(document);
+		var editor = new StyleEditor(<HTMLElement><any>document);
 		expect($(document).find('.styleEditorStuff').length).toEqual(1);
 	});
-	it("constructor adds a style with id documentStyles", function() {
-		var editor = new Bloom.StyleEditor(document);
+	it("constructor adds a style with id documentStyles", function () {
+		var editor = new StyleEditor(<HTMLElement><any>document);
 		expect($(document).find('.styleEditorStuff').length).toEqual(1);
 	});
-	it("MakeBigger creates a style for the correct class if it is missing", function() {
+	it("MakeBigger creates a style for the correct class if it is missing", function () {
 		$('body').append("<div class='ignore fooStyle ignoreMeToo '></div>");
 		expect(GetStylesAfterMakeBigger()).toContain(<Object>"fooStyle");
 	});
-	it("MakeBigger doesn't make a duplicate style if there is already one there", function() {
+	it("MakeBigger doesn't make a duplicate style if there is already one there", function () {
 		$('body').append("<div class='ignore fooStyle ignoreMeToo '></div>");
-		expect(GetStylesAfterMakeBigger().split('fooStyle').length-1).toEqual(1);
+		expect(GetStylesAfterMakeBigger().split('fooStyle').length - 1).toEqual(1);
 	});
 });
