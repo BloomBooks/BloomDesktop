979265f56a59525699d6d9cd9de68fd7c71133ca
John Hatton <hattonjohn@gmail.com>
2013-11-26 16:28:28 -0800

    Initial Parse.com login and book count functions and tests.

---
+++ src/BloomExe/WebLibraryIntegration/BookTransfer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bloom.WebLibraryIntegration
+{
+	/// <summary>
+	/// Currently pushes a book's metadata to Parse.com (a mongodb service) and files to Amazon S3
+	/// </summary>
+	public class BookTransfer
+	{
+
+	}
+}
