0e1d555c1abdc8816e80ab644b9e24ef5bfec03a
John Hatton <hattonjohn@gmail.com>
2013-12-19 11:22:33 -0800

    Add <body> classes to help stylsheets in different contexts

--- src/BloomExe/Book/Book.cs
+++ src/BloomExe/Book/Book.cs
@@ -466,6 +466,7 @@ namespace Bloom.Book
 
             AddPreviewJScript(previewDom);
             previewDom.AddPublishClassToBody();
+    	    previewDom.AddFirefoxClassToBody();
             return previewDom;
         }
 
