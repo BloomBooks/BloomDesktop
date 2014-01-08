fe7d9c6329ec364025a2533d45017b7bc280f2fa
Gordon Martin <gordon-soft_martin@sil.org>
2014-01-08 07:15:14 -0800

    Change name of class that hides language tags

--- src/BloomBrowserUI/bookEdit/js/bloomEditing.js
+++ src/BloomBrowserUI/bookEdit/js/bloomEditing.js
@@ -739,7 +739,7 @@ function ResizeUsingPercentages(e,ui){
 
          // if this or any parent element has the class .noLangDisplay, we don't want to show any of these tags
          // first usage (for instance) was turning off language tags for a whole page
-         if ($(this).hasClass('noLangDisplay') || $(this).parents('.noLangDisplay').length != 0) {
+         if ($(this).hasClass('bloom-hideLanguageNameDisplay') || $(this).parents('.bloom-hideLanguageNameDisplay').length != 0) {
              return;
          }
 
