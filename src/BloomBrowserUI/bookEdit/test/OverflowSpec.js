7a2fa110e89bc8e4bde32e33cf7620562e77b338
Gordon Martin <gordon-soft_martin@sil.org>
2014-02-20 09:48:00 -0800

    Fix spacing in bloomEditing file; change tack on testing overflow

--- src/BloomBrowserUI/bookEdit/test/OverflowSpec.js
+++ src/BloomBrowserUI/bookEdit/test/OverflowSpec.js
@@ -1,26 +1,14 @@
 "use strict";
 
-function PasteTextIntoDiv(id, text) {
-    var fullId = "#" + id;
-    $(fullId).prop('selected', 'selected');
-    $(fullId).trigger('paste');
-}
-
 describe("Overflow Tests", function () {
-    // most perplexingly, jasmine doesn't reset the dom between tests
-    beforeEach(function () {
-        $('body').html('');
-    });
+    /*beforeEach(function () {
+    });*/
 
-    it("basic empty field doesn't overflow", function () {
-        // setup test
-        $('body').append("<div id='testTarget' class='bloom-editable'></div><div id='textToCopy'>A</div>");
-        AddOverflowHandler();
-        $("#textToCopy").prop('selected', 'selected');
-        $("#textToCopy").trigger("copy");
-        $("#testTarget").trigger("paste"); // SUT
-        console.log('Pasted text: '+$('#testTarget').text());
-        var overflowing = $("#testTarget").hasClass('overflow');
-        expect(overflowing).toBe(false);
+    it("Check test page for overflows", function() {
+        $(document).load('test/OverflowTestPage.html');
+        $(".testTarget").each(function () {
+            var overflowing = $(this).IsOverflowing();
+            expect(overflowing).toBe($(this).hasClass('expectToPass'));
+        });
     });
 });
