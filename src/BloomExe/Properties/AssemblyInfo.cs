fd037840e9bb37018e0808fdacc0be00cb4b72f7
John Hatton <hattonjohn@gmail.com>
2014-03-20 15:27:40 -0600

    Update the getdependencies script for 1.2

--- src/BloomExe/Properties/AssemblyInfo.cs
+++ src/BloomExe/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SIL")]
 [assembly: AssemblyProduct("Bloom")]
-[assembly: AssemblyCopyright("© SIL International  2013")]
+[assembly: AssemblyCopyright("© SIL International  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.9.999.0")]
-[assembly: AssemblyVersion("1.0.000.0")]
-[assembly: AssemblyFileVersion("1.0.000.0")]
+[assembly: AssemblyVersion("1.2.000.0")]
+[assembly: AssemblyFileVersion("1.2.000.0")]
