7a24f4feb9756e02ff02e9cf63567784020b9403
John Hatton <hattonjohn@gmail.com>
2014-04-16 08:47:19 -0600

    Fix unit tests moq'ing the htmlthumbnailer

--- src/BloomTests/Book/BookTests.cs
+++ src/BloomTests/Book/BookTests.cs
@@ -83,7 +83,7 @@ namespace BloomTests.Book
 			//warning: we're neutering part of what the code under test is trying to do here:
 			_fileLocator.Setup(x => x.CloneAndCustomize(It.IsAny<IEnumerable<string>>())).Returns(_fileLocator.Object);
 
-            _thumbnailer = new Moq.Mock<HtmlThumbNailer>(new object[] { 60, 60 });
+            _thumbnailer = new Moq.Mock<HtmlThumbNailer>(new object[] { 60, 60, new MonitorTarget() });
             _pageSelection = new Mock<PageSelection>();
             _pageListChangedEvent = new PageListChangedEvent();
       }
