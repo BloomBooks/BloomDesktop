eb21797d1d7f7a3099cab9fba6037b4ede1df79f
Gordon Martin <gordon-soft_martin@sil.org>
2014-05-14 10:18:48 -0700

    Make accordion panel full-length

---
+++ src/BloomBrowserUI/bookEdit/js/customAccordion.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../../lib/jquery.d.ts" />
+
+interface accordionInterface extends JQuery {
+	accordion(options: any): JQuery;
+	resizable(options: any): JQuery;
+}
+
+$(function () {
+	(<accordionInterface>$("#accordion")).accordion({
+		heightStyle: "fill"
+	});
+});
+
+$(function () {
+	(<accordionInterface>$("#editControlsRoot")).resizable({
+		minHeight: 300,
+		minWidth: 200,
+		resize: function () {
+			(<accordionInterface>$("#accordion")).accordion("refresh");
+		}
+	});
+});
