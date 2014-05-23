a3a86925ecbd4fa026c245dcf59d4c4256e75eb8
Eberhard Beilharz <eb1@sil.org>
2014-05-23 17:11:18 +0200

    Fix linux build after merge

--- src/BloomExe/HtmlThumbNailer.cs
+++ src/BloomExe/HtmlThumbNailer.cs
@@ -459,6 +459,8 @@ namespace Bloom
 			}
 			return thumbnail;
 #else
+			int skipMarginH = 30;
+			int skipMarginV = 30;
 			Bitmap croppedImage = (bmp as Bitmap).Clone(new Rectangle(new Point(skipMarginH, skipMarginV), new Size(bmp.Width - 2 * skipMarginH, bmp.Height - 2 * skipMarginV)), bmp.PixelFormat);
 			return croppedImage.GetThumbnailImage(destinationWidth, destinationHeight, null, System.IntPtr.Zero);
 #endif
