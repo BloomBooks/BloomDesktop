0c1556f48003766d9c9fd3a8a942bfa9b6e4db9b
Gordon Martin <gordon-soft_martin@sil.org>
2014-06-19 11:17:54 -0700

    Add dialog to SourceCollection jump to BloomLibrary.org

---
+++ src/BloomExe/CollectionTab/BloomLibraryLinkVerification.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using L10NSharp;
+
+namespace Bloom.CollectionTab
+{
+    public partial class BloomLibraryLinkVerification : Form
+    {
+        public BloomLibraryLinkVerification()
+        {
+            InitializeComponent();
+        }
+
+        private void Initialize()
+        {
+            // Set localized caption
+            Text = LocalizationManager.GetString("CollectionTab.bloomLibraryLinkVerificationCaption", "Source Collection", "get this clicking on BloomLibrary.org link in source collection");
+
+            // Set localized message
+            var msg = "Note: The Bloom Library contains shell books." + Environment.NewLine + Environment.NewLine;
+            msg += "Your current collection is a \"source collection\", meaning it is for making shell books," + Environment.NewLine;
+            msg += "not for translating them into the vernacular." + Environment.NewLine + Environment.NewLine;
+            msg += "When you've made a shell, you can then upload it to the Bloom Library from the Publish tab.";
+            _message.Text = LocalizationManager.GetString("CollectionTab.bloomLibraryLinkVerification", msg, "get this clicking on BloomLibrary.org link in source collection");
+
+            // Set information icon
+            // Review: Could use a larger image here...
+            _infoIcon.Image = SystemIcons.Information.ToBitmap();
+        }
+
+        internal DialogResult GetVerification(IWin32Window owner)
+        {
+            Initialize();
+            return ShowDialog(owner);
+        }
+    }
+}
