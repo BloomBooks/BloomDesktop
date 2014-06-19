12ad7514e14bfcf8e0b1ff0663156b2789a41774
John Hatton <hattonjohn@gmail.com>
2014-06-19 19:58:01 +0000

    BloomLibraryLinkVerification.cs edited online with Bitbucket

--- src/BloomExe/CollectionTab/BloomLibraryLinkVerification.cs
+++ src/BloomExe/CollectionTab/BloomLibraryLinkVerification.cs
@@ -18,8 +18,8 @@ namespace Bloom.CollectionTab
 			Text = LocalizationManager.GetString("CollectionTab.bloomLibraryLinkVerificationCaption", "Source Collection", "get this clicking on BloomLibrary.org link in source collection");
 
 			// Set localized message
-			var msg = "Note: The Bloom Library contains shell books." + Environment.NewLine + Environment.NewLine;
-			msg += "Your current collection is a \"source collection\", meaning it is for making shell books," + Environment.NewLine;
+			var msg = "Note: The Bloom Library contains shell books, whereas ";
+			msg += "your current collection is a \"source collection\", meaning it is for making shell books," + Environment.NewLine;
 			msg += "not for translating them into the vernacular." + Environment.NewLine + Environment.NewLine;
 			msg += "When you've made a shell, you can then upload it to the Bloom Library from the Publish tab.";
 			_message.Text = LocalizationManager.GetString("CollectionTab.bloomLibraryLinkVerification", msg, "get this clicking on BloomLibrary.org link in source collection");
