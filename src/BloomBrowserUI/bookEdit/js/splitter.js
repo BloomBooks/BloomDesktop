8e1d6d69df4dba2629565c8a19c50e6f92fc490c
John Hatton <hattonjohn@gmail.com>
2014-07-09 10:50:16 -0600

    Spiked feasibility of a horizontal splitter between page elements to allow user to resize them.

---
+++ src/BloomBrowserUI/bookEdit/js/splitter.js
@@ -0,0 +1,70 @@
+$(function() {
+
+    // We stick a horizontalSplitter in between pairs of objects with the heightAdjustable class
+    $('.heightAdjustable').each(function(){
+        if($(this).next().hasClass('heightAdjustable')){
+            $(this).after("<div class='horizontalSplitter'></div>");
+        }
+    });
+    $('.bloom-translationGroup, .bloom-imageContainer').each(function(){
+        var page = $(this).closest('.bloom-page')[0];
+        if(page.id.indexOf('5dcd48df')!=0 &&
+           $(page).data('pagelineage').indexOf('5dcd48df')!=0)
+            return;
+        var previous = $(this).prev();
+        if(previous.hasClass('bloom-translationGroup') || previous.hasClass('bloom-imageContainer')){
+            $(this).before("<div class='horizontalSplitter'></div>");
+        }
+    });
+    function Splitter(affordance, topElement, bottomElement){
+        var lastY= 0;
+        var self= this;
+
+        this.topElement = topElement;
+        this.bottomElement = bottomElement;
+
+        affordance.addEventListener('mousedown', function(event) {
+            event.preventDefault();    /* prevent teYt selection */
+
+            self.lastY = event.clientY;
+
+            window.addEventListener('mousemove', self.drag);
+            window.addEventListener('mouseup', self.endDrag);
+        });
+
+        this.drag = function(event) {
+            var topHeight;
+            var bottomHeight;
+            var verticalChange = event.clientY - self.lastY;
+            topHeight = $(self.topElement).height();
+            bottomHeight = $(self.bottomElement).height();
+            topHeight = parseInt(topHeight, 10) + verticalChange;
+            bottomHeight = parseInt(bottomHeight, 10) - verticalChange;
+            $(self.topElement).height(topHeight + 'px');
+            $(self.bottomElement).height(bottomHeight + 'px');
+
+            self.lastY = event.clientY;
+        };
+
+        this.endDrag=function() {
+            window.removeEventListener('mousemove', self.drag);
+            window.removeEventListener('mouseup', self.endDrag);
+        };
+    }
+    $('.horizontalSplitter').each(function(){
+        new Splitter(this, $(this).prev(), $(this).next());
+    });
+});
+
+
+//    Experiments in the next stage which would be allowing adding of new image and text blocks
+// $('.addImage').click(function(){
+//        var before = $(this).closest('.addOns')[0];
+//         before = $(before).prev();
+//        $(before).after("<div class='bloom-imageContainer'><img src='placeholder.png'></div>");
+//    });
+//    $('.addText').click(function(){
+//        var before = $(this).closest('.addOns')[0];
+//         before = $(before).prev();
+//        $(before).after("<P>Hello World</P>");
+//    });
