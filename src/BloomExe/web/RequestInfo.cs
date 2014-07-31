120876451458c74199a77da9cbb3f0f463562e82
Phil Hopper <phillip_hopper@sil.org>
2014-07-31 15:25:33 -0400

    Add comments

--- src/BloomExe/web/RequestInfo.cs
+++ src/BloomExe/web/RequestInfo.cs
@@ -53,8 +53,17 @@ namespace Bloom.web
 			{
 				_actualContext.Response.ContentLength64 = fs.Length;
 
+				// A HEAD request (rather than a GET or POST request) is a request for just headers, and nothing can be written
+				// to the OutputStream. It is normally used to check if the contents of the file have changed without taking the
+				// time and bandwidth needed to download the full contents of the file. The 2 pieces of information being returned 
+				// are the Content-Length and Last-Modified headers. The requestor can use this information to determine if the
+				// contents of the file have changed, and if they have changed the requestor can then decide if the file needs to
+				// be reloaded.
 				if (_actualContext.Request.HttpMethod == "HEAD")
 				{
+					// Originally we were returning the Last-Modified header with every response, but we discovered that this was
+					// causing Geckfx to cache the contents of the files. This made debugging difficult because, even if the file
+					// changed, Geckofx would use the cached file rather than requesting the updated file from the localhost.
 					_actualContext.Response.AppendHeader("Last-Modified", lastModified);
 				}
 				else
