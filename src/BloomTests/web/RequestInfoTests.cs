f8551d10b0fd14994976953a9d1f527d42542182
Phil Hopper <phillip_hopper@sil.org>
2014-08-25 15:08:20 -0400

    Test response with special characters \&<'@?>/"

---
+++ src/BloomTests/web/RequestInfoTests.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Text;
+using Bloom.web;
+using NUnit.Framework;
+using Palaso.IO;
+
+namespace BloomTests.web
+{
+	[TestFixture]
+	class RequestInfoTests
+	{
+
+		[Test]
+		public void RetrieveFileWithSpecialCharacters()
+		{
+			const string fileContents = @"\&<'@?>/" + "\"";
+			using (var asciiFile = MakeTempFile(Encoding.ASCII.GetBytes(fileContents)))
+			{
+				using (var utf8File = MakeTempFile(Encoding.UTF8.GetBytes(fileContents)))
+				{
+					var request = new PretendRequestInfo(ServerBase.PathEndingInSlash);
+
+					request.WriteCompleteOutput(File.ReadAllText(asciiFile.Path));
+					var asciiString = request.ReplyContents;
+
+					Assert.AreEqual(asciiString.Length, 9);
+					Assert.AreEqual(asciiString[0], '\\');
+					Assert.AreEqual(asciiString[1], '&');
+					Assert.AreEqual(asciiString[2], '<');
+					Assert.AreEqual(asciiString[3], '\'');
+					Assert.AreEqual(asciiString[4], '@');
+					Assert.AreEqual(asciiString[5], '?');
+					Assert.AreEqual(asciiString[6], '>');
+					Assert.AreEqual(asciiString[7], '/');
+					Assert.AreEqual(asciiString[8], '"');
+
+					request.WriteCompleteOutput(File.ReadAllText(utf8File.Path));
+					var utf8String = request.ReplyContents;
+					Assert.AreEqual(utf8String.Length, 9);
+					Assert.AreEqual(utf8String[0], '\\');
+					Assert.AreEqual(utf8String[1], '&');
+					Assert.AreEqual(utf8String[2], '<');
+					Assert.AreEqual(utf8String[3], '\'');
+					Assert.AreEqual(utf8String[4], '@');
+					Assert.AreEqual(utf8String[5], '?');
+					Assert.AreEqual(utf8String[6], '>');
+					Assert.AreEqual(utf8String[7], '/');
+					Assert.AreEqual(utf8String[8], '"');
+				}
+			}
+		}
+
+		private TempFile MakeTempFile(byte[] contents)
+		{
+			var file = TempFile.WithExtension(".tmp");
+			File.Delete(file.Path);
+			File.WriteAllBytes(file.Path, contents);
+			return file;
+		}
+	}
+}
