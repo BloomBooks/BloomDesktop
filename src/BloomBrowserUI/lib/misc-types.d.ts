357cffe1338eb6152cf126f64465352979899bac
Phil Hopper <phillip_hopper@sil.org>
2014-09-05 14:46:09 -0400

    Fix things that were broken after merge with default.

---
+++ src/BloomBrowserUI/lib/misc-types.d.ts
@@ -0,0 +1,9 @@
+/**
+ * This file is for defining things that are not in .ts files, but the TypeScript compiler wants them.
+ */
+
+declare function GetInlineDictionary(): any;
+
+interface String {
+	startsWith(str: string): boolean
+}
