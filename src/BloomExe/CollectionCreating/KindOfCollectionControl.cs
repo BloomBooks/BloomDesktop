2851c986fce5fa4e8f8bd1d6dcc1029cc489d1ae
John Hatton <hattonjohn@gmail.com>
2014-09-10 10:01:39 -0600

    Work around for L10NSharp bug listed in BL-316 "L10NSharp code scanner drops NewCollectionWizard.KindOfCollectionPage.sourceCollectionDescription"

--- src/BloomExe/CollectionCreating/KindOfCollectionControl.cs
+++ src/BloomExe/CollectionCreating/KindOfCollectionControl.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
 using Bloom.Collection;
+using L10NSharp;
+using Palaso.UI.WindowsForms.i18n;
 
 namespace Bloom.CollectionCreating
 {
@@ -12,6 +14,11 @@ namespace Bloom.CollectionCreating
 		public KindOfCollectionControl()
 		{
 			InitializeComponent();
+
+			//this is a work-around https://jira.sil.org/browse/BL-316 until we figure out why l10nsharp is dropping this one item
+			_sourceCollectionDescription.Text =
+				LocalizationManager.GetString("NewCollectionWizard.KindOfCollectionPage.sourceCollectionDescription",
+						"A collection of shell or template books in one or more languages of wider communication. You will be able to upload these shells to BloomLibrary.org and optionally make a BloomPack to give to others so that they can make vernacular books with your shells.");
 		}
 
 		private void _radioNormalVernacularCollection_CheckedChanged(object sender, System.EventArgs e)
