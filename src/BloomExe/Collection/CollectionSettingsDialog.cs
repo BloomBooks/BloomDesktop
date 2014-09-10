0fd7793fba40b569816892543fa4a769dae1b414
Gordon Martin <gordon-soft_martin@sil.org>
2014-09-10 10:58:09 -0700

    Get CollectionSettings to load, save and check/uncheck rtl feature

--- src/BloomExe/Collection/CollectionSettingsDialog.cs
+++ src/BloomExe/Collection/CollectionSettingsDialog.cs
@@ -77,11 +77,8 @@ namespace Bloom.Collection
 			_language2Name.Text = string.Format("{0} ({1})", lang2UiName, _collectionSettings.Language2Iso639Code);
 			_language1FontLabel.Text = string.Format("Default Font for {0}", lang1UiName);
 			_language2FontLabel.Text = string.Format("Default Font for {0}", lang2UiName);
-			// Setup dropdown items for Right To Left combo
-			_rtlLanguagesCombo.DropDownItems.Clear();
-			_rtlLanguagesCombo.DropDownItems.Add(lang1UiName);
-			_rtlLanguagesCombo.DropDownItems.Add(lang2UiName);
 
+			var lang3UiName = string.Empty;
 			if (string.IsNullOrEmpty(_collectionSettings.Language3Iso639Code))
 			{
 				_language3Name.Text = "--";
@@ -92,7 +89,7 @@ namespace Bloom.Collection
 			}
 			else
 			{
-				var lang3UiName = _collectionSettings.GetLanguage3Name(LocalizationManager.UILanguageId);
+				lang3UiName = _collectionSettings.GetLanguage3Name(LocalizationManager.UILanguageId);
 				_language3Name.Text = string.Format("{0} ({1})", lang3UiName, _collectionSettings.Language3Iso639Code);
 				_language3FontLabel.Text = string.Format("Default Font for {0}", lang3UiName);
 				_removeLanguage3Link.Visible = true;
@@ -102,6 +99,7 @@ namespace Bloom.Collection
 				_rtlLanguagesCombo.DropDownItems.Add(lang3UiName);
 			}
 
+			SetupItemsForRtlCombo(lang1UiName, lang2UiName, lang3UiName);
 			_restartReminder.Visible = _restartRequired;
             _okButton.Text = _restartRequired ? LocalizationManager.GetString("CollectionSettingsDialog.Restart", "Restart", "If you make certain changes in the settings dialog, the OK button changes to this.") : LocalizationManager.GetString("Common.OKButton", "&OK");
 			
@@ -110,6 +108,42 @@ namespace Bloom.Collection
 			_xmatterPackCombo.SelectedItem = _xmatterPackFinder.FindByKey(_collectionSettings.XMatterPackName); 
 			if (_xmatterPackCombo.SelectedItem == null) //if something goes wrong
 				_xmatterPackCombo.SelectedItem = _xmatterPackFinder.FactoryDefault;
+		}
+
+		private void SetupItemsForRtlCombo(string lang1, string lang2, string lang3)
+		{
+			_rtlLanguagesCombo.DropDownItems.Clear();
+			var item1 = (ToolStripMenuItem)_rtlLanguagesCombo.DropDownItems.Add(lang1);
+			item1.Tag = _collectionSettings.Language1Iso639Code;
+			item1.Checked = _collectionSettings.IsLanguage1Rtl;
+			item1.CheckOnClick = true;
+			item1.CheckedChanged += new EventHandler(OnRtlLanguagesCombo_CheckedChanged);
+			var item2 = (ToolStripMenuItem)_rtlLanguagesCombo.DropDownItems.Add(lang2);
+			item2.Tag = _collectionSettings.Language2Iso639Code;
+			item2.Checked = _collectionSettings.IsLanguage2Rtl;
+			item2.CheckOnClick = true;
+			item2.CheckedChanged += new EventHandler(OnRtlLanguagesCombo_CheckedChanged);
+			if (!String.IsNullOrEmpty(lang3))
+			{
+				var item3 = (ToolStripMenuItem)_rtlLanguagesCombo.DropDownItems.Add(lang3);
+				item3.Tag = _collectionSettings.Language3Iso639Code;
+				item3.Checked = _collectionSettings.IsLanguage3Rtl;
+				item3.CheckOnClick = true;
+				item3.CheckedChanged += new EventHandler(OnRtlLanguagesCombo_CheckedChanged);
+			}
+		}
+
+		private void OnRtlLanguagesCombo_CheckedChanged(object sender, EventArgs e)
+		{
+			var item = (ToolStripMenuItem)sender;
+			var itemCode = item.Tag as string;
+			var itemChecked = item.Checked;
+			if (itemCode == _collectionSettings.Language1Iso639Code)
+				_collectionSettings.IsLanguage1Rtl = itemChecked;
+			else if (itemCode == _collectionSettings.Language2Iso639Code)
+				_collectionSettings.IsLanguage2Rtl = itemChecked;
+			else
+				_collectionSettings.IsLanguage3Rtl = itemChecked;
 		}
 
 		private void _language1ChangeLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
