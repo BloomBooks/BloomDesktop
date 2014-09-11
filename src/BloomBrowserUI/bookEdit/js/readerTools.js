e8231bdea3c24175faf0acf0eb65153ca07110ec
John Thomson <john_thomson@sil.org>
2014-09-11 16:50:52 -0500

    Improve restoring selection in presence of <br>s
    Todo: some debug code should be removed or disabled

--- src/BloomBrowserUI/bookEdit/js/readerTools.js
+++ src/BloomBrowserUI/bookEdit/js/readerTools.js
@@ -450,15 +450,26 @@ ReaderToolsModel.prototype.getElementsToCheck = function() {
 };
 
 // Make a selection in the specified node at the specified offset
-ReaderToolsModel.prototype.makeSelectionIn = function(node, offset) {
+// if divBrCount is >=0, we expect to make the selection offset characters into node itself
+// (typically the root div). After traversing offset characters, we will try to additionally
+// traverse divBrCount <br> elements.
+ReaderToolsModel.prototype.selectAtOffset = function(node, offset) {
+    var range = parent.window.document.getElementById('page').contentWindow.document.createRange();
+    range.setStart(node, offset);
+    range.setEnd(node, offset);
+    var selection = parent.window.document.getElementById('page').contentWindow.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    console.log("Selected at " + offset + " in node of type " + node.localName + " with text '" + node.textContent + "' (of length) " + node.textContent.length);
+    if (node.localName === null && node.textContent.length > 0 && node.textContent.charCodeAt(0) == 10) {
+        console.log("first character " + node.textContent.charCodeAt(0));
+    }
+}
+
+ReaderToolsModel.prototype.makeSelectionIn = function(node, offset, divBrCount, atStart) {
     if (node.nodeType === 3) {
         // drilled down to a text node. Make the selection.
-        var range = parent.window.document.getElementById('page').contentWindow.document.createRange();
-        range.setStart(node, offset);
-        range.setEnd(node, offset);
-        var selection = parent.window.document.getElementById('page').contentWindow.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(range);
+        this.selectAtOffset(node, offset);
         return true;
     }
     var i = 0;
@@ -467,12 +478,26 @@ ReaderToolsModel.prototype.makeSelectionIn = function(node, offset) {
     for (; i < node.childNodes.length && offset >= 0; i++) {
         childNode = node.childNodes[i];
         len = childNode.textContent.length;
+        if (divBrCount >= 0 && len == offset) {
+            // We want the selection after childNode itself, plus if possible an additional divBrCount <br> elements
+            for(i++;
+                i < node.childNodes.length && divBrCount > 0 && node.childNodes[i].textContent.length == 0;
+                i++) {
+                if (node.childNodes[i].localName === 'br') divBrCount--;
+            }
+            // We want the selection in node itself, before childNode[i].
+            this.selectAtOffset(node, i);
+            return true;
+        }
         // If it's at the end of a child (that is not the last child) we have a choice whether to put it at the
         // end of that node or the start of the following one. For some reason the IP is invisible if
-        // placed at the end of the preceding one, so prefer the start of the following one.
+        // placed at the end of the preceding one, so prefer the start of the following one, which is why
+        // we generally call this routine with atStart true.
         // (But, of course, if it is the last node we must be able to put the IP at the very end.)
-        if (offset < len || (offset == len && i == node.childNodes.length - 1)) {
-            if (this.makeSelectionIn(childNode, offset)) {
+        // When trying to do a precise restore, we pass atStart carefully, as it may control
+        // whether we end up before or after some <br>s
+        if (offset < len || (offset == len && (i == node.childNodes.length - 1 || !atStart))) {
+            if (this.makeSelectionIn(childNode, offset, -1, atStart)) {
                 return true;
             }
         }
@@ -485,7 +510,7 @@ ReaderToolsModel.prototype.makeSelectionIn = function(node, offset) {
     {
         childNode = node.childNodes[i];
         len = childNode.textContent.length;
-        if (this.makeSelectionIn(childNode, len)) {return true;}
+        if (this.makeSelectionIn(childNode, len, -1, atStart)) {return true;}
     }
     // can't select anywhere (maybe this has no text-node children? Hopefully the caller can find
     // an equivalent place in an adjacent node).
@@ -510,9 +535,33 @@ ReaderToolsModel.prototype.doKeypressMarkup = function() {
         var myRange = selection.getRangeAt(0).cloneRange();
         myRange.setStart(active, 0);
         var offset = myRange.toString().length;
+        // In case the IP is somewhere like after the last <br> or between <br>s,
+        // its anchorNode is the div itself, or perhaps one of its spans, and we want to try to put it back
+        // in a comparable position. -1 marks a selection that is at a text level.
+        // other values count the <br> elements immediately before the selection.
+        // I am hoping it doesn't happen that there are <br>s at multiple levels.
+        // Note that the newly marked up version will have any <br>s at the top level only (children of the div).
+        var divBrCount = -1;
+        if (current.nodeType !== 3)
+        {
+            divBrCount = 0;
+            // endoffset counts the number of childNodes that the selection is after.
+            // We want to know how many <br> nodes are between it and the previous non-empty node.
+            for (k = myRange.endOffset - 1; k >= 0; k-- ) {
+                if (current.childNodes[k].localName === 'br') divBrCount++;
+                else if (current.childNodes[k].textContent.length > 0) break;
+            }
+        }
+        console.log("-----------------");
+        console.log('before: ' + active.innerHTML);
+        var atStart = myRange.endOffset === 0;
+
         self.doMarkup();
+
+        console.log('after: ' + active.innerHTML);
+        console.log('restoring selection using ' + offset + " with brs " + divBrCount + " and atStart " + atStart);
         // Now we try to restore the selection at the specified position.
-        self.makeSelectionIn(active, offset);
+        self.makeSelectionIn(active, offset, divBrCount, atStart);
 
     }, 500);
 };
@@ -553,7 +602,7 @@ ReaderToolsModel.prototype.undo = function() {
     this.activeElement.innerHTML = this.undoStack[this.undoStack.length - 1].html;
     var restoreOffset = this.undoStack[this.undoStack.length - 1].offset;
     if (restoreOffset < 0) return;
-    this.makeSelectionIn(this.activeElement, restoreOffset);
+    this.makeSelectionIn(this.activeElement, restoreOffset, null, true);
 };
 
 ReaderToolsModel.prototype.canUndo = function() {
@@ -573,7 +622,7 @@ ReaderToolsModel.prototype.redo = function() {
     this.activeElement.innerHTML = this.undoStack[this.undoStack.length - 1].html;
     var restoreOffset = this.undoStack[this.undoStack.length - 1].offset;
     if (restoreOffset < 0) return;
-    this.makeSelectionIn(this.activeElement, restoreOffset);
+    this.makeSelectionIn(this.activeElement, restoreOffset, null, true);
 };
 
 /**
