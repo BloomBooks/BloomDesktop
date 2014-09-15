68139e3a1cd7084d9d2c0d6bb8062dc239a71589
Phil Hopper <phillip_hopper@sil.org>
2014-09-15 14:50:48 -0400

    Fix broken JS test

--- src/BloomBrowserUI/bookEdit/test/readerTools_libSynphony.Test.js
+++ src/BloomBrowserUI/bookEdit/test/readerTools_libSynphony.Test.js
@@ -6,7 +6,7 @@ describe("readerTools-libSynphony tests", function() {
         var model = new ReaderToolsModel();
 
         var settings = {};
-        settings.letters = 'a b c d e f g h i j k l m n o p q r s t u v w x y z';
+        settings.letters = 'a b c d e f g h i j k l m n o p q r s t u v w x y z th';
         settings.moreWords = 'one two three';
         settings.stages = [];
 
