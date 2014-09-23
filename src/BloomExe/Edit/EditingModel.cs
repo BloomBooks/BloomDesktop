8266409bf486665199197f4e7d267780036e9aaa
John Thomson <john_thomson@sil.org>
2014-09-23 11:21:05 -0500

    Remove unnecessary locking
    Determined that both methods can only be called on UI thread, so simplified.

--- src/BloomExe/Edit/EditingModel.cs
+++ src/BloomExe/Edit/EditingModel.cs
@@ -42,8 +42,7 @@ namespace Bloom.Edit
 		private string _accordionFolder;
         private EnhancedImageServer _server;
 
-		// As a precaution against the remote possibility that SaveNow can be called from another thread,
-		// please protect all access to these two variables by locking on _tasksToDoAfterSaving.
+		// These variables are not thread-safe. Access only on UI thread.
 	    private bool _inProcessOfSaving;
 		private List<Action> _tasksToDoAfterSaving = new List<Action>(); 
 
@@ -146,34 +145,34 @@ namespace Bloom.Edit
 
 	    private void OnDuplicatePage()
 		{
-			try
-			{
-				SaveNow(); //ensure current page is saved first
-				_domForCurrentPage = null; //prevent us trying to save it later, as the page selection changes
-				_currentlyDisplayedBook.DuplicatePage(_pageSelection.CurrentSelection);
-				_view.UpdatePageList(false);
-				Logger.WriteEvent("Duplicate Page");
-				Analytics.Track("Duplicate Page");
-			}
-			catch (Exception error)
-			{
-				ErrorReport.NotifyUserOfProblem(error, "Could not duplicate that page. Try quiting Bloom, run it again, and then attempt to duplicate the page again. And please click 'details' below and report this to us.");
-			}
-	    }
+		    try
+		    {
+			    SaveNow(); //ensure current page is saved first
+			    _domForCurrentPage = null; //prevent us trying to save it later, as the page selection changes
+			    _currentlyDisplayedBook.DuplicatePage(_pageSelection.CurrentSelection);
+			    _view.UpdatePageList(false);
+			    Logger.WriteEvent("Duplicate Page");
+			    Analytics.Track("Duplicate Page");
+		    }
+		    catch (Exception error)
+		    {
+			    ErrorReport.NotifyUserOfProblem(error,
+				    "Could not duplicate that page. Try quiting Bloom, run it again, and then attempt to duplicate the page again. And please click 'details' below and report this to us.");
+		    }
+		}
 
         private void OnDeletePage()
         {
-	        lock (_tasksToDoAfterSaving)
+			// This can only be called on the UI thread in response to a user button click.
+			// If that ever changed we might need to arrange locking for access to _inProcessOfSaving and _tasksToDoAfterSaving.
+			Debug.Assert(!_view.InvokeRequired);
+	        if (_inProcessOfSaving)
 	        {
-		        if (_inProcessOfSaving)
-		        {
-			        // Somehow (BL-431) it's possible that a Save is still in progress when we start executing a delete page.
-			        // If this happens, to prevent crashes we need to let the Save complete before we go ahead with the delete.
-			        // I don't _think_ Save can happen on another thread, but let's make this as robust as possible.
-			        _tasksToDoAfterSaving.Add(OnDeletePage);
+		        // Somehow (BL-431) it's possible that a Save is still in progress when we start executing a delete page.
+		        // If this happens, to prevent crashes we need to let the Save complete before we go ahead with the delete.
+		        _tasksToDoAfterSaving.Add(OnDeletePage);
 
-			        return;
-		        }
+		        return;
 	        }
 	        try
             {
@@ -796,35 +795,30 @@ namespace Bloom.Edit
 
         public void SaveNow()
         {
-            if (_domForCurrentPage != null)
-            {
-	            try
-	            {
-		            lock (_tasksToDoAfterSaving)
-		            {
-						_inProcessOfSaving = true;
-						_tasksToDoAfterSaving.Clear();
-		            }
-		            _view.CleanHtmlAndCopyToPageDom();
-					_bookSelection.CurrentSelection.SavePage(_domForCurrentPage);
-	            }
-	            finally
-	            {
-		            lock (_tasksToDoAfterSaving)
-		            {
-			            _inProcessOfSaving = false;
-		            }
-	            }
-	            lock (_tasksToDoAfterSaving)
-	            {
-		            while (_tasksToDoAfterSaving.Count > 0)
-		            {
-			            var task = _tasksToDoAfterSaving[0];
-			            _tasksToDoAfterSaving.RemoveAt(0);
-			            task(); // Review: should we release the lock while we do this?
-		            }
-	            }
-            }
+	        if (_domForCurrentPage != null)
+	        {
+		        try
+		        {
+			        // CleanHtml already requires that we are on UI thread. But it's worth asserting here too in case that changes.
+			        // If we weren't sure of that we would need locking for access to _tasksToDoAfterSaving and _inProcessOfSaving,
+			        // and would need to be careful about whether any delayed tasks needed to be on the UI thread.
+			        Debug.Assert(!_view.InvokeRequired);
+			        _inProcessOfSaving = true;
+			        _tasksToDoAfterSaving.Clear();
+			        _view.CleanHtmlAndCopyToPageDom();
+			        _bookSelection.CurrentSelection.SavePage(_domForCurrentPage);
+		        }
+		        finally
+		        {
+			        _inProcessOfSaving = false;
+		        }
+		        while (_tasksToDoAfterSaving.Count > 0)
+		        {
+			        var task = _tasksToDoAfterSaving[0];
+			        _tasksToDoAfterSaving.RemoveAt(0);
+			        task();
+		        }
+	        }
         }
 
 		public void ChangePicture(GeckoHtmlElement img, PalasoImage imageInfo, IProgress progress)
