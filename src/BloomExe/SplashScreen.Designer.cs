2f53a69d5f782854843fd31d551b91c6d3b04de9
Eberhard Beilharz <eb1@sil.org>
2014-09-24 10:49:25 +0200

    Re-add 'Unstable'

--- src/BloomExe/SplashScreen.Designer.cs
+++ src/BloomExe/SplashScreen.Designer.cs
@@ -39,10 +39,13 @@
 			this._shortVersionLabel = new System.Windows.Forms.Label();
 			this.label1 = new System.Windows.Forms.Label();
 			this.pictureBox2 = new System.Windows.Forms.PictureBox();
-			this.label4 = new System.Windows.Forms.Label();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.flowLayoutPanel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// pictureBox1
@@ -104,7 +107,7 @@
 			// 
 			this.flowLayoutPanel1.Controls.Add(this._shortVersionLabel);
 			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(209, 16);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(209, 46);
 			this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
 			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
 			this.flowLayoutPanel1.Size = new System.Drawing.Size(157, 34);
@@ -147,15 +150,27 @@
 			this.pictureBox2.TabIndex = 0;
 			this.pictureBox2.TabStop = false;
 			// 
-			// label4
+			// pictureBox3
 			// 
-			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(86)))), ((int)(((byte)(73)))));
-			this.label4.Location = new System.Drawing.Point(55, 9);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(150, 41);
-			this.label4.TabIndex = 25;
-			this.label4.Text = "All Children Reading";
+			this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox3.Image = global::Bloom.Properties.Resources.unstable;
+			this.pictureBox3.Location = new System.Drawing.Point(8, 9);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(92, 67);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 24;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox4.Image = global::Bloom.Properties.Resources.unstable;
+			this.pictureBox4.Location = new System.Drawing.Point(316, 137);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(92, 67);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox4.TabIndex = 25;
+			this.pictureBox4.TabStop = false;
 			// 
 			// SplashScreen
 			// 
@@ -164,9 +179,10 @@
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
 			this.ClientSize = new System.Drawing.Size(412, 309);
 			this.ControlBox = false;
-			this.Controls.Add(this.label4);
 			this.Controls.Add(this.pictureBox2);
 			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.pictureBox3);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.flowLayoutPanel1);
 			this.Controls.Add(this.label2);
@@ -185,6 +201,8 @@
 			this.flowLayoutPanel1.ResumeLayout(false);
 			this.flowLayoutPanel1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -201,6 +219,7 @@
 		private System.Windows.Forms.Label _shortVersionLabel;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.PictureBox pictureBox2;
-		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
