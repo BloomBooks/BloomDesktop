ac2adbc5dda1853f72d8ff1f9ec2a52d6c32a1bb
Phillip Hopper <phillip_hopper@sil.org>
2014-10-22 09:51:19 -0400

    Fix a few TypeScript warnings

--- src/BloomBrowserUI/bookEdit/js/BloomAccordion.ts
+++ src/BloomBrowserUI/bookEdit/js/BloomAccordion.ts
@@ -1,19 +1,16 @@
 ﻿/// <reference path="../../lib/jquery.d.ts" />
-
-interface accordionInterface extends JQuery {
-	accordion(options: any): JQuery;
-}
+/// <reference path="../../lib/jquery-ui.d.ts" />
 
 class BloomAccordion {
 
 	constructor() {
-		(<accordionInterface>$("#accordion")).accordion({
+		$("#accordion").accordion({
 			heightStyle: "fill"
 		});
 	}
 
 	static Resize() {
-		(<accordionInterface>$("#accordion")).accordion("refresh");
+		$("#accordion").accordion("refresh");
 		//var myHeight = $(document).find(".accordionRoot").innerHeight();
 		//console.log("Refreshed accordion to: "+myHeight);
 	}
