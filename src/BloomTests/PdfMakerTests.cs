f8821daeadc98e911689ca9dccfc55ba1dfaefa5
JohnThomson <john_thomson@sil.org>
2014-11-12 14:26:46 -0600

    Fix polarity of test on last change

--- src/BloomTests/PdfMakerTests.cs
+++ src/BloomTests/PdfMakerTests.cs
@@ -26,7 +26,7 @@ namespace BloomTests
 		[TestFixtureTearDown]
 		public void FixtureTearDown()
 		{
-#if !__MonoCS__
+#if __MonoCS__
 			// Doing this in Windows works on dev machines but somehow freezes the TC test runner
 			if (Gecko.Xpcom.IsInitialized)
 				Gecko.Xpcom.Shutdown();
