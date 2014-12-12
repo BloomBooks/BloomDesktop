d993aed0da356590705cdc6d333693ec4bfdd6c0
Eberhard Beilharz <eb1@sil.org>
2014-12-12 14:59:17 +0100

    Fix failing unit tests on Linux builds on Jenkins
    The unit tests that access S3 fail on Jenkins because the build
    agent is behind a proxy.The Amazon SDK classes don't make use of the
    http_proxy environment variables, causing the tests to fail.
    
    This change retrieves the proxy settings from the http_proxy or
    HTTP_PROXY environment variables and sets the corresponding
    properties for the S3 client.

---
+++ src/BloomExe/WebLibraryIntegration/ProxyManager.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) 2014 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
+using System.Diagnostics;
+
+namespace Bloom.WebLibraryIntegration
+{
+	/// <summary>
+	/// Helper class that reads the proxy settings from environment variable http_proxy
+	/// or HTTP_PROXY and provides the information for the Amazon S3Client.
+	/// </summary>
+	public class ProxyManager
+	{
+		public ProxyManager()
+		{
+			var proxy = Environment.GetEnvironmentVariable("http_proxy");
+			if (string.IsNullOrEmpty(proxy))
+				proxy = Environment.GetEnvironmentVariable("HTTP_PROXY");
+
+			Parse(proxy);
+		}
+
+		protected void Parse(string proxyString)
+		{
+			if (string.IsNullOrEmpty(proxyString))
+				return;
+
+			try
+			{
+				var uri = new Uri(proxyString);
+				Hostname = uri.Host;
+				Port = uri.Port;
+				if (!string.IsNullOrEmpty(uri.UserInfo))
+				{
+					Username = uri.UserInfo;
+					if (Username.Contains(":"))
+					{
+						var parts = Username.Split(':');
+						Username = parts[0];
+						Password = parts[1];
+					}
+
+				}
+			}
+			catch (UriFormatException)
+			{
+				// We simply ignore invalid URIs (which might be as simple as missing http://)
+				return;
+			}
+		}
+
+		public string Hostname { get; private set; }
+
+		public int Port { get; private set; }
+
+		public string Username { get; private set; }
+
+		public string Password { get; private set; }
+	}
+}
---
+++ src/BloomTests/WebLibraryIntegration/ProxyManagerTests.cs
@@ -0,0 +1,150 @@
+﻿// Copyright (c) 2014 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
+using NUnit.Framework;
+using Bloom.WebLibraryIntegration;
+
+namespace BloomTests.WebLibraryIntegration
+{
+	[TestFixture]
+	public class ProxyManagerTests
+	{
+		private class ProxyManagerDouble: ProxyManager
+		{
+			public ProxyManagerDouble(string proxy)
+			{
+				Parse(proxy);
+			}
+		}
+
+		private string ProxyVariableLowercase { get; set; }
+		private string ProxyVariableUppercase { get; set; }
+
+		[TestFixtureSetUp]
+		public void FixtureSetup()
+		{
+			ProxyVariableLowercase = Environment.GetEnvironmentVariable("http_proxy");
+			ProxyVariableUppercase = Environment.GetEnvironmentVariable("HTTP_PROXY");
+		}
+
+		[TestFixtureTearDown]
+		public void FixtureTearDown()
+		{
+			Environment.SetEnvironmentVariable("http_proxy", ProxyVariableLowercase);
+			Environment.SetEnvironmentVariable("HTTP_PROXY", ProxyVariableUppercase);
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			Environment.SetEnvironmentVariable("http_proxy", null);
+			Environment.SetEnvironmentVariable("HTTP_PROXY", null);
+		}
+
+		[Test]
+		public void NoProxy_DefaultsToNull()
+		{
+			var proxy = new ProxyManagerDouble(null);
+
+			Assert.That(proxy.Hostname, Is.Null);
+			Assert.That(proxy.Port, Is.EqualTo(0));
+			Assert.That(proxy.Username, Is.Null);
+			Assert.That(proxy.Password, Is.Null);
+		}
+
+		[Test]
+		public void ProxyNoPort_DefaultsToPort80()
+		{
+			var proxy = new ProxyManagerDouble("http://example.com");
+
+			Assert.That(proxy.Hostname, Is.EqualTo("example.com"));
+			Assert.That(proxy.Port, Is.EqualTo(80));
+			Assert.That(proxy.Username, Is.Null);
+			Assert.That(proxy.Password, Is.Null);
+		}
+
+		[Test]
+		public void ProxyNoProtocol_DefaultsToNull()
+		{
+			var proxy = new ProxyManagerDouble("example.com");
+
+			Assert.That(proxy.Hostname, Is.Null);
+			Assert.That(proxy.Port, Is.EqualTo(0));
+			Assert.That(proxy.Username, Is.Null);
+			Assert.That(proxy.Password, Is.Null);
+		}
+
+		[Test]
+		public void ProxyAndPort_DefaultsToUsernameNull()
+		{
+			var proxy = new ProxyManagerDouble("http://example.com:8080");
+
+			Assert.That(proxy.Hostname, Is.EqualTo("example.com"));
+			Assert.That(proxy.Port, Is.EqualTo(8080));
+			Assert.That(proxy.Username, Is.Null);
+			Assert.That(proxy.Password, Is.Null);
+		}
+
+		[Test]
+		public void ProxyAndPortAndUser_DefaultsToPasswordNull()
+		{
+			var proxy = new ProxyManagerDouble("http://user@example.com:8080");
+
+			Assert.That(proxy.Hostname, Is.EqualTo("example.com"));
+			Assert.That(proxy.Port, Is.EqualTo(8080));
+			Assert.That(proxy.Username, Is.EqualTo("user"));
+			Assert.That(proxy.Password, Is.Null);
+		}
+
+		[Test]
+		public void ProxyAndPortAndUserAndPassword()
+		{
+			var proxy = new ProxyManagerDouble("http://user:password@example.com:8080");
+
+			Assert.That(proxy.Hostname, Is.EqualTo("example.com"));
+			Assert.That(proxy.Port, Is.EqualTo(8080));
+			Assert.That(proxy.Username, Is.EqualTo("user"));
+			Assert.That(proxy.Password, Is.EqualTo("password"));
+		}
+
+		[Test]
+		public void LowercaseEnvironmentVariable()
+		{
+			Environment.SetEnvironmentVariable("http_proxy", "http://example.com");
+
+			var proxy = new ProxyManager();
+
+			Assert.That(proxy.Hostname, Is.EqualTo("example.com"));
+			Assert.That(proxy.Port, Is.EqualTo(80));
+			Assert.That(proxy.Username, Is.Null);
+			Assert.That(proxy.Password, Is.Null);
+		}
+
+		[Test]
+		public void UppercaseEnvironmentVariable()
+		{
+			Environment.SetEnvironmentVariable("HTTP_PROXY", "http://example.com");
+
+			var proxy = new ProxyManager();
+
+			Assert.That(proxy.Hostname, Is.EqualTo("example.com"));
+			Assert.That(proxy.Port, Is.EqualTo(80));
+			Assert.That(proxy.Username, Is.Null);
+			Assert.That(proxy.Password, Is.Null);
+		}
+
+		[Test]
+		public void BothEnvironmentVariables_UsesLowercaseVariable()
+		{
+			Environment.SetEnvironmentVariable("http_proxy", "http://example1.com");
+			Environment.SetEnvironmentVariable("HTTP_PROXY", "http://example2.com");
+
+			var proxy = new ProxyManager();
+
+			Assert.That(proxy.Hostname, Is.EqualTo("example1.com"));
+			Assert.That(proxy.Port, Is.EqualTo(80));
+			Assert.That(proxy.Username, Is.Null);
+			Assert.That(proxy.Password, Is.Null);
+		}
+	}
+}
