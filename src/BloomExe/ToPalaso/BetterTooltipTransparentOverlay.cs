416ca483fdbbfd6a43ff06157b2c5de4f4c7d08b
Eberhard Beilharz <eb1@sil.org>
2015-02-11 18:24:06 +0100

    Refactor BetterToolTip
    - rename member variables to follow our conventions
    - put all member variables at top of class
    - remove unnecessary `this` in front of members

--- src/BloomExe/ToPalaso/BetterTooltipTransparentOverlay.cs
+++ src/BloomExe/ToPalaso/BetterTooltipTransparentOverlay.cs
@@ -12,16 +12,16 @@ namespace Bloom.ToPalaso
 		public BetterTooltipTransparentOverlay()
 		{
 			// Disable painting the background.
-			this.SetStyle(ControlStyles.Opaque, true);
-			this.UpdateStyles();
+			SetStyle(ControlStyles.Opaque, true);
+			UpdateStyles();
 
 			// Make sure to set the AutoScaleMode property to None
 			// so that the location and size property don't automatically change
 			// when placed in a form that has different font than this.
-			this.AutoScaleMode = AutoScaleMode.None;
+			AutoScaleMode = AutoScaleMode.None;
 
 			// Tab stop on a transparent sheet makes no sense.
-			this.TabStop = false;
+			TabStop = false;
 		}
 
 		private const short WS_EX_TRANSPARENT = 0x20;
@@ -31,10 +31,10 @@ namespace Bloom.ToPalaso
 			[SecurityPermission(SecurityAction.LinkDemand, UnmanagedCode = true)]
 			get
 			{
-				CreateParams l_cp;
-				l_cp = base.CreateParams;
-				l_cp.ExStyle = (l_cp.ExStyle | WS_EX_TRANSPARENT);
-				return l_cp;
+				CreateParams createParams;
+				createParams = base.CreateParams;
+				createParams.ExStyle = (createParams.ExStyle | WS_EX_TRANSPARENT);
+				return createParams;
 			}
 		}
 	}}
