68f9be0496932873c531092c53c2636203bb1a3f
Gordon Martin <gordon_martin@sil.org>
2015-03-02 10:50:37 -0800

    Get Wall Calendar configuration working
    * create jade version of configuration.htm
    * create typescript version of configure.js
    * small update of Wall Calendar.jade
    * remove Id from cloned pages

--- DistFiles/factoryCollections/Templates/Wall Calendar/configure.ts
+++ DistFiles/factoryCollections/Templates/Wall Calendar/configure.ts
@@ -14,27 +14,35 @@
 //       One with classes 'calendarMonthTop'
 //       One with classes 'calendarMonthBottom'
 //
-// <reference path="jquery.d.ts" />
+/// <reference path="jquery.d.ts" />
+
 // Test class
-var TestCalendar = (function () {
-    function TestCalendar() {
-        // test config is in French, just for fun
-        this.testConfig = '{"calendar": { "year": "2015" },' + '"library":  {"calendar": {' + '"monthNames": ["janv", "fév", "mars", "avr", "mai", "juin", "juil", "aôut", "sept", "oct", "nov", "déc"],' + '"dayAbbreviations": ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]}}}';
+class TestCalendar {
+    // test config is in French, just for fun
+    private testConfig : string =  '{"calendar": { "year": "2015" },' +
+        '"library":  {"calendar": {' +
+        '"monthNames": ["janv", "fév", "mars", "avr", "mai", "juin", "juil", "aôut", "sept", "oct", "nov", "déc"],' +
+        '"dayAbbreviations": ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]}}}';
+    constructor() {
         var configurator = new CalendarConfigurator(this.testConfig);
         configurator.updateDom();
     }
-    return TestCalendar;
-})();
+}
 
-function runUpdate(config) {
+function runUpdate(config:string) {
     var configurator = new CalendarConfigurator(config);
     configurator.updateDom();
 }
 
 // Used to create a calendar in a Wall Calendar book
-var CalendarConfigurator = (function () {
-    function CalendarConfigurator(configString) {
-        if (configString && configString.length > 0)
+class CalendarConfigurator {
+
+    private wrapper: HTMLElement;
+    private configObject : CalendarConfigObject;
+    private table: HTMLTableElement;
+
+    constructor(configString:string) {
+        if(configString && configString.length > 0)
             this.configObject = new CalendarConfigObject(configString);
         else
             this.configObject = new CalendarConfigObject(); // shouldn't happen, even in test...
@@ -45,45 +53,46 @@ var CalendarConfigurator = (function () {
     // @param {configuration} members come from the name attributes of the corresponding configuration.htm file.
     //       Put a new input control in that file, give it a @name attribute, and the value will be available here.
     //
-    CalendarConfigurator.prototype.updateDom = function () {
+    updateDom():void {
         // TODO: As is, the following line is not localizable. Also the class doesn't exist anywhere.
         // $('.vernacularBookTitle').set("text", configuration.year +" Calendar");
-        var year = this.configObject.year;
-        var previous = $('.calendarMonthTop')[0];
-        var originalMonthsPicturePage = $('.calendarMonthTop')[0];
-        for (var month = 0; month < 12; month++) {
-            var monthsPicturePage = $(originalMonthsPicturePage).clone();
+        var year:string = this.configObject.year;
+        var previous:HTMLDivElement = $('.calendarMonthTop')[0];
+        var originalMonthsPicturePage:HTMLDivElement = $('.calendarMonthTop')[0];
+        for (var month:number = 0; month < 12; month++) {
+
+            var monthsPicturePage:HTMLDivElement = $(originalMonthsPicturePage).clone();
             $(monthsPicturePage).removeClass('templateOnly').removeAttr('id'); // don't want to copy a Guid!
 
             $(previous).after(monthsPicturePage);
 
-            var monthDaysPage = this.generateMonth(year, month, this.configObject.monthNames[month], this.configObject.dayAbbreviations);
+            var monthDaysPage:HTMLDivElement = this.generateMonth(year, month, this.configObject.monthNames[month], this.configObject.dayAbbreviations);
             $(monthsPicturePage).after(monthDaysPage);
             previous = monthDaysPage;
         }
         $('.templateOnly').remove();
-    };
+    }
 
-    CalendarConfigurator.prototype.generateMonth = function (year, month, monthName, dayAbbreviations) {
-        var marginBox = document.createElement('div');
+    generateMonth(year: string, month: number, monthName: string, dayAbbreviations: string[]):HTMLDivElement {
+        var marginBox:HTMLDivElement = document.createElement('div');
         $(marginBox).addClass('marginBox');
 
-        var monthPage = document.createElement('div');
+        var monthPage:HTMLDivElement = document.createElement('div');
         $(monthPage).addClass('bloom-page bloom-required A5Landscape calendarMonthBottom');
         $(monthPage).attr('data-page', 'required');
         this.initialize(marginBox);
         this.draw(year, month, monthName, dayAbbreviations);
         monthPage.appendChild(marginBox);
         return monthPage;
-    };
+    }
 
-    CalendarConfigurator.prototype.initialize = function (wrapper) {
-        if (typeof wrapper !== "undefined" && wrapper != null)
+    initialize (wrapper: HTMLElement):void {
+        if(typeof wrapper !== "undefined" && wrapper != null)
             this.wrapper = wrapper;
-    };
+    }
 
-    CalendarConfigurator.prototype.draw = function (year, month, monthName, dayAbbreviations) {
-        var header = document.createElement('p');
+    draw(year : string, month : number, monthName : string, dayAbbreviations : string[]):void {
+        var header:HTMLParagraphElement = document.createElement('p');
         $(header).addClass('calendarBottomPageHeader');
         $(header).text(monthName + " " + year);
         $(this.wrapper).append(header);
@@ -91,44 +100,45 @@ var CalendarConfigurator = (function () {
         this.drawHeader(dayAbbreviations);
         this.drawBody(year, month);
         $(this.wrapper).append(this.table);
-        //TODO we'll see if we need this later...
-        //        var style:HTMLStyleElement = document.createElement('style');
-        //        style.type = 'text/css';
-        //        style.innerHTML = '.book-font { font-family: ' + params[1] + '; }';
-        //        document.getElementsByTagName('head')[0].appendChild(style);
-    };
 
-    CalendarConfigurator.prototype.drawHeader = function (dayAbbreviations) {
-        var thead = document.createElement('thead');
-        var row = document.createElement('tr');
+        //TODO we'll see if we need this later...
+//        var style:HTMLStyleElement = document.createElement('style');
+//        style.type = 'text/css';
+//        style.innerHTML = '.book-font { font-family: ' + params[1] + '; }';
+//        document.getElementsByTagName('head')[0].appendChild(style);
+    }
+
+    drawHeader(dayAbbreviations : string[]) {
+        var thead:HTMLTableSectionElement = document.createElement('thead');
+        var row:HTMLTableRowElement = document.createElement('tr');
         dayAbbreviations.forEach(function (abbr) {
-            var thElem = document.createElement('th');
+            var thElem:HTMLTableHeaderCellElement = document.createElement('th');
             $(thElem).text(abbr);
             $(row).append(thElem);
         });
         $(thead).append(row);
         $(this.table).append(thead);
-    };
+    }
 
-    CalendarConfigurator.prototype.drawBody = function (year, month) {
-        var body = document.createElement('tbody');
+    drawBody(year:string, month:number): void {
+        var body:HTMLTableSectionElement = document.createElement('tbody');
         var start = new Date(parseInt(year), month, 1);
         start.setDate(1 - start.getDay());
         do {
             start = this.drawWeek(body, start, month);
-        } while(start.getMonth() == month);
+        } while (start.getMonth() == month);
         $(this.table).append(body);
-    };
+    }
 
-    CalendarConfigurator.prototype.drawWeek = function (body, date, month) {
-        var row = document.createElement('tr');
-        for (var i = 0; i < 7; i++) {
-            var dayCell = document.createElement('td');
+    drawWeek(body, date, month): Date {
+        var row:HTMLTableRowElement = document.createElement('tr');
+        for (var i:number = 0; i < 7; i++) {
+            var dayCell:HTMLTableDataCellElement = document.createElement('td');
             if (date.getMonth() == month) {
-                var dayNumberElement = document.createElement('p');
+                var dayNumberElement:HTMLParagraphElement = document.createElement('p');
                 $(dayNumberElement).text(date.getDate());
                 $(dayCell).append(dayNumberElement);
-                var holidayText = document.createElement('textarea');
+                var holidayText:HTMLTextAreaElement = document.createElement('textarea');
                 $(dayCell).append(holidayText);
             }
             $(row).append(dayCell);
@@ -136,20 +146,25 @@ var CalendarConfigurator = (function () {
         }
         $(body).append(row);
         return date;
-    };
-    return CalendarConfigurator;
-})();
+    }
+}
 
-var CalendarConfigObject = (function () {
-    function CalendarConfigObject(jsonString) {
-        this.defaultConfig = '{"calendar": { "year": "2015" },' + '"library":  {"calendar": {' + '"monthNames": ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],' + '"dayAbbreviations": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]}}}';
-        if (typeof jsonString === "undefined" || jsonString.length == 0)
+class CalendarConfigObject {
+    private defaultConfig : string =  '{"calendar": { "year": "2015" },' +
+    '"library":  {"calendar": {' +
+    '"monthNames": ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],' +
+    '"dayAbbreviations": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]}}}';
+
+    public year : string;
+    public monthNames : string[];
+    public dayAbbreviations : string[];
+
+    constructor (jsonString? : string) {
+        if(typeof jsonString === "undefined" || jsonString.length == 0)
             jsonString = this.defaultConfig;
         var object = $.parseJSON(jsonString);
         this.year = object.calendar.year;
         this.monthNames = object.library.calendar.monthNames;
         this.dayAbbreviations = object.library.calendar.dayAbbreviations;
     }
-    return CalendarConfigObject;
-})();
-//# sourceMappingURL=configure.js.map
+}
