7b16d5b9173b606ff5e053f5c5f726ffc7e9e045
unknown <hopper@SWD-Hopper.Waxhaw.Jaars.Org>
2015-03-19 14:36:34 -0400

    Queue requests to localhost/bloom
    rather than processing as many as possible simultaneously

--- src/BloomExe/web/ServerBase.cs
+++ src/BloomExe/web/ServerBase.cs
@@ -1,99 +1,56 @@
-﻿// Copyright (c) 2014 SIL International
+﻿// Copyright (c) 2015 SIL International
 // This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Windows.Forms;
-using Palaso.Reporting;
 using Palaso.Code;
+using Palaso.Reporting;
+using ThreadState = System.Threading.ThreadState;
+
 
 namespace Bloom.web
 {
-	public abstract class ServerBase: IDisposable
+	public abstract class ServerBase : IDisposable
 	{
-		public static bool IsAbleToUsePort;
-
-		private HttpListener _listener;
+		private readonly HttpListener _listener;
+		private readonly Thread _listenerThread;
+		private readonly Thread[] _workers;
+		private readonly ManualResetEvent _stop, _ready;
+		private readonly Queue<HttpListenerContext> _queue;
 		private bool _isDisposing;
-		private Thread _listenerThread;
-		private readonly ManualResetEvent _stop;
 
-		public ServerBase()
+		protected ServerBase()
 		{
+			_workers = new Thread[4];
+			_queue = new Queue<HttpListenerContext>();
 			_stop = new ManualResetEvent(false);
+			_ready = new ManualResetEvent(false);
+			_listener = new HttpListener();
+			_listenerThread = new Thread(HandleRequests);
 		}
 
-		#region Disposable stuff
-
-		#if DEBUG
+#if DEBUG
 		/// <summary/>
 		~ServerBase()
 		{
 			Dispose(false);
 		}
-		#endif
-
-		/// <summary/>
-		public bool IsDisposed { get; private set; }
-
-		/// <summary/>
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-
-		/// <summary/>
-		protected virtual void Dispose(bool fDisposing)
-		{
-			System.Diagnostics.Debug.WriteLineIf(!fDisposing, "****** Missing Dispose() call for " + GetType() + ". *******");
-			if (fDisposing && !IsDisposed)
-			{
-				// dispose managed and unmanaged objects
-				try
-				{
-					_isDisposing = true;
-
-					if (_listener != null)
-					{
-						//prompted by the mysterious BL 273, Crash while closing down the imageserver
-						Guard.AgainstNull(_listenerThread, "_listenerThread");
-						//prompted by the mysterious BL 273, Crash while closing down the imageserver
-						Guard.AgainstNull(_stop, "_stop");
-
-						_stop.Set();
-						_listenerThread.Join();
-						_listener.Stop();
-
-						_listener.Close();
-					}
-					_listener = null;
-				}
-				catch (Exception e)
-				{
-					//prompted by the mysterious BL 273, Crash while closing down the imageserver
-					#if DEBUG
-					throw;
-					#else       //just quitely report this
-					DesktopAnalytics.Analytics.ReportException(e);
-					#endif
-				}
-			}
-			IsDisposed = true;
-		}
-
-		#endregion
+#endif
 
 		public void StartWithSetupIfNeeded()
 		{
-			Exception error = null;
+			Exception error;
 			StartWithSetupIfNeeded(out error);
 		}
 
 		protected virtual bool StartWithSetupIfNeeded(out Exception error)
 		{
-			bool didStart = StartWithExceptionHandling(out error);
+			var didStart = StartWithExceptionHandling(out error);
 			if (didStart)
 				return true;
 
@@ -116,140 +73,94 @@ namespace Bloom.web
 			catch (Exception e)
 			{
 				error = e;
-				_listener = null;
 				return false;
 			}
 		}
 
 		private bool TryStart()
 		{
-			_listener = new HttpListener();
 			_listener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
 			_listener.Prefixes.Add(PathEndingInSlash);
 			_listener.Start();
-			//			_listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
 
-			_listenerThread = new Thread(HandleRequests);
 			_listenerThread.Start();
 
+			for (var i = 0; i < _workers.Length; i++)
+			{
+				_workers[i] = new Thread(Worker);
+				_workers[i].Start();
+			}
+
 			return GetIsAbleToUsePort();
-		}
-
-		private bool GetIsAbleToUsePort()
-		{
-			IsAbleToUsePort = false;
-			try
-			{
-				var x = new WebClientWithTimeout { Timeout = 3000 };
-
-				IsAbleToUsePort = ("OK" == x.DownloadString(PathEndingInSlash + "testconnection"));
-			}
-			catch (Exception e)
-			{
-				IsAbleToUsePort = false;
-			}
-			return IsAbleToUsePort;
 		}
 
 		private void HandleRequests()
 		{
 			while (_listener.IsListening)
 			{
-				var context = _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+				var context = _listener.BeginGetContext(ContextReady, null);
+
 				if (0 == WaitHandle.WaitAny(new[] { _stop, context.AsyncWaitHandle }))
 					return;
 			}
 		}
 
-		/// <summary>
-		/// TODO: Note: doing this at runtime isn't as good as doing it in the installer, because we have no way of
-		/// removing these entries on uninstall (but the installer does).
-		/// </summary>
-		private static void AddUrlAccessControlEntry()
+		private void ContextReady(IAsyncResult ar)
 		{
-			MessageBox.Show(
-				string.Format("We need to do one more thing before Bloom is ready. Bloom needs temporary administrator privileges to set up part of its communication with the embedded web browser.{0}{0}After you click 'OK', you may be asked to authorize this step.", Environment.NewLine),
-				"Almost there!", MessageBoxButtons.OK);
-
-			var startInfo = new System.Diagnostics.ProcessStartInfo();
-			startInfo.UseShellExecute = true;
-			startInfo.Verb = "runas"; //makes it as for elevation to admin rights
-
-			if (Environment.OSVersion.Version.Major == 5 /*win xp*/)
-			{
-				startInfo.FileName = "httpcfg";
-				startInfo.Arguments = string.Format("set urlacl -u {0} /a \"D:(A;;GX;;;WD)\"", PathEndingInSlash);
-			}
-			else
-			{
-				startInfo.FileName = "netsh";
-				startInfo.Arguments = string.Format("http add urlacl url={0} user=everyone", PathEndingInSlash.TrimEnd('/'));
-			}
-			System.Diagnostics.Process.Start(startInfo);
-		}
-
-		private void ListenerCallback(IAsyncResult ar)
-		{
-			if (_isDisposing || _listener == null || !_listener.IsListening)
-				return; //strangely, this callback is fired when we close down the listener
-			string rawurl="unknown";
 			try
 			{
-				HttpListenerContext context = _listener.EndGetContext(ar);
-				rawurl = context.Request.RawUrl;
-				HttpListenerRequest request = context.Request;
-				MakeReply(new RequestInfo(context));
+				lock (_queue)
+				{
+					_queue.Enqueue(_listener.EndGetContext(ar));
+					_ready.Set();
+				}
 			}
-			catch(HttpListenerException e)
+			catch { }
+		}
+
+		private void Worker()
+		{
+			WaitHandle[] wait = { _ready, _stop };
+			while (0 == WaitHandle.WaitAny(wait))
 			{
-				//http://stackoverflow.com/questions/4801868/c-sharp-problem-with-httplistener
-				Logger.WriteEvent("At ServerBase: ListenerCallback(): HttpListenerException, which may indicate that the caller closed the connection before we could reply. msg=" + e.Message);
-				Logger.WriteEvent("At ServerBase: ListenerCallback(): url=" + rawurl);
-			}
-			catch (Exception error)
-			{
-				Logger.WriteEvent("At ServerBase: ListenerCallback(): msg="+ error.Message);
-				Logger.WriteEvent("At ServerBase: ListenerCallback(): url="+rawurl);
-				Logger.WriteEvent("At ServerBase: ListenerCallback(): stack=");
-				Logger.WriteEvent(error.StackTrace);
+				HttpListenerContext context;
+				lock (_queue)
+				{
+					if (_queue.Count > 0)
+						context = _queue.Dequeue();
+					else
+					{
+						_ready.Reset();
+						continue;
+					}
+				}
+
+				if (_isDisposing || _listener == null || !_listener.IsListening)
+					return; //strangely, this callback is fired when we close down the listener
+
+				var rawurl = "unknown";
+				try
+				{
+					rawurl = context.Request.RawUrl;
+					MakeReply(new RequestInfo(context));
+				}
+				catch (HttpListenerException e)
+				{
+					// http://stackoverflow.com/questions/4801868/c-sharp-problem-with-httplistener
+					Logger.WriteEvent("At ServerBase: ListenerCallback(): HttpListenerException, which may indicate that the caller closed the connection before we could reply. msg=" + e.Message);
+					Logger.WriteEvent("At ServerBase: ListenerCallback(): url=" + rawurl);
+				}
+				catch (Exception error)
+				{
+					Logger.WriteEvent("At ServerBase: ListenerCallback(): msg=" + error.Message);
+					Logger.WriteEvent("At ServerBase: ListenerCallback(): url=" + rawurl);
+					Logger.WriteEvent("At ServerBase: ListenerCallback(): stack=");
+					Logger.WriteEvent(error.StackTrace);
 #if DEBUG
-				throw;
+					throw;
 #endif
+				}
 			}
-			finally
-			{
-				//	_listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
-			}
-		}
-
-		/// <summary>
-		/// This is designed to be easily unit testable by not taking actual HttpContext, but doing everything through this IRequestInfo object
-		/// </summary>
-		/// <param name="info"></param>
-		internal virtual void MakeReply(IRequestInfo info)
-		{
-			if (!ProcessRequest(info))
-			{
-				ReportMissingFile(info);
-			}
-		}
-
-		protected static string GetLocalPathWithoutQuery(IRequestInfo info)
-		{
-			var r = info.LocalPathWithoutQuery;
-			const string slashBloomSlash = "/bloom/";
-			if (r.StartsWith(slashBloomSlash))
-				r = r.Substring(slashBloomSlash.Length);
-			r = r.Replace("%3A", ":");
-			r = r.Replace("%20", " ");
-			r = r.Replace("%27", "'");
-			return r;
-		}
-
-		protected void ReportMissingFile(IRequestInfo info)
-		{
-			Logger.WriteEvent("**{0}: File Missing: {1}", GetType().Name, GetLocalPathWithoutQuery(info));
-			info.WriteError(404);
 		}
 
 		protected virtual bool ProcessRequest(IRequestInfo info)
@@ -270,6 +181,36 @@ namespace Bloom.web
 			return false;
 		}
 
+		/// <summary>
+		/// This is designed to be easily unit testable by not taking actual HttpContext, but doing everything through this IRequestInfo object
+		/// </summary>
+		/// <param name="info"></param>
+		internal void MakeReply(IRequestInfo info)
+		{
+			if (!ProcessRequest(info))
+			{
+				ReportMissingFile(info);
+			}
+		}
+
+		private void ReportMissingFile(IRequestInfo info)
+		{
+			Logger.WriteEvent("**{0}: File Missing: {1}", GetType().Name, GetLocalPathWithoutQuery(info));
+			info.WriteError(404);
+		}
+
+		protected static string GetLocalPathWithoutQuery(IRequestInfo info)
+		{
+			var r = info.LocalPathWithoutQuery;
+			const string slashBloomSlash = "/bloom/";
+			if (r.StartsWith(slashBloomSlash))
+				r = r.Substring(slashBloomSlash.Length);
+			r = r.Replace("%3A", ":");
+			r = r.Replace("%20", " ");
+			r = r.Replace("%27", "'");
+			return r;
+		}
+
 		public static string PathEndingInSlash
 		{
 			get { return "http://localhost:8089/bloom/"; }
@@ -279,19 +220,116 @@ namespace Bloom.web
 		{
 			switch (extension)
 			{
-				case ".css":  return "text/css";
-				case ".gif":  return "image/gif";
+				case ".css": return "text/css";
+				case ".gif": return "image/gif";
 				case ".htm":
 				case ".html": return "text/html";
 				case ".jpg":
 				case ".jpeg": return "image/jpeg";
-				case ".js":   return "application/x-javascript";
-				case ".png":  return "image/png";
-				case ".pdf":  return "application/pdf";
-				case ".txt":  return "text/plain";
-				case ".svg":  return "image/svg+xml";
-				default:      return "application/octet-stream";
+				case ".js": return "application/x-javascript";
+				case ".png": return "image/png";
+				case ".pdf": return "application/pdf";
+				case ".txt": return "text/plain";
+				case ".svg": return "image/svg+xml";
+				default: return "application/octet-stream";
 			}
 		}
+
+		/// <summary>
+		/// TODO: Note: doing this at runtime isn't as good as doing it in the installer, because we have no way of
+		/// removing these entries on uninstall (but the installer does).
+		/// </summary>
+		private static void AddUrlAccessControlEntry()
+		{
+			MessageBox.Show(
+				string.Format("We need to do one more thing before Bloom is ready. Bloom needs temporary administrator privileges to set up part of its communication with the embedded web browser.{0}{0}After you click 'OK', you may be asked to authorize this step.", Environment.NewLine),
+					@"Almost there!", MessageBoxButtons.OK);
+
+			var startInfo = new ProcessStartInfo { UseShellExecute = true, Verb = "runas" };
+
+			if (Environment.OSVersion.Version.Major == 5 /*win xp*/)
+			{
+				startInfo.FileName = "httpcfg";
+				startInfo.Arguments = string.Format("set urlacl -u {0} /a \"D:(A;;GX;;;WD)\"", PathEndingInSlash);
+			}
+			else
+			{
+				startInfo.FileName = "netsh";
+				startInfo.Arguments = string.Format("http add urlacl url={0} user=everyone", PathEndingInSlash.TrimEnd('/'));
+			}
+			Process.Start(startInfo);
+		}
+
+		private static bool GetIsAbleToUsePort()
+		{
+			try
+			{
+				var x = new WebClientWithTimeout { Timeout = 3000 };
+				return ("OK" == x.DownloadString(PathEndingInSlash + "testconnection"));
+			}
+			catch (Exception e)
+			{
+				return false;
+			}
+		}
+
+		#region Disposable stuff
+
+		private bool IsDisposed { get; set; }
+
+		public void Dispose()
+		{
+			//Stop();
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool fDisposing)
+		{
+			Debug.WriteLineIf(!fDisposing, "****** Missing Dispose() call for " + GetType() + ". *******");
+			if (fDisposing && !IsDisposed)
+			{
+				// dispose managed and unmanaged objects
+				try
+				{
+					_isDisposing = true;
+
+					if (_listener != null)
+					{
+						//prompted by the mysterious BL 273, Crash while closing down the imageserver
+						Guard.AgainstNull(_listenerThread, "_listenerThread");
+						//prompted by the mysterious BL 273, Crash while closing down the imageserver
+						Guard.AgainstNull(_stop, "_stop");
+
+						_stop.Set();
+
+						if (_listenerThread.ThreadState != ThreadState.Unstarted)
+							_listenerThread.Join();
+
+						foreach (var worker in _workers.Where(worker => (worker != null) && (worker.ThreadState != ThreadState.Unstarted)))
+						{
+							worker.Join();
+						}
+
+						_listener.Stop();
+						_listener.Close();
+					}
+				}
+				// ReSharper disable once RedundantCatchClause
+				catch (Exception e)
+				{
+					//prompted by the mysterious BL 273, Crash while closing down the imageserver
+#if DEBUG
+					throw;
+#else				//just quitely report this
+					DesktopAnalytics.Analytics.ReportException(e);
+#endif
+				}
+			}
+			IsDisposed = true;
+		}
+
+		#endregion
 	}
+
 }
