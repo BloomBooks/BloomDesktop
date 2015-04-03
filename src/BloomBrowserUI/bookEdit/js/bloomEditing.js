bcc86198666a7db7ba88cfeb5588c2e83298eddb
John Hatton <hattonjohn@gmail.com>
2015-04-03 10:47:26 -0600

    Factor out most existing image-related code from bloomEditing.js

--- src/BloomBrowserUI/bookEdit/js/bloomEditing.js
+++ src/BloomBrowserUI/bookEdit/js/bloomEditing.js
@@ -228,87 +228,11 @@ function Cleanup() {
     TrimTrailingLineBreaksInDivs(this);
     });
 
-  $('.bloom-imageContainer').css('opacity', '');//comes in on img containers from an old version of myimgscale, and is a major problem if the image is missing
-    $('.bloom-imageContainer').css('overflow', '');//review: also comes form myimgscale; is it a problem?
 
+    cleanupImages();
     cleanupOrigami();
 }
 
- //Make a toolbox off to the side (implemented using qtip), with elements that can be dragged
- //onto the page
-function AddToolbox(container){
-    $(container).find('div.bloom-page.bloom-enablePageCustomization').each(function () {
-        $(this).find('.marginBox').droppable({
-            hoverClass: "ui-state-hover",
-            accept: function () { return true; },
-            drop: function (event, ui) {
-                //is it being dragged in from a toolbox, or just moved around inside the page?
-                if ($(ui.draggable).hasClass('widgetInToolbox')) {
-
-                    //review: since we already did a clone during the tearoff, why clone again?
-                    var $x = $($(ui.draggable).clone()[0]);
-                    // $x.text("");
-
-                    //we need different behavior when it is in the toolbox vs. once it is live
-                    $x.attr("class", $x.data("classesafterdrop"));
-                    $x.removeAttr("classesafterdrop");
-
-                    if ($x.hasClass('bloom-imageContainer')) {
-                        SetupImageContainer($x);
-                    }
-
-                    //review: this find() implies that the draggable thing isn't necesarily the widgetInToolbox. Why not?
-//                    $(this).find('.widgetInToolbox')
-//                            .removeAttr("style")
-//                            .draggable({ containment: "parent" })
-//                            .removeClass("widgetInToolbox")
-//                            .SetupResizableElement(this)
-                    //                            .SetupDeletable(this);
-                    $x.removeAttr("style");
-                    $x.draggable({ containment: "parent" });
-                    $x.removeClass("widgetInToolbox");
-                    SetupResizableElement($x);
-                    SetupDeletable($x);
-
-                    $(this).append($x);
-                }
-            }
-        });
-        var lang1ISO = GetSettings().languageForNewTextBoxes;
-        var heading1CenteredWidget = '<div class="heading1-style centered widgetInToolbox"  data-classesafterdrop="bloom-translationGroup heading1-style centered bloom-resizable bloom-deletable bloom-draggable"><div data-classesafterdrop="bloom-editable bloom-content1" lang="' + lang1ISO + '">Heading 1 Centered</div></div>';
-        var heading2LeftWidget = '<div class="heading2-style widgetInToolbox"  data-classesafterdrop="bloom-translationGroup heading2-style  bloom-resizable bloom-deletable bloom-draggable"><div data-classesafterdrop="bloom-editable bloom-content1" lang="' + lang1ISO + '">Heading 2, Left</div></div>';
-        var fieldWidget = '<div class="widgetInToolbox" data-classesafterdrop="bloom-translationGroup bloom-resizable bloom-deletable bloom-draggable"><div data-classesafterdrop="bloom-editable bloom-content1" lang="' + lang1ISO + '"> A block of normal text.</div></div>';
-        // old one: var imageWidget = '<div class="bloom-imageContainer bloom-resizable bloom-draggable  bloom-deletable widgetInToolbox"><img src="placeHolder.png"></div>';
-        var imageWidget = '<div class="widgetInToolbox " data-classesafterdrop="bloom-imageContainer  bloom-resizable bloom-draggable  bloom-deletable"><img src="placeHolder.png"></div>';
-
-        var toolbox = $(this).parent().append("<div id='toolbox'><h3>Page Elements</h3><ul class='toolbox'><li>" + heading1CenteredWidget + "</li><li>" + heading2LeftWidget + "</li><li>" + fieldWidget + "</li><li>" + imageWidget + "</li></ul></div>");
-
-
-        toolbox.find('.widgetInToolbox').each(function () {
-            $(this).draggable({
-                //note: this is just used for drawing what you drag around..
-                //it isn't what the droppable is actually given. For that, look in the 'drop' item of the droppable() call above.
-                helper: function(event) {
-                    var tearOff = $(this).clone(); //.removeClass('widgetInToolbox');//by removing this, we show it with the actual size it will be when dropped
-                    return tearOff;
-                }
-            });
-        });
-        $(this).qtipSecondary({
-            content: "<div id='experimentNotice'><img src='/bloom/images/experiment.png'/>This is an experimental prototype of template-making within Bloom itself. Much more work is needed before it is ready for real work, so don't bother reporting problems with it yet. The Trello board is <a href='https://trello.com/board/bloom-custom-template-dev/4fb2501b34909fbe417a7b7d'>here</a></b></div>",
-            show: { ready: true },
-            hide: false,
-            position: {
-                at: 'right top',
-                my: 'left top'
-            },
-            style: {
-                classes: 'ui-tooltip-red',
-                tip: { corner: false }
-            }
-        });
-    })
-}
 
 
 function AddExperimentalNotice(element) {
@@ -522,172 +446,6 @@ function SetupDeletable(containerDiv) {
         });
 
     return $(containerDiv);
-}
-
-//Bloom "imageContainer"s are <div>'s with wrap an <img>, and automatically proportionally resize
-//the img to fit the available space
-function SetupImageContainer(containerDiv) {
-    $(containerDiv).mouseenter(function () {
-        var buttonModifier = "largeImageButton";
-        if ($(this).height() < 95) {
-            buttonModifier = 'smallImageButton';
-        }
-        $(this).prepend('<button class="pasteImageButton ' + buttonModifier + '" title="' + localizationManager.getText("EditTab.Image.PasteImage") + '"></button>');
-        $(this).prepend('<button class="changeImageButton ' + buttonModifier + '" title="' + localizationManager.getText("EditTab.Image.ChangeImage") + '"></button>');
-
-        var img = $(this).find('img');
-        if (CreditsAreRelevantForImage(img)) {
-            $(this).prepend('<button class="editMetadataButton ' + buttonModifier + '" title="' + localizationManager.getText("EditTab.Image.EditMetadata") + '"></button>');
-        }
-
-        $(this).addClass('hoverUp');
-    })
-    .mouseleave(function () {
-        $(this).removeClass('hoverUp');
-        $(this).find(".changeImageButton").each(function () {
-            $(this).remove()
-        });
-        $(this).find(".pasteImageButton").each(function () {
-            $(this).remove()
-        });
-        $(this).find(".editMetadataButton").each(function () {
-            if (!$(this).hasClass('imgMetadataProblem')) {
-                $(this).remove()
-            }
-        });
-    });
-}
-
-function CreditsAreRelevantForImage(img) {
-    return $(img).attr('src').toLowerCase().indexOf('placeholder') == -1; //don't offer to edit placeholder credits
-}
-
-//While the actual metadata is embedded in the images (Bloom/palaso does that), Bloom sticks some metadata in data-* attributes
-// so that we can easily & quickly get to the here.
-function SetOverlayForImagesWithoutMetadata(container) {
-    $(container).find(".bloom-imageContainer").each(function () {
-        var img = $(this).find('img');
-        if (!CreditsAreRelevantForImage(img)) {
-           return;
-        }
-        var container = $(this);
-
-        UpdateOverlay(container, img);
-
-        //and if the bloom program changes these values (i.e. the user changes them using bloom), I
-        //haven't figured out a way (apart from polling) to know that. So for now I'm using a hack
-        //where Bloom calls click() on the image when it wants an update, and we detect that here.
-        $(img).click(function () {
-            UpdateOverlay(container, img);
-        });
-    });
-}
-
-function UpdateOverlay(container, img) {
-
-    $(container).find(".imgMetadataProblem").each(function () {
-        $(this).remove()
-    });
-
-    //review: should we also require copyright, illustrator, etc? In many contexts the id of the work-for-hire illustrator isn't available
-    var copyright = $(img).attr('data-copyright');
-    if (!copyright || copyright.length == 0) {
-
-        var buttonModifier = "largeImageButton";
-        if ($(container).height() < 80) {
-            buttonModifier = 'smallImageButton';
-        }
-
-        $(container).prepend("<button class='editMetadataButton imgMetadataProblem "+buttonModifier+"' title='Image is missing information on Credits, Copyright, or License'></button>");
-    }
-}
-
-// Instead of "missing", we want to show it in the right ui language. We also want the text
-// to indicate that it might not be missing, just didn't load (this happens on slow machines)
-// TODO: internationalize
-function SetAlternateTextOnImages(element) {
-    if ($(element).attr('src').length > 0) { //don't show this on the empty license image when we don't know the license yet
-        var nameWithoutQueryString = $(element).attr('src').split("?")[0];
-        $(element).attr('alt', 'This picture, ' + nameWithoutQueryString + ', is missing or was loading too slowly.');
-    } else {
-        $(element).attr('alt', '');//don't be tempted to show something like a '?' unless you fix the result when you have a custom book license on top of that '?'
-    }
-}
-
-function SetupResizableElement(element) {
-    $(element).mouseenter(
-        function () {
-            $(this).addClass("ui-mouseOver")
-        }).mouseleave(function () {
-            $(this).removeClass("ui-mouseOver")
-        });
-    var childImgContainer = $(element).find(".bloom-imageContainer");
-    // A Picture Dictionary Word-And-Image
-    if ($(childImgContainer).length > 0) {
-        /* The case here is that the thing with this class actually has an
-         inner image, as is the case for the Picture Dictionary.
-         The key, non-obvious, difficult requirement is keeping the text below
-         a picture dictionary item centered underneath the image.  I'd be
-         surprised if this wasn't possible in CSS, but I'm not expert enough.
-         So, I switched from having the image container be resizable, to having the
-         whole div (image+headwords) be resizable, then use the "alsoResize"
-         parameter to make the imageContainer resize.  Then, in order to make
-         the image resize in real-time as you're dragging, I use the "resize"
-         event to scale the image up proportionally (and centered) inside the
-         newly resized container.
-         */
-        var img = $(childImgContainer).find("img");
-        $(element).resizable({handles:'nw, ne, sw, se',
-            containment: "parent",
-            alsoResize:childImgContainer,
-           resize:function (event, ui) {
-                img.scaleImage({scale:"fit"})
-            }});
-        return $(element);
-    }
-    //An Image Container div (which must have an inner <img>
-    else if ($(element).hasClass('bloom-imageContainer')) {
-        var img = $(element).find("img");
-        $(element).resizable({handles:'nw, ne, sw, se',
-            containment: "parent",
-            resize:function (event, ui) {
-                img.scaleImage({scale:"fit"})
-            }});
-    }
-    // some other kind of resizable
-    else {
-        $(element).resizable({
-            handles:'nw, ne, sw, se',
-            containment: "parent",
-             stop: ResizeUsingPercentages,
-            start: function(e,ui){
-               if($(ui.element).css('top')=='0px' && $(ui.element).css('left')=='0px'){
-                   $(ui.element).data('doRestoreRelativePosition', 'true');
-               }
-            }
-        });
-    }
-}
-
-//jquery resizable normally uses pixels. This makes it use percentages, which are mor robust across page size/orientation changes
-function ResizeUsingPercentages(e,ui){
-    var parent = ui.element.parent();
-    ui.element.css({
-        width: ui.element.width()/parent.width()*100+"%",
-        height: ui.element.height()/parent.height()*100+"%"
-    });
-
-    //after any resize jquery adds an absolute position, which we don't want unless the user has resized
-    //so this removes it, unless we previously noted that the user had moved it
-    if($(ui.element).data('doRestoreRelativePosition'))
-    {
-        ui.element.css({
-            position: '',
-            top: '',
-            left: ''
-        });
-    }
-    $(ui.element).removeData('hadPreviouslyBeenRelocated');
 }
 
 // Actual testable determination of overflow or not
@@ -1075,25 +833,6 @@ function GetEditor() {
     return new StyleEditor("/bloom/bookEdit");
 }
 
-function SetupImage(image) {
-    //make images scale up to their container without distorting their proportions, while being centered within it.
-    $(image).scaleImage({ scale: "fit" }); //uses jquery.myimgscale.js
-
-    // when the image changes, we need to scale again:
-    $(image).load(function () {
-        $(this).scaleImage({ scale: "fit" });
-    });
-
-    //and when their parent is resized by the user, we need to scale again:
-    $(image).parent().resize(function () {
-        $(this).find("img").scaleImage({ scale: "fit" });
-        try {
-            ResetRememberedSize(this);
-        } catch (error) {
-            console.log(error);
-        }
-    });
-}
 
 function IsInTranslationMode() {
     var body = $("body");
@@ -1121,6 +860,8 @@ $.fn.hasAttr = function (name) {
 // REVIEW: Some of these would be better off in OneTimeSetup, but too much risk to try to decide right now.
 function SetupElements(container) {
 
+    SetupImagesInContainer(container);
+
     //add a marginBox if it's missing. We introduced it early in the first beta
     $(container).find(".bloom-page").each(function () {
         if ($(this).find(".marginBox").length == 0) {
@@ -1135,8 +876,6 @@ function SetupElements(container) {
     $(container).find("textarea").blur(function () {
         this.innerHTML = this.value;
     });
-
-    
 
     var accordion = parent.window.document.getElementById("accordion");
     var model;
@@ -1267,23 +1006,7 @@ function SetupElements(container) {
         });
     });
 
-    //make images look click-able when you cover over them
-    $(container).find(".bloom-imageContainer").each(function () {
-        SetupImageContainer(this);
-    });
-
-    //todo: this had problems. Check out the later approach, seen in draggableLabel (e.g. move handle on the inside, using a background image on a div)
-    $(container).find(".bloom-draggable").mouseenter(function () {
-        $(this).prepend("<button class='moveButton' title='Move'></button>");
-        $(this).find(".moveButton").mousedown(function (e) {
-            $(this).parent().trigger(e);
-        });
-    });
-    $(container).find(".bloom-draggable").mouseleave(function () {
-        $(this).find(".moveButton").each(function () {
-            $(this).remove()
-        });
-    });
+  
 
     $(container).find('div.bloom-editable').each(function () {
         $(this).attr('contentEditable', 'true');
@@ -1349,8 +1072,6 @@ function SetupElements(container) {
     });
 
     //first used in the Uganda SHRP Primer 1 template, on the image on day 1
-    //This took *enormous* fussing in the css. TODO: copy what we learned there
-    //to the (currently experimental) Toolbox template (see 'bloom-draggable')
     $(container).find(".bloom-draggableLabel").each(function () {
         // previous to June 2014, containment was not working, so some items may be
         // out of bounds. Or the stylesheet could change the size of things. This gets any such back in bounds.
@@ -1443,9 +1164,7 @@ function SetupElements(container) {
         SetupResizableElement(this);
     });
 
-    $(container).find("img").each(function () {
-        SetAlternateTextOnImages(this);
-    });
+
 
     SetOverlayForImagesWithoutMetadata(container);
 
@@ -1468,9 +1187,7 @@ function SetupElements(container) {
         });
     }
 
-    $(container).find(".bloom-imageContainer img").each(function() {
-        SetupImage(this);
-    });
+
 
     // Add overflow event handlers so that when a div is overfull,
     // we add the overflow class and it gets a red background or something
---
+++ src/BloomBrowserUI/bookEdit/js/bloomImages.js
@@ -0,0 +1,224 @@
+﻿// Enhance: this could be turned into a Typescript Module with only two public methods
+
+function cleanupImages() {
+    $('.bloom-imageContainer').css('opacity', '');//comes in on img containers from an old version of myimgscale, and is a major problem if the image is missing
+    $('.bloom-imageContainer').css('overflow', '');//review: also comes form myimgscale; is it a problem?
+
+}
+
+function SetupImagesInContainer(container)
+{
+    $(container).find(".bloom-imageContainer img").each(function() {
+        SetupImage(this);
+    });
+
+    $(container).find(".bloom-imageContainer").each(function() {
+        SetupImageContainer(this);
+    });
+
+    //todo: this had problems. Check out the later approach, seen in draggableLabel (e.g. move handle on the inside, using a background image on a div)
+    $(container).find(".bloom-draggable").mouseenter(function() {
+        $(this).prepend("<button class='moveButton' title='Move'></button>");
+        $(this).find(".moveButton").mousedown(function(e) {
+            $(this).parent().trigger(e);
+        });
+    });
+    $(container).find(".bloom-draggable").mouseleave(function() {
+        $(this).find(".moveButton").each(function() {
+            $(this).remove()
+        });
+    });
+
+    $(container).find("img").each(function () {
+        SetAlternateTextOnImages(this);
+    });
+}
+
+function SetupImage(image) {
+    //make images scale up to their container without distorting their proportions, while being centered within it.
+    $(image).scaleImage({ scale: "fit" }); //uses jquery.myimgscale.js
+
+    // when the image changes, we need to scale again:
+    $(image).load(function () {
+        $(this).scaleImage({ scale: "fit" });
+    });
+
+    //and when their parent is resized by the user, we need to scale again:
+    $(image).parent().resize(function () {
+        $(this).find("img").scaleImage({ scale: "fit" });
+        try {
+            ResetRememberedSize(this);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
+
+//Bloom "imageContainer"s are <div>'s with wrap an <img>, and automatically proportionally resize
+//the img to fit the available space
+function SetupImageContainer(containerDiv) {
+    $(containerDiv).mouseenter(function () {
+        var buttonModifier = "largeImageButton";
+        if ($(this).height() < 95) {
+            buttonModifier = 'smallImageButton';
+        }
+        $(this).prepend('<button class="pasteImageButton ' + buttonModifier + '" title="' + localizationManager.getText("EditTab.Image.PasteImage") + '"></button>');
+        $(this).prepend('<button class="changeImageButton ' + buttonModifier + '" title="' + localizationManager.getText("EditTab.Image.ChangeImage") + '"></button>');
+
+        var img = $(this).find('img');
+        if (CreditsAreRelevantForImage(img)) {
+            $(this).prepend('<button class="editMetadataButton ' + buttonModifier + '" title="' + localizationManager.getText("EditTab.Image.EditMetadata") + '"></button>');
+        }
+
+        $(this).addClass('hoverUp');
+    })
+    .mouseleave(function () {
+        $(this).removeClass('hoverUp');
+        $(this).find(".changeImageButton").each(function () {
+            $(this).remove()
+        });
+        $(this).find(".pasteImageButton").each(function () {
+            $(this).remove()
+        });
+        $(this).find(".editMetadataButton").each(function () {
+            if (!$(this).hasClass('imgMetadataProblem')) {
+                $(this).remove()
+            }
+        });
+    });
+}
+
+function CreditsAreRelevantForImage(img) {
+    return $(img).attr('src').toLowerCase().indexOf('placeholder') == -1; //don't offer to edit placeholder credits
+}
+
+//While the actual metadata is embedded in the images (Bloom/palaso does that), Bloom sticks some metadata in data-* attributes
+// so that we can easily & quickly get to the here.
+function SetOverlayForImagesWithoutMetadata(container) {
+    $(container).find(".bloom-imageContainer").each(function () {
+        var img = $(this).find('img');
+        if (!CreditsAreRelevantForImage(img)) {
+            return;
+        }
+        var container = $(this);
+
+        UpdateOverlay(container, img);
+
+        //and if the bloom program changes these values (i.e. the user changes them using bloom), I
+        //haven't figured out a way (apart from polling) to know that. So for now I'm using a hack
+        //where Bloom calls click() on the image when it wants an update, and we detect that here.
+        $(img).click(function () {
+            UpdateOverlay(container, img);
+        });
+    });
+}
+
+function UpdateOverlay(container, img) {
+
+    $(container).find(".imgMetadataProblem").each(function () {
+        $(this).remove()
+    });
+
+    //review: should we also require copyright, illustrator, etc? In many contexts the id of the work-for-hire illustrator isn't available
+    var copyright = $(img).attr('data-copyright');
+    if (!copyright || copyright.length == 0) {
+
+        var buttonModifier = "largeImageButton";
+        if ($(container).height() < 80) {
+            buttonModifier = 'smallImageButton';
+        }
+
+        $(container).prepend("<button class='editMetadataButton imgMetadataProblem " + buttonModifier + "' title='Image is missing information on Credits, Copyright, or License'></button>");
+    }
+}
+
+// Instead of "missing", we want to show it in the right ui language. We also want the text
+// to indicate that it might not be missing, just didn't load (this happens on slow machines)
+// TODO: internationalize
+function SetAlternateTextOnImages(element) {
+    if ($(element).attr('src').length > 0) { //don't show this on the empty license image when we don't know the license yet
+        var nameWithoutQueryString = $(element).attr('src').split("?")[0];
+        $(element).attr('alt', 'This picture, ' + nameWithoutQueryString + ', is missing or was loading too slowly.');
+    } else {
+        $(element).attr('alt', '');//don't be tempted to show something like a '?' unless you fix the result when you have a custom book license on top of that '?'
+    }
+}
+
+function SetupResizableElement(element) {
+    $(element).mouseenter(
+        function () {
+            $(this).addClass("ui-mouseOver")
+        }).mouseleave(function () {
+            $(this).removeClass("ui-mouseOver")
+        });
+    var childImgContainer = $(element).find(".bloom-imageContainer");
+    // A Picture Dictionary Word-And-Image
+    if ($(childImgContainer).length > 0) {
+        /* The case here is that the thing with this class actually has an
+         inner image, as is the case for the Picture Dictionary.
+         The key, non-obvious, difficult requirement is keeping the text below
+         a picture dictionary item centered underneath the image.  I'd be
+         surprised if this wasn't possible in CSS, but I'm not expert enough.
+         So, I switched from having the image container be resizable, to having the
+         whole div (image+headwords) be resizable, then use the "alsoResize"
+         parameter to make the imageContainer resize.  Then, in order to make
+         the image resize in real-time as you're dragging, I use the "resize"
+         event to scale the image up proportionally (and centered) inside the
+         newly resized container.
+         */
+        var img = $(childImgContainer).find("img");
+        $(element).resizable({
+            handles: 'nw, ne, sw, se',
+            containment: "parent",
+            alsoResize: childImgContainer,
+            resize: function (event, ui) {
+                img.scaleImage({ scale: "fit" })
+            }
+        });
+        return $(element);
+    }
+        //An Image Container div (which must have an inner <img>
+    else if ($(element).hasClass('bloom-imageContainer')) {
+        var img = $(element).find("img");
+        $(element).resizable({
+            handles: 'nw, ne, sw, se',
+            containment: "parent",
+            resize: function (event, ui) {
+                img.scaleImage({ scale: "fit" })
+            }
+        });
+    }
+        // some other kind of resizable
+    else {
+        $(element).resizable({
+            handles: 'nw, ne, sw, se',
+            containment: "parent",
+            stop: ResizeUsingPercentages,
+            start: function (e, ui) {
+                if ($(ui.element).css('top') == '0px' && $(ui.element).css('left') == '0px') {
+                    $(ui.element).data('doRestoreRelativePosition', 'true');
+                }
+            }
+        });
+    }
+}
+
+//jquery resizable normally uses pixels. This makes it use percentages, which are mor robust across page size/orientation changes
+function ResizeUsingPercentages(e, ui) {
+    var parent = ui.element.parent();
+    ui.element.css({
+        width: ui.element.width() / parent.width() * 100 + "%",
+        height: ui.element.height() / parent.height() * 100 + "%"
+    });
+
+    //after any resize jquery adds an absolute position, which we don't want unless the user has resized
+    //so this removes it, unless we previously noted that the user had moved it
+    if ($(ui.element).data('doRestoreRelativePosition')) {
+        ui.element.css({
+            position: '',
+            top: '',
+            left: ''
+        });
+    }
+    $(ui.element).removeData('hadPreviouslyBeenRelocated');
+}
