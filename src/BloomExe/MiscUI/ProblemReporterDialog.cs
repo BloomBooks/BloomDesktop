08236bf39c0cff7c5f2312a5784e3abd926d7858
JohnThomson <john_thomson@sil.org>
2015-06-15 14:39:56 -0500

    BL-2248, provide email for private reports

--- src/BloomExe/MiscUI/ProblemReporterDialog.cs
+++ src/BloomExe/MiscUI/ProblemReporterDialog.cs
@@ -489,12 +489,13 @@ namespace Bloom.MiscUI
 
 		private void _privacyLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
 		{
+			var domain = "bloomlibrary.org";
 			var msg = LocalizationManager.GetString("ReportProblemDialog.PrivacyNotice",
 				@"If you don't care who reads your report, you can skip this notice.
 
 Your report goes into our issue tracking system and will be visible via the web. We will obfuscate your address, so automatic spammers are unlikely to get your email this way.
 
-So if you have something private to say, please send it to one of the developers privately with a note that you want your report to stay private.");
+So if you have something private to say, please email it to private@" + domain + ".");
 			MessageBox.Show(this, msg, _privacyLabel.Text, MessageBoxButtons.OK);
 		}
 	}
