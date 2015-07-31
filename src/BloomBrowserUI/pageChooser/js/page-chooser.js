b1afc404940fdc6d04eb969d10096218bfb38b0c
Gordon Martin <gordon_martin@sil.org>
2015-07-31 11:54:52 -0700

    Make PageChooser a class in .ts
    * added message eventlistener to receive messages from dialog frame
    * added fireCSharpEvent capability
    * removed testButton handler

--- src/BloomBrowserUI/pageChooser/js/page-chooser.js
+++ src/BloomBrowserUI/pageChooser/js/page-chooser.js
@@ -1,28 +1,41 @@
+/// <reference path="../../bookEdit/js/getIframeChannel.ts" />
 /// <reference path="../../lib/jquery.d.ts" />
+function pageWindow() {
+    if (window.parent)
+        return window.parent.document.getElementById('page').contentWindow;
+}
+window.addEventListener('message', process_IO_Message, false);
+var logic;
+function process_IO_Message(event) {
+    var params = event.data.split("\n");
+    switch (params[0]) {
+        case 'AddSelectedPage':
+            if (logic)
+                logic.addPageClickHandler();
+            else
+                alert("received AddSelectedPage message before PageChooser was created");
+            return;
+        case 'Data':
+            logic = new PageChooser(params[1]);
+            logic.LoadInstalledCollections();
+            return;
+        default:
+    }
+}
 // this version of the test string may be useful later testing more than one template collection
 //var JSONTestString = "[{ \"templateBookUrl\": \"../../../DistFiles/factoryCollections/Templates/Basic Book/Basic Book.htm\" }, { \"templateBookUrl\": \"../../../DistFiles/factoryCollections/Templates/Basic Book/Basic Book.htm\" }]";
-var JSONTestString = "[{ \"templateBookUrl\": \"../localhost/C$/BloomDesktop/DistFiles/factoryCollections/Templates/Basic Book/Basic Book.htm\" }]";
+var JSONTestString = "[{ \"templateBookUrl\": \"bloom/localhost/C$/BloomDesktop/DistFiles/factoryCollections/Templates/Basic Book/Basic Book.htm\" }]";
 var PageChooser = (function () {
-    // Constructor
     function PageChooser(templateBookUrls) {
-        this._templateBookUrls = templateBookUrls;
-        this._selectedTemplatePage = undefined;
-        if (this._templateBookUrls != undefined) {
-            LoadInstalledCollections();
+        if (templateBookUrls) {
+            this._templateBookUrls = templateBookUrls;
         }
+        else {
+            console.log('Expected url in PageChooser ctor!');
+        }
+        this._selectedTemplatePage = undefined;
     }
-    $(document).ready(function () {
-        var addPage = $("#addPageButton");
-        addPage.prop("disabled", true);
-        addPage.click(function () {
-            addPageClickHandler();
-        }); // Add Page button click
-        // Comment out this function to disable testButton; or better yet just hide it in the css
-        $("#testButton").click(function () {
-            testButtonClickHandler();
-        }); // Test button click
-    }); // document ready
-    var thumbnailClickHandler = function (div) {
+    PageChooser.prototype.thumbnailClickHandler = function (div) {
         // 'div' is an .invisibleThumbCover
         // Mark any previously selected thumbnail as no longer selected
         if (this._selectedTemplatePage != undefined) {
@@ -32,47 +45,49 @@ var PageChooser = (function () {
         this._selectedTemplatePage = $(div).parent();
         $(this._selectedTemplatePage).addClass('ui-selected');
         // Display large preview
-        $('#previewCaption').text($('.templatePageCaption', this._selectedTemplatePage).text());
-        $('#previewCaption').attr('style', 'display: block;');
+        var caption = $('#previewCaption');
+        caption.text($('.templatePageCaption', this._selectedTemplatePage).text());
+        caption.attr('style', 'display: block;');
         $('#preview').attr('src', $(this._selectedTemplatePage).find('iframe').first().attr('src'));
-        $('#addPageButton').prop("disabled", false);
+        // TODO: disable the main dialog one! $( '#addPageButton' ).prop( "disabled", false );
     }; // thumbnailClickHandler
-    var addPageClickHandler = function () {
+    PageChooser.prototype.addPageClickHandler = function () {
         if (this._selectedTemplatePage == undefined || this._templateBookUrls == undefined) {
             return null;
         }
         // TODO: Add page to book here
         var pageId = $(this._selectedTemplatePage).find('iframe').first().attr('src');
-        console.log('Selected template page: ' + pageId);
-        console.log('Input urls: ' + this._templateBookUrls);
+        console.log('firing CSharp event - Selected template page: ' + pageId);
+        this.fireCSharpEvent('addPage', pageId);
         // Mark any previously selected thumbnail as no longer selected
         $(this._selectedTemplatePage).removeClass('ui-selected');
-    }; // AddPageClickHandler
-    var cancelButtonClickHandler = function () {
-        $(this).dialog("close");
-    };
-    var LoadInstalledCollections = function () {
+    }; // addPageClickHandler
+    PageChooser.prototype.LoadInstalledCollections = function () {
         // Save html sections that will get cloned later
         // there should only be one 'collection' at this point; a stub with one default template page
         var collectionHTML = $('.collection', document).first().clone();
         // there should only be the one default 'templatePage' at this point
-        var pageHTML = $('.templatePage', collectionHTML).first().clone();
-        console.log('Loading installed template pages...');
-        var collectionUrls = $.parseJSON(this._templateBookUrls);
+        var templatePageHTML = $('.templatePage', collectionHTML).first().clone();
+        var collectionUrls;
+        try {
+            collectionUrls = $.parseJSON(logic._templateBookUrls);
+        }
+        catch (e) {
+            console.log('Received bad template url: ' + e);
+        }
         if ($(collectionUrls).length > 0) {
             // Remove original stub section
             $('.outerCollectionContainer', document).empty();
             $.each(collectionUrls, function (index) {
-                console.log('  ' + (index + 1) + ' loading... ' + this['templateBookUrl']);
-                LoadCollection(this['templateBookUrl'], collectionHTML, pageHTML);
+                //console.log('  ' + (index + 1) + ' loading... ' + this['templateBookUrl'] );
+                logic.LoadCollection(this['templateBookUrl'], collectionHTML, templatePageHTML);
             });
             window.scrollTo(0, 0); // TODO: wrong window!
-            console.log('Available pages loaded.');
         }
     }; // LoadInstalledCollections
-    var LoadCollection = function (pageUrl, collectionHTML, pageHTML) {
+    PageChooser.prototype.LoadCollection = function (pageUrl, collectionHTML, templatePageHTML) {
         var request = $.get(pageUrl);
-        request.success(function (pageData) {
+        request.done(function (pageData) {
             // TODO: for now just grab the first book title, we may want to know which lang to grab eventually
             var dataBookArray = $("div[data-book='bookTitle']", pageData);
             var collectionTitle = $(dataBookArray.first()).text();
@@ -83,13 +98,13 @@ var PageChooser = (function () {
             // Grab all pages in this collection
             // N.B. normal selector syntax or .find() WON'T work here because pageData is not yet part of the DOM!
             var pages = $(pageData).filter(".bloom-page[id]");
-            LoadPagesFromCollection(collectionToAdd, pages, pageHTML, pageUrl);
+            logic.LoadPagesFromCollection(collectionToAdd, pages, templatePageHTML, pageUrl);
         }, "html");
-        request.error(function (jqXHR, textStatus, errorThrown) {
+        request.fail(function (jqXHR, textStatus, errorThrown) {
             console.log('There was a problem reading: ' + pageUrl + ' see documentation on : ' + jqXHR.status + ' ' + textStatus + ' ' + errorThrown);
         });
     }; // LoadCollection
-    var LoadPagesFromCollection = function (currentCollection, pageArray, pageHTML, url) {
+    PageChooser.prototype.LoadPagesFromCollection = function (currentCollection, pageArray, templatePageHTML, url) {
         if ($(pageArray).length < 1) {
             return;
         }
@@ -100,7 +115,7 @@ var PageChooser = (function () {
             var currentId = $(div).attr('id');
             // TODO: for now just grab the first page label, we may want to know which lang to grab eventually
             var pageTitle = $('.pageLabel', div).first().text();
-            var currentPageHTML = $(pageHTML).clone();
+            var currentPageHTML = $(templatePageHTML).clone();
             $('.templatePageCaption', currentPageHTML).first().text(pageTitle);
             $('iframe', currentPageHTML).attr('src', url + '#' + currentId);
             $('.innerCollectionContainer', currentCollection).append(currentPageHTML);
@@ -108,14 +123,22 @@ var PageChooser = (function () {
         // once the template pages are installed, attach click handler to them.
         $('.invisibleThumbCover', currentCollection).each(function (index, div) {
             $(div).click(function () {
-                thumbnailClickHandler(div);
+                logic.thumbnailClickHandler(div);
             }); // invisibleThumbCover click
         }); // each
     }; // LoadPagesFromCollection
-    var testButtonClickHandler = function () {
-        this._templateBookUrls = JSONTestString;
-        LoadInstalledCollections();
-    }; // testButtonClickHandler
+    /**
+     * Fires an event for C# to handle
+     * @param {String} eventName
+     * @param {String} eventData
+     */
+    PageChooser.prototype.fireCSharpEvent = function (eventName, eventData) {
+        //console.log('firing CSharp event: ' + eventName);
+        var event = new MessageEvent();
+        event.initEvent(eventName, true, true);
+        event.data = eventData;
+        pageWindow().dispatchEvent(event);
+    };
     return PageChooser;
 })();
 //# sourceMappingURL=page-chooser.js.map
--- src/BloomBrowserUI/pageChooser/js/page-chooser.ts
+++ src/BloomBrowserUI/pageChooser/js/page-chooser.ts
@@ -1,32 +1,56 @@
+/// <reference path="../../bookEdit/js/getIframeChannel.ts" />
 /// <reference path="../../lib/jquery.d.ts" />
+
+function pageWindow(): Window {
+    if (window.parent)
+        return (<HTMLIFrameElement>window.parent.document.getElementById('page')).contentWindow;
+}
+
+window.addEventListener('message', process_IO_Message, false);
+
+var logic;
+
+function process_IO_Message(event: MessageEvent): void {
+
+    var params = event.data.split("\n");
+
+    switch(params[0]) {
+        case 'AddSelectedPage':
+            if(logic)
+                logic.addPageClickHandler();
+            else
+                alert("received AddSelectedPage message before PageChooser was created");
+            return;
+
+        case 'Data':
+            logic = new PageChooser(params[1]);
+            logic.LoadInstalledCollections();
+            return;
+
+        default:
+    }
+}
 
 // this version of the test string may be useful later testing more than one template collection
 //var JSONTestString = "[{ \"templateBookUrl\": \"../../../DistFiles/factoryCollections/Templates/Basic Book/Basic Book.htm\" }, { \"templateBookUrl\": \"../../../DistFiles/factoryCollections/Templates/Basic Book/Basic Book.htm\" }]";
-var JSONTestString = "[{ \"templateBookUrl\": \"../localhost/C$/BloomDesktop/DistFiles/factoryCollections/Templates/Basic Book/Basic Book.htm\" }]";
+var JSONTestString = "[{ \"templateBookUrl\": \"bloom/localhost/C$/BloomDesktop/DistFiles/factoryCollections/Templates/Basic Book/Basic Book.htm\" }]";
 
-var PageChooser = (function () {
-    // Constructor
-    function PageChooser(templateBookUrls) {
-        this._templateBookUrls = templateBookUrls;
-        this._selectedTemplatePage = undefined;
-        if (this._templateBookUrls != undefined) {
-            LoadInstalledCollections();
+class PageChooser {
+
+    private _templateBookUrls : string;
+    private _selectedTemplatePage : JQuery;
+
+    constructor(templateBookUrls: string) {
+        if(templateBookUrls) {
+            this._templateBookUrls = templateBookUrls;
+        } else {
+            console.log('Expected url in PageChooser ctor!');
         }
+
+        this._selectedTemplatePage = undefined;
     }
 
-    $( document ).ready(function() {
-        var addPage = $( "#addPageButton" );
-        addPage.prop( "disabled", true );
-        addPage.click( function() {
-            addPageClickHandler();
-        }); // Add Page button click
-        // Comment out this function to disable testButton; or better yet just hide it in the css
-        $( "#testButton" ).click( function() {
-            testButtonClickHandler();
-        }); // Test button click
-    }); // document ready
-
-    var thumbnailClickHandler = function( div ) {
+    thumbnailClickHandler( div ) : void {
         // 'div' is an .invisibleThumbCover
         // Mark any previously selected thumbnail as no longer selected
         if (this._selectedTemplatePage != undefined) {
@@ -36,51 +60,52 @@ var PageChooser = (function () {
         this._selectedTemplatePage = $( div ).parent();
         $(this._selectedTemplatePage).addClass('ui-selected');
         // Display large preview
-        $( '#previewCaption').text($('.templatePageCaption', this._selectedTemplatePage).text());
-        $('#previewCaption').attr('style', 'display: block;');
+        var caption = $( '#previewCaption');
+        caption.text($('.templatePageCaption', this._selectedTemplatePage).text());
+        caption.attr('style', 'display: block;');
         $( '#preview' ).attr( 'src', $( this._selectedTemplatePage ).find('iframe').first().attr('src'));
-        $( '#addPageButton' ).prop( "disabled", false );
-    }; // thumbnailClickHandler
+        // TODO: disable the main dialog one! $( '#addPageButton' ).prop( "disabled", false );
+    } // thumbnailClickHandler
 
-    var addPageClickHandler = function() {
+    addPageClickHandler() : void {
         if (this._selectedTemplatePage == undefined || this._templateBookUrls == undefined) {
             return null;
         }
         // TODO: Add page to book here
         var pageId = $(this._selectedTemplatePage).find('iframe').first().attr('src');
-        console.log('Selected template page: ' + pageId);
-        console.log('Input urls: ' + this._templateBookUrls);
+        console.log('firing CSharp event - Selected template page: ' + pageId);
+        this.fireCSharpEvent('addPage', pageId);
         // Mark any previously selected thumbnail as no longer selected
         $(this._selectedTemplatePage).removeClass('ui-selected');
-    }; // AddPageClickHandler
+    } // addPageClickHandler
 
-    var cancelButtonClickHandler = function() {
-        $(this).dialog("close");
-    };
-
-    var LoadInstalledCollections = function() {
+    LoadInstalledCollections() : void {
         // Save html sections that will get cloned later
         // there should only be one 'collection' at this point; a stub with one default template page
         var collectionHTML =  $('.collection', document).first().clone();
         // there should only be the one default 'templatePage' at this point
-        var pageHTML = $( '.templatePage', collectionHTML).first().clone();
-        console.log('Loading installed template pages...');
-        var collectionUrls = $.parseJSON( this._templateBookUrls );
+        var templatePageHTML = $( '.templatePage', collectionHTML).first().clone();
+        var collectionUrls;
+        try {
+            collectionUrls = $.parseJSON( logic._templateBookUrls );
+        } catch (e) {
+            console.log('Received bad template url: ' + e);
+        }
         if ($( collectionUrls).length > 0) {
             // Remove original stub section
             $('.outerCollectionContainer', document).empty();
             $.each(collectionUrls, function (index) {
-                console.log('  ' + (index + 1) + ' loading... ' + this['templateBookUrl'] );
-                LoadCollection( this['templateBookUrl'], collectionHTML, pageHTML );
+                //console.log('  ' + (index + 1) + ' loading... ' + this['templateBookUrl'] );
+                logic.LoadCollection( this['templateBookUrl'], collectionHTML, templatePageHTML );
             });
             window.scrollTo(0,0); // TODO: wrong window!
-            console.log('Available pages loaded.');
+            //console.log('Available pages loaded.');
         }
-    }; // LoadInstalledCollections
+    } // LoadInstalledCollections
 
-    var LoadCollection = function( pageUrl, collectionHTML, pageHTML ) {
+    LoadCollection( pageUrl, collectionHTML, templatePageHTML ) : void {
         var request = $.get( pageUrl);
-        request.success( function( pageData ) {
+        request.done( function( pageData ) {
             // TODO: for now just grab the first book title, we may want to know which lang to grab eventually
             var dataBookArray = $( "div[data-book='bookTitle']", pageData );
             var collectionTitle = $( dataBookArray.first() ).text();
@@ -91,15 +116,15 @@ var PageChooser = (function () {
             // Grab all pages in this collection
             // N.B. normal selector syntax or .find() WON'T work here because pageData is not yet part of the DOM!
             var pages = $( pageData).filter( ".bloom-page[id]" );
-            LoadPagesFromCollection(collectionToAdd, pages, pageHTML, pageUrl );
+            logic.LoadPagesFromCollection(collectionToAdd, pages, templatePageHTML, pageUrl );
         }, "html");
-        request.error( function(jqXHR, textStatus, errorThrown) {
+        request.fail( function(jqXHR, textStatus, errorThrown) {
             console.log('There was a problem reading: ' + pageUrl + ' see documentation on : ' +
                 jqXHR.status + ' ' + textStatus + ' ' + errorThrown);
         });
-    }; // LoadCollection
+    } // LoadCollection
 
-    var LoadPagesFromCollection = function( currentCollection, pageArray, pageHTML, url ) {
+    LoadPagesFromCollection( currentCollection, pageArray, templatePageHTML, url ) : void {
         if ($(pageArray).length < 1) {
             return;
         }
@@ -110,7 +135,7 @@ var PageChooser = (function () {
             var currentId = $(div).attr('id');
             // TODO: for now just grab the first page label, we may want to know which lang to grab eventually
             var pageTitle = $('.pageLabel', div).first().text();
-            var currentPageHTML = $(pageHTML).clone();
+            var currentPageHTML = $(templatePageHTML).clone();
             $('.templatePageCaption', currentPageHTML).first().text(pageTitle);
             $('iframe', currentPageHTML).attr('src', url + '#' + currentId);
             $('.innerCollectionContainer', currentCollection).append(currentPageHTML);
@@ -118,15 +143,21 @@ var PageChooser = (function () {
         // once the template pages are installed, attach click handler to them.
         $('.invisibleThumbCover', currentCollection).each(function(index, div) {
             $(div).click(function() {
-                thumbnailClickHandler(div);
+                logic.thumbnailClickHandler(div);
             }); // invisibleThumbCover click
         }); // each
-    }; // LoadPagesFromCollection
+    } // LoadPagesFromCollection
 
-    var testButtonClickHandler = function() {
-        this._templateBookUrls = JSONTestString;
-        LoadInstalledCollections();
-    }; // testButtonClickHandler
-
-    return PageChooser;
-})();
+    /**
+     * Fires an event for C# to handle
+     * @param {String} eventName
+     * @param {String} eventData
+     */
+    fireCSharpEvent(eventName, eventData) : void {
+        //console.log('firing CSharp event: ' + eventName);
+        var event = new MessageEvent();
+        event.initEvent(eventName, true, true);
+        event.data = eventData;
+        pageWindow().dispatchEvent(event);
+    }
+}
