ef840effe8e7fe92853855741ddb35eb16c316f6
John Hatton <hattonjohn@gmail.com>
2015-08-07 15:55:44 -0600

    Add a little MarkDownTextBox() control for rendering static Markdown

---
+++ src/BloomExe/MiscUI/MarkDownTextBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Gecko;
+using MarkdownSharp;
+using Palaso.UI.WindowsForms.Extensions;
+
+namespace Bloom.MiscUI
+{
+	public partial class MarkDownTextBox : UserControl
+	{
+		private  string _markdown="Need to set the property \"MarkDownText\"";
+		
+		public MarkDownTextBox()
+		{
+			InitializeComponent();
+			if(this.DesignModeAtAll())
+				return;
+		}
+
+		private void MarkDownTextBox_Load(object sender, EventArgs e)
+		{
+			if(this.DesignModeAtAll())
+				return;
+			 var markdownTransformer = new Markdown();
+			_htmlLabel.HTML = markdownTransformer.Transform(_markdown);
+		}
+
+		[Browsable(true), Category("MarkDownText")]
+		public string MarkDownText
+		{
+			get { return _markdown; }
+			set{_markdown = value;}
+		}
+	}
+}
