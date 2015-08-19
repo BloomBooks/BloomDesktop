e975840ea79678e737a245b82b886c0a25965787
John Hatton <hattonjohn@gmail.com>
2015-08-18 20:23:55 -0600

    Rework "Add Page" button

--- src/BloomExe/Edit/PageListView.Designer.cs
+++ src/BloomExe/Edit/PageListView.Designer.cs
@@ -30,12 +30,13 @@
         {
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageListView));
+			System.Drawing.Imaging.ImageAttributes imageAttributes2 = new System.Drawing.Imaging.ImageAttributes();
 			this._pageThumbnails = new System.Windows.Forms.ImageList(this.components);
 			this._pagesLabel = new System.Windows.Forms.Label();
 			this._L10NSharpExtender = new L10NSharp.UI.L10NSharpExtender(this.components);
-			this._addPageButton = new Bloom.Edit.GraphicButton();
-			this._thumbNailList = new Bloom.Edit.WebThumbNailList();
+			this._addPageButton = new Palaso.UI.WindowsForms.Widgets.BitmapButton();
 			this._pageControlsPanel = new System.Windows.Forms.Panel();
+			this._thumbNailList = new Bloom.Edit.WebThumbNailList();
 			((System.ComponentModel.ISupportInitialize)(this._L10NSharpExtender)).BeginInit();
 			this._pageControlsPanel.SuspendLayout();
 			this.SuspendLayout();
@@ -69,23 +70,53 @@
 			// 
 			// _addPageButton
 			// 
-			this._addPageButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this._addPageButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._addPageButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this._addPageButton.BorderColor = System.Drawing.Color.Transparent;
+			this._addPageButton.DisabledTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(87)))), ((int)(((byte)(87)))));
 			this._addPageButton.FlatAppearance.BorderSize = 0;
 			this._addPageButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this._addPageButton.ForeColor = System.Drawing.Color.White;
+			this._addPageButton.FocusRectangleEnabled = true;
+			this._addPageButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._addPageButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(129)))), ((int)(((byte)(146)))));
+			this._addPageButton.Image = null;
 			this._addPageButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+			this._addPageButton.ImageAttributes = imageAttributes2;
+			this._addPageButton.ImageBorderColor = System.Drawing.Color.Transparent;
+			this._addPageButton.ImageBorderEnabled = false;
+			this._addPageButton.ImageDropShadow = false;
+			this._addPageButton.ImageFocused = null;
+			this._addPageButton.ImageInactive = null;
+			this._addPageButton.ImageMouseOver = null;
+			this._addPageButton.ImageNormal = global::Bloom.Properties.Resources.AddPageButton;
+			this._addPageButton.ImagePressed = null;
+			this._addPageButton.InnerBorderColor = System.Drawing.Color.Transparent;
+			this._addPageButton.InnerBorderColor_Focus = System.Drawing.Color.LightBlue;
+			this._addPageButton.InnerBorderColor_MouseOver = System.Drawing.Color.Gold;
 			this._L10NSharpExtender.SetLocalizableToolTip(this._addPageButton, null);
 			this._L10NSharpExtender.SetLocalizationComment(this._addPageButton, null);
-			this._L10NSharpExtender.SetLocalizingId(this._addPageButton, "EditTab.AddPageButton");
-			this._addPageButton.Location = new System.Drawing.Point(52, 17);
+			this._L10NSharpExtender.SetLocalizingId(this._addPageButton, "EditTab._undoButton");
+			this._addPageButton.Location = new System.Drawing.Point(23, 17);
 			this._addPageButton.Name = "_addPageButton";
-			this._addPageButton.Size = new System.Drawing.Size(67, 41);
-			this._addPageButton.TabIndex = 0;
+			this._addPageButton.OffsetPressedContent = true;
+			this._addPageButton.Size = new System.Drawing.Size(68, 60);
+			this._addPageButton.StretchImage = false;
+			this._addPageButton.TabIndex = 10;
 			this._addPageButton.Text = "Add Page";
 			this._addPageButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-			this._addPageButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+			this._addPageButton.TextDropShadow = false;
 			this._addPageButton.UseVisualStyleBackColor = false;
 			this._addPageButton.Click += new System.EventHandler(this._addPageButton_Click);
+			// 
+			// _pageControlsPanel
+			// 
+			this._pageControlsPanel.Controls.Add(this._addPageButton);
+			this._pageControlsPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this._pageControlsPanel.Location = new System.Drawing.Point(0, 199);
+			this._pageControlsPanel.Name = "_pageControlsPanel";
+			this._pageControlsPanel.Size = new System.Drawing.Size(137, 80);
+			this._pageControlsPanel.TabIndex = 3;
 			// 
 			// _thumbNailList
 			// 
@@ -102,15 +133,6 @@
 			this._thumbNailList.RelocatePageEvent = null;
 			this._thumbNailList.Size = new System.Drawing.Size(137, 179);
 			this._thumbNailList.TabIndex = 4;
-			// 
-			// _pageControlsPanel
-			// 
-			this._pageControlsPanel.Controls.Add(this._addPageButton);
-			this._pageControlsPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this._pageControlsPanel.Location = new System.Drawing.Point(0, 199);
-			this._pageControlsPanel.Name = "_pageControlsPanel";
-			this._pageControlsPanel.Size = new System.Drawing.Size(137, 80);
-			this._pageControlsPanel.TabIndex = 3;
 			// 
 			// PageListView
 			// 
@@ -140,8 +162,8 @@
         private System.Windows.Forms.Label _pagesLabel;
         private System.Windows.Forms.ImageList _pageThumbnails;
 		private L10NSharp.UI.L10NSharpExtender _L10NSharpExtender;
-		private GraphicButton _addPageButton;
 		private WebThumbNailList _thumbNailList;
 		internal System.Windows.Forms.Panel _pageControlsPanel;
-    }
+		private Palaso.UI.WindowsForms.Widgets.BitmapButton _addPageButton;
+	}
 }
