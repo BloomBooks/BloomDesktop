11d685e5789270d26a6caade92516342be1080be
unknown <Thomson@LS-ThomsonJ.dallas.sil.org>
2015-12-16 11:41:33 -0600

    Add icon for talking book tool

--- src/BloomExe/Edit/TalkingBookTool.cs
+++ src/BloomExe/Edit/TalkingBookTool.cs
@@ -2,7 +2,6 @@
 {
 	class TalkingBookTool : ToolboxTool
 	{
-
 		public const string StaticToolId = "talkingBook";  // Avoid changing value; see ToolboxTool.JsonToolId
 		public override string ToolId { get { return StaticToolId; } }
 	}
