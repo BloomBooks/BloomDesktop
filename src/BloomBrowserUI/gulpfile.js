e98f34ba4e4cb8f9f0fd22d20fee46216d6ed21e
John Hatton <hattonjohn@gmail.com>
2016-01-11 09:43:14 -0700

    Add gulp for less and jade processing. Stop using webcompiler
    In visual studio, the default grunt task is now run before a build. In Visual Code, do CTRL-Shift-B for a build (or keep a "gulp watch" running).

---
+++ src/BloomBrowserUI/gulpfile.js
@@ -0,0 +1,35 @@
+/// <binding BeforeBuild='default' />
+var gulp = require('gulp');  
+var debug = require('gulp-debug');
+var less = require('gulp-less');
+var jade = require('gulp-jade');
+var path = require('path');
+var paths = {
+  less: ['./**/*.less',  '!./node_modules/**/*.less'],
+  jade: ['./**/*.jade',  '!./node_modules/**/*.jade']
+};
+//Currently we are putting all css's into the same directories as the less
+//A next step would be to set it out to \output but that will require 
+//changing where things are looked for at development time and on user machines,
+//and some build/installer modifications
+gulp.task('less', function () {
+  return gulp.src(paths.less)
+    .pipe(debug({title: 'less:'}))
+    .pipe(less())
+    .pipe(gulp.dest('./')); //drop all css's into the same dirs.
+});
+
+gulp.task('jade', function () {
+  return gulp.src(paths.jade)
+    .pipe(debug({title: 'jade:'}))
+    .pipe(jade())
+    .pipe(gulp.dest('./')); //drop all css's into the same dirs.
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.less, ['less']),
+  gulp.watch(paths.jade, ['jade']);
+});
+
+gulp.task('default', ['less', 'jade']);
