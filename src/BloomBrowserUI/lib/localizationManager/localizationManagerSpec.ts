025d91797a97a526c3dfe85e98db638465e6bd98
John Hatton <hattonjohn@gmail.com>
2016-02-08 07:25:27 -0700

    Stop ignoring individual generated files, they are now in output

--- src/BloomBrowserUI/lib/localizationManager/localizationManagerSpec.ts
+++ src/BloomBrowserUI/lib/localizationManager/localizationManagerSpec.ts
@@ -2,10 +2,6 @@
 ///<reference path="../../typings/bundledFromTSC.d.ts"/>
 import theOneLocalizationManager from './localizationManager';
 
-/*
-/// <reference path="../../lib/jquery-1.9.1.js"/>
-*/
-
 "use strict";
 
 describe("localizationManager", function () {
