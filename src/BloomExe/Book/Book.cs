41dbb69f6f25708e13ee117733dd05b3c2b8c4e5
DALLAS\Thomson <john_thomson@sil.org>
2016-02-24 16:27:44 -0600

    A possible fix for BL-3166, pushed for testing
    Problem does not occur on dev machines, so
    we need to experiment on Len's. If it seems to work,
    need to consider whether it will defeat the purpose
    of the SynchronizeDataItemsThroughoutDOM call.

--- src/BloomExe/Book/Book.cs
+++ src/BloomExe/Book/Book.cs
@@ -785,6 +785,9 @@ namespace Bloom.Book
 		private void BringBookUpToDate(HtmlDom bookDOM /* may be a 'preview' version*/, IProgress progress)
 		{
 			progress.WriteStatus("Updating Front/Back Matter...");
+			// Nothing in the update process should change the license info, so save what is current before we mess with
+			// anything (may fix BL-3166).
+			var licenseMetadata = GetLicenseMetadata();
 			BringXmatterHtmlUpToDate(bookDOM);
 
 			progress.WriteStatus("Gathering Data...");
@@ -811,8 +814,8 @@ namespace Bloom.Book
 				var bd = new BookData(bookDOM, _collectionSettings, UpdateImageMetadataAttributes);
 				bd.SynchronizeDataItemsThroughoutDOM();
 			}
-			// get any license info into the json
-			BookCopyrightAndLicense.SetMetadata(GetLicenseMetadata(), bookDOM, FolderPath, CollectionSettings);
+			// get any license info into the json and restored in the replaced front matter.
+			BookCopyrightAndLicense.SetMetadata(licenseMetadata, bookDOM, FolderPath, CollectionSettings);
 
 			bookDOM.RemoveMetaElement("bloomBookLineage", () => BookInfo.BookLineage, val => BookInfo.BookLineage = val);
 			bookDOM.RemoveMetaElement("bookLineage", () => BookInfo.BookLineage, val => BookInfo.BookLineage = val);
