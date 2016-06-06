a4697378927a7ee03873ccefcd3829c7786ef5c9
John Hatton <hattonjohn@gmail.com>
2016-06-06 07:42:00 -0600

    Fix BL3477 Longpress Broken

---
+++ src/BloomExe/web/controllers/KeybordingConfigAPI.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+using Bloom.Api;
+
+namespace Bloom.web.controllers
+{
+	class KeybordingConfigApi
+	{
+		public void RegisterWithServer(EnhancedImageServer server)
+		{
+			server.RegisterEndpointHandler("keyboarding/useLongPress", (ApiRequest request) => 
+			{
+				//detect if some keyboarding system is active, e.g. KeyMan. If it is, don't enable LongPress
+				var form = Application.OpenForms.Cast<Form>().Last();
+				request.ReplyWithText(SIL.Windows.Forms.Keyboarding.KeyboardController.IsFormUsingInputProcessor(form)?"false":"true");
+			});
+		}
+	}
+}
