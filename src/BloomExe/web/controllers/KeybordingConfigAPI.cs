862f0e0d2ecb61a86faa9100469422705f8bf622
John Hatton <hattonjohn@gmail.com>
2016-07-29 10:11:53 -0600

    Fix BL-3614 Crash while checking for keyman
    I wasn't able to reproduce the crash, but I made it not require UI thread and catch any errors and report them passively.

--- src/BloomExe/web/controllers/KeybordingConfigAPI.cs
+++ src/BloomExe/web/controllers/KeybordingConfigAPI.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows.Forms;
 using Bloom.Api;
 
@@ -8,12 +9,22 @@ namespace Bloom.web.controllers
 	{
 		public void RegisterWithServer(EnhancedImageServer server)
 		{
-			server.RegisterEndpointHandler("keyboarding/useLongPress", (ApiRequest request) => 
+			server.RegisterEndpointHandler("keyboarding/useLongPress", (ApiRequest request) =>
 			{
-				//detect if some keyboarding system is active, e.g. KeyMan. If it is, don't enable LongPress
-				var form = Application.OpenForms.Cast<Form>().Last();
-				request.ReplyWithText(SIL.Windows.Forms.Keyboarding.KeyboardController.IsFormUsingInputProcessor(form)?"false":"true");
-			});
+				try
+				{
+					//detect if some keyboarding system is active, e.g. KeyMan. If it is, don't enable LongPress
+					var form = Application.OpenForms.Cast<Form>().Last();
+					request.ReplyWithText(SIL.Windows.Forms.Keyboarding.KeyboardController.IsFormUsingInputProcessor(form)
+						? "false"
+						: "true");
+				}
+				catch(Exception error)
+				{
+					request.ReplyWithText("true"); // This is arbitrary. I don't know if it's better to assume keyman, or not.
+					NonFatalProblem.Report(ModalIf.None, PassiveIf.All, "Error checking for keyman","", error);
+				}
+			}, handleOnUiThread:false);
 		}
 	}
 }
