eca361993663b04b0256dcaa20e2ac76384bb969
John Hatton <hattonjohn@gmail.com>
2017-03-20 11:38:50 -0600

    Don't copy ReadMeImages into new books

--- src/BloomExe/Book/Book.cs
+++ src/BloomExe/Book/Book.cs
@@ -53,6 +53,7 @@ namespace Bloom.Book
 
 		public event EventHandler ContentsChanged;
 		private readonly BookData _bookData;
+		public const string ReadMeImagesFolderName = "ReadMeImages";
 
 		//for moq'ing only
 		public Book()
