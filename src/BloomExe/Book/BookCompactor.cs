dadf6d50eb8f13a8f3b6f43dc465e1cf463d78b0
andrew-polk <andrew_polk@sil.org>
2017-05-17 10:20:18 -0400

    Add BookCompactor (BL-4647)

---
+++ src/BloomExe/Book/BookCompactor.cs
@@ -0,0 +1,24 @@
+﻿using SIL.IO;
+
+namespace Bloom.Book
+{
+	public class BookCompactor
+	{
+		public const string ExtensionForDeviceBloomBook = ".bloomd";
+
+		public static string CompactBookForDevice(Book book)
+		{
+			book.Save();
+
+			using (var tempFile = TempFile.WithFilenameInTempFolder(book.Title + ExtensionForDeviceBloomBook))
+			{
+				var zip = new BloomZipFile(tempFile.Path);
+				zip.AddDirectory(book.FolderPath);
+				zip.Save();
+
+				tempFile.Detach();
+				return tempFile.Path;
+			}
+		}
+	}
+}
---
+++ src/BloomTests/Book/BookCompactorTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Linq;
+using Bloom.Book;
+using ICSharpCode.SharpZipLib.Zip;
+using NUnit.Framework;
+
+namespace BloomTests.Book
+{
+	class BookCompactorTests : BookTestsBase
+	{
+		[Test]
+		public void CompactBookForDevice_FileNameIsCorrect()
+		{
+			var testBook = CreateBook();
+
+			var pathToCompactedBook = BookCompactor.CompactBookForDevice(testBook);
+			Assert.AreEqual(testBook.Title + BookCompactor.ExtensionForDeviceBloomBook, Path.GetFileName(pathToCompactedBook));
+		}
+
+		[Test]
+		public void CompactBookForDevice_ContainsCorrectNumberOfFiles()
+		{
+			var testBook = CreateBook();
+			var bookDirInfo = new DirectoryInfo(testBook.FolderPath);
+
+			ZipFile zip = new ZipFile(BookCompactor.CompactBookForDevice(testBook));
+			Assert.True(zip.Count > 0);
+			Assert.AreEqual(bookDirInfo.EnumerateFiles().Count(), zip.Count);
+		}
+	}
+}
