7cf861ce8fda8ba8293f8db14e50469723f3812e
Stephen McConnel <stephen_mcconnel@sil.org>
2017-06-15 15:58:15 -0500

    Reduce images for .bloomd books (BL-4645)

--- src/BloomTests/Book/BookCompressorTests.cs
+++ src/BloomTests/Book/BookCompressorTests.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using Bloom.Book;
 using ICSharpCode.SharpZipLib.Zip;
 using NUnit.Framework;
+using SIL.IO;
+using SIL.Windows.Forms.ClearShare;
 
 namespace BloomTests.Book
 {
@@ -26,6 +28,106 @@ namespace BloomTests.Book
 			ZipFile zip = new ZipFile(BookCompressor.CompressBookForDevice(testBook));
 			Assert.True(zip.Count > 0);
 			Assert.AreEqual(bookDirInfo.EnumerateFiles().Count(), zip.Count);
+		}
+
+		// re-use the images from another test
+		private const string _pathToTestImages = "src/BloomTests/ImageProcessing/images";
+
+		[Test]
+		public void GetBytesOfReducedImage_ReducesImageIfAppropriate()
+		{
+			// bird.png:                   PNG image data, 274 x 300, 8-bit/color RGBA, non-interlaced
+			// man.jpg:                    JPEG image data, JFIF standard 1.01, aspect ratio, density 1x1, segment length 16, baseline, precision 8, 118x154, frames 3
+			// shirtWithTransparentBg.png: PNG image data, 2208 x 2400, 8-bit/color RGBA, non-interlaced
+			// LakePendOreille.jpg:        JPEG image data, JFIF standard 1.01, aspect ratio, density 1x1, segment length 16,  baseline, ... precision 8, 3264x2448, frames 3
+
+			var path = SIL.IO.FileLocator.GetFileDistributedWithApplication(_pathToTestImages, "bird.png");
+			byte[] originalBytes = File.ReadAllBytes(path);
+			byte[] reducedBytes = BookCompressor.GetBytesOfReducedImage(path);
+			Assert.AreEqual(originalBytes, reducedBytes, "bird.png is already small enough (274x300)");
+			using (var tempFile = TempFile.WithExtension(Path.GetExtension(path)))
+			{
+				var oldMetadata = Metadata.FromFile(path);
+				RobustFile.WriteAllBytes(tempFile.Path, reducedBytes);
+				var newMetadata = Metadata.FromFile(tempFile.Path);
+				if (oldMetadata.IsEmpty)
+				{
+					Assert.IsTrue(newMetadata.IsEmpty);
+				}
+				else
+				{
+					Assert.IsFalse(newMetadata.IsEmpty);
+					Assert.AreEqual(oldMetadata.CopyrightNotice, newMetadata.CopyrightNotice, "copyright preserved for bird.png");
+					Assert.AreEqual(oldMetadata.Creator, newMetadata.Creator, "creator preserved for bird.png");
+					Assert.AreEqual(oldMetadata.License.ToString(), newMetadata.License.ToString(), "license preserved for bird.png");
+				}
+			}
+
+			path = SIL.IO.FileLocator.GetFileDistributedWithApplication(_pathToTestImages, "man.jpg");
+			originalBytes = File.ReadAllBytes(path);
+			reducedBytes = BookCompressor.GetBytesOfReducedImage(path);
+			Assert.AreEqual(originalBytes, reducedBytes, "man.jpg is already small enough (118x154)");
+			using (var tempFile = TempFile.WithExtension(Path.GetExtension(path)))
+			{
+				var oldMetadata = Metadata.FromFile(path);
+				RobustFile.WriteAllBytes(tempFile.Path, reducedBytes);
+				var newMetadata = Metadata.FromFile(tempFile.Path);
+				if (oldMetadata.IsEmpty)
+				{
+					Assert.IsTrue(newMetadata.IsEmpty);
+				}
+				else
+				{
+					Assert.IsFalse(newMetadata.IsEmpty);
+					Assert.AreEqual(oldMetadata.CopyrightNotice, newMetadata.CopyrightNotice, "copyright preserved for man.jpg");
+					Assert.AreEqual(oldMetadata.Creator, newMetadata.Creator, "creator preserved for man.jpg");
+					Assert.AreEqual(oldMetadata.License.ToString(), newMetadata.License.ToString(), "license preserved for man.jpg");
+				}
+			}
+
+			path = SIL.IO.FileLocator.GetFileDistributedWithApplication(_pathToTestImages, "shirtWithTransparentBg.png");
+			originalBytes = File.ReadAllBytes(path);
+			reducedBytes = BookCompressor.GetBytesOfReducedImage(path);
+			Assert.Greater(originalBytes.Length, reducedBytes.Length, "shirtWithTransparentBg.png is reduced from 2208x2400");
+			using (var tempFile = TempFile.WithExtension(Path.GetExtension(path)))
+			{
+				var oldMetadata = Metadata.FromFile(path);
+				RobustFile.WriteAllBytes(tempFile.Path, reducedBytes);
+				var newMetadata = Metadata.FromFile(tempFile.Path);
+				if (oldMetadata.IsEmpty)
+				{
+					Assert.IsTrue(newMetadata.IsEmpty);
+				}
+				else
+				{
+					Assert.IsFalse(newMetadata.IsEmpty);
+					Assert.AreEqual(oldMetadata.CopyrightNotice, newMetadata.CopyrightNotice, "copyright preserved for shirtWithTransparentBg.png");
+					Assert.AreEqual(oldMetadata.Creator, newMetadata.Creator, "creator preserved for shirtWithTransparentBg.png");
+					Assert.AreEqual(oldMetadata.License.ToString(), newMetadata.License.ToString(), "license preserved for shirtWithTransparentBg.png");
+				}
+			}
+
+			path = SIL.IO.FileLocator.GetFileDistributedWithApplication(_pathToTestImages, "LakePendOreille.jpg");
+			originalBytes = File.ReadAllBytes(path);
+			reducedBytes = BookCompressor.GetBytesOfReducedImage(path);
+			Assert.Greater(originalBytes.Length, reducedBytes.Length, "LakePendOreille.jpg is reduced from 3264x2448");
+			using (var tempFile = TempFile.WithExtension(Path.GetExtension(path)))
+			{
+				var oldMetadata = Metadata.FromFile(path);
+				RobustFile.WriteAllBytes(tempFile.Path, reducedBytes);
+				var newMetadata = Metadata.FromFile(tempFile.Path);
+				if (oldMetadata.IsEmpty)
+				{
+					Assert.IsTrue(newMetadata.IsEmpty);
+				}
+				else
+				{
+					Assert.IsFalse(newMetadata.IsEmpty);
+					Assert.AreEqual(oldMetadata.CopyrightNotice, newMetadata.CopyrightNotice, "copyright preserved for LakePendOreille.jpg");
+					Assert.AreEqual(oldMetadata.Creator, newMetadata.Creator, "creator preserved for LakePendOreille.jpg");
+					Assert.AreEqual(oldMetadata.License.ToString(), newMetadata.License.ToString(), "license preserved for LakePendOreille.jpg");
+				}
+			}
 		}
 	}
 }
