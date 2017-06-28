93fa6ae578a23ef70db8dcbc584ec76fb4c973c8
andrew-polk <andrew_polk@sil.org>
2017-06-28 16:38:05 -0400

    Add ability to push books via USB/MTP to Android (BL-4648)

---
+++ src/BloomExe/Communication/AndroidDeviceConnection.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using PodcastUtilities.PortableDevices;
+
+namespace Bloom.Communication
+{
+	/// <summary>
+	/// Handles non-UI functions of connecting to an Android device and working with the file system there
+	/// </summary>
+	class AndroidDeviceConnection
+	{
+		public event EventHandler OneApplicableDeviceFound;
+		public event EventHandler<List<string>> MoreThanOneApplicableDeviceFound;
+
+		private const string kBloomFolderOnDevice = "Bloom";
+		private IDevice _device;
+		private string _bloomFolderPath;
+		private bool _stopLookingForDevice;
+
+		/// <summary>
+		/// Attempt to establish a connection with a device which has the Bloom Reader app
+		/// </summary>
+		public void FindDevice()
+		{
+			_stopLookingForDevice = false;
+			var devices = EnumerateAllDevices();
+			GetOneDevice(devices);
+
+			while (!_stopLookingForDevice && _device == null)
+			{
+				Thread.Sleep(1000);
+				devices = EnumerateAllDevices();
+				GetOneDevice(devices);
+			}
+		}
+
+		public void StopFindingDevice()
+		{
+			_stopLookingForDevice = true;
+		}
+
+		public bool BookExists(string fileName)
+		{
+			if (_device == null || _bloomFolderPath == null)
+				throw new InvalidOperationException("Must connect before calling BookExists");
+
+			return _device.GetObjectFromPath(Path.Combine(_bloomFolderPath, fileName)) != null;
+		}
+
+		public void SendBook(string bloomdPath)
+		{
+			if (_device == null || _bloomFolderPath == null)
+				throw new InvalidOperationException("Must connect before calling SendBook");
+
+			var sourceStream = File.OpenRead(bloomdPath);
+			var targetStream = _device.OpenWrite(Path.Combine(_bloomFolderPath, Path.GetFileName(bloomdPath)), new FileInfo(bloomdPath).Length, true);
+			Copy(sourceStream, targetStream);
+		}
+
+		public string GetDeviceName()
+		{
+			if (_device == null)
+				throw new InvalidOperationException("Must connect before calling GetDeviceName");
+
+			return _device.Name;
+		}
+
+		private void Copy(Stream source, Stream destination)
+		{
+			const int kCopyBufferSize = 262144;
+
+			var buffer = new byte[kCopyBufferSize];
+
+			while (true)
+			{
+				var readSize = source.Read(buffer, 0, kCopyBufferSize);
+				if (readSize == 0)
+				{
+					break;
+				}
+
+				destination.Write(buffer, 0, readSize);
+			}
+
+			destination.Flush();
+		}
+
+		private static IEnumerable<IDevice> EnumerateAllDevices()
+		{
+			IDeviceManager manager = new DeviceManager();
+			return manager.GetAllDevices();
+		}
+
+		/// <summary>
+		/// Find the one with Bloom Reader installed (indicated by the presence of the Bloom directory
+		/// as a direct child of a root storage object)
+		/// </summary>
+		/// <param name="devices"></param>
+		/// <returns></returns>
+		private void GetOneDevice(IEnumerable<IDevice> devices)
+		{
+			List<IDevice> applicableDevices = new List<IDevice>();
+			foreach (var device in devices)
+			{
+				_bloomFolderPath = GetBloomFolderPath(device);
+				if (_bloomFolderPath != null)
+					applicableDevices.Add(device);
+			}
+
+			if (applicableDevices.Count == 1)
+			{
+				_device = applicableDevices[0];
+				EventHandler handler = OneApplicableDeviceFound;
+				handler?.Invoke(this, null);
+			}
+
+			_bloomFolderPath = null;
+			if (applicableDevices.Count > 1)
+			{
+				EventHandler<List<string>> handler = MoreThanOneApplicableDeviceFound;
+				handler?.Invoke(this, applicableDevices.Select(d => d.Name).ToList());
+			}
+		}
+
+		private string GetBloomFolderPath(IDevice device)
+		{
+			foreach (var rso in device.GetDeviceRootStorageObjects())
+			{
+				var possiblePath = Path.Combine(rso.Name, kBloomFolderOnDevice);
+				if (device.GetObjectFromPath(possiblePath) != null)
+					return possiblePath;
+			}
+			return null;
+		}
+	}
+}
---
+++ src/BloomExe/Publish/ReaderBookPublisher.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Bloom.Book;
+using Bloom.Communication;
+using SIL.Progress;
+
+namespace Bloom.Publish
+{
+	public class ReaderBookPublisher
+	{
+		public EventHandler Connected;
+
+		private readonly IProgress _progress;
+		private readonly AndroidDeviceConnection _androidDeviceConnection;
+		private bool _moreThanOneReported;
+
+		public ReaderBookPublisher(IProgress progress)
+		{
+			_progress = progress;
+			_androidDeviceConnection = new AndroidDeviceConnection();
+		}
+
+		/// <summary>
+		/// Attempt to connect to a device
+		/// </summary>
+		public void Connect()
+		{
+			var unableToConnectMessage = L10NSharp.LocalizationManager.GetString("Publish.ReaderBookPublisher.UnableToConnect",
+				"Unable to connect to any Android device which has Bloom Reader.");
+			try
+			{
+				_progress.WriteMessage(L10NSharp.LocalizationManager.GetString("Publish.ReaderBookPublisher.LookingForDevice",
+					"Looking for an Android device connected by USB cable and set up for MTP..."));
+
+				_androidDeviceConnection.OneApplicableDeviceFound += OneApplicableDeviceFound;
+				_androidDeviceConnection.MoreThanOneApplicableDeviceFound += MoreThanOneApplicableDeviceFound;
+
+				var backgroundWorker = new BackgroundWorker();
+				backgroundWorker.DoWork += (sender, args) => _androidDeviceConnection.FindDevice();
+				backgroundWorker.RunWorkerCompleted += (sender, args) =>
+				{
+					if (args.Error != null)
+					{
+						_androidDeviceConnection.StopFindingDevice();
+						_progress.WriteError(unableToConnectMessage);
+						SIL.Reporting.Logger.WriteError(args.Error);
+					}
+				};
+				backgroundWorker.RunWorkerAsync();
+			}
+			catch (Exception e)
+			{
+				_androidDeviceConnection.StopFindingDevice();
+				_progress.WriteError(unableToConnectMessage);
+				SIL.Reporting.Logger.WriteError(e);
+			}
+		}
+
+		private void OneApplicableDeviceFound(object sender, EventArgs args)
+		{
+			_progress.WriteMessage(string.Format(L10NSharp.LocalizationManager.GetString(
+				"Publish.ReaderBookPublisher.Connected",
+				"Connected to {0}...", "{0} is a device name"), _androidDeviceConnection.GetDeviceName()));
+
+			EventHandler handler = Connected;
+			handler?.Invoke(this, null);
+		}
+
+		private void MoreThanOneApplicableDeviceFound(object sender, List<string> deviceNames)
+		{
+			if (_moreThanOneReported)
+				return;
+
+			_moreThanOneReported = true;
+
+			_progress.WriteWarning(L10NSharp.LocalizationManager.GetString("Publish.ReaderBookPublisher.MoreThanOne",
+				"The following connected devices all have Bloom Reader installed. Please connect only one of these devices."));
+			foreach (var deviceName in deviceNames)
+			{
+				_progress.WriteWarning($"\t{deviceName}");
+			}
+		}
+
+		/// <summary>
+		/// Attempt to send the book to the device
+		/// </summary>
+		/// <param name="book"></param>
+		/// <returns>true if book was sent successfully</returns>
+		public bool SendBook(Book.Book book)
+		{
+			string generalFailureMessage = L10NSharp.LocalizationManager.GetString("Publish.ReaderBookPublisher.FailureToSend",
+				"An error occurred and the book was not sent to your Android device.");
+			try
+			{
+				var bookTitle = book.Title;
+				_progress.WriteMessage(string.Format(L10NSharp.LocalizationManager.GetString("Publish.ReaderBookPublisher.LookingForExisting",
+					"Looking for an existing \"{0}\"...", "{0} is a book title"), bookTitle));
+				var bookExistsOnDevice =
+					_androidDeviceConnection.BookExists(bookTitle + BookCompressor.ExtensionForDeviceBloomBook);
+
+				_progress.WriteMessage(string.Format(L10NSharp.LocalizationManager.GetString("Publish.ReaderBookPublisher.PackagingBook",
+					"Packaging \"{0}\" for use with Bloom Reader...", "{0} is a book title"), bookTitle));
+				var bloomdPath = BookCompressor.CompressBookForDevice(book);
+
+				if (bookExistsOnDevice)
+					_progress.WriteMessage(string.Format(L10NSharp.LocalizationManager.GetString("Publish.ReaderBookPublisher.ReplacingBook",
+						"Replacing existing \"{0}\"...", "{0} is a book title"), bookTitle));
+				else
+					_progress.WriteMessage(string.Format(L10NSharp.LocalizationManager.GetString("Publish.ReaderBookPublisher.SendingBook",
+						"Sending \"{0}\" to your Android device...", "{0} is a book title"), bookTitle));
+				_androidDeviceConnection.SendBook(bloomdPath);
+
+				if (_androidDeviceConnection.BookExists(bookTitle + BookCompressor.ExtensionForDeviceBloomBook))
+				{
+					_progress.WriteMessage(string.Format(L10NSharp.LocalizationManager.GetString(
+						"Publish.ReaderBookPublisher.BookSent",
+						"You can now read \"{0}\" in Bloom Reader!", "{0} is a book title"), bookTitle));
+					return true;
+				}
+				_progress.WriteError(generalFailureMessage);
+				return false;
+			}
+			catch (Exception e)
+			{
+				_progress.WriteError(generalFailureMessage);
+				SIL.Reporting.Logger.WriteError(e);
+				return false;
+			}
+		}
+	}
+}
