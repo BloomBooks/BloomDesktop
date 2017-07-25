cb516541dd97bef9fb60917e42801d7308a19a21
DALLAS\Thomson <john_thomson@sil.org>
2017-07-25 12:23:41 -0500

    BL-4754 Connect to Android using WiFi broadcast

---
+++ src/BloomExe/Publish/WiFiAdvertiser.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Bloom.Api;
+using ThirdParty.Json.LitJson;
+
+namespace Bloom.Publish
+{
+	/// <summary>
+	/// This class broadcasts a message over the network offering to download a book to any Android that wants it.
+	/// </summary>
+	public class WiFiAdvertiser : IDisposable
+	{
+		private UdpClient _client;
+		private Thread _thread;
+		private IPEndPoint _endPoint;
+		// The port on which we advertise.
+		// ChorusHub uses 5911 to advertise. Bloom looks for a port for its server at 8089 and 10 following ports.
+		// https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers shows a lot of ports in use around 8089,
+		// but nothing between 5900 and 5931. Decided to use a number similar to ChorusHub.
+		private const int Port = 5913; // must match port in BloomPlayer NewBookListenerService.startListenForUDPBroadcast
+		private string _currentIpAddress;
+		private byte[] _sendBytes; // Data we send in each advertisement packet
+
+		public void Start()
+		{
+			// The doc seems to indicate that EnableBroadcast is required for doing broadcasts.
+			// In practice it seems to be required on Mono but not on Windows.
+			// This may be fixed in a later version of one platform or the other, but please
+			// test both if tempted to remove it.
+			_client = new UdpClient
+			{
+				EnableBroadcast = true
+			};
+			_endPoint = new IPEndPoint(IPAddress.Parse("255.255.255.255"), Port);
+			_thread = new Thread(Work);
+			_thread.Start();
+		}
+
+		public string BookTitle;
+		public string BookVersion;
+
+		private void Work()
+		{
+			try
+			{
+				while (true)
+				{
+					UpdateAdvertisementBasedOnCurrentIpAddress();
+					_client.BeginSend(_sendBytes, _sendBytes.Length, _endPoint, SendCallback, _client);
+					Thread.Sleep(1000);
+				}
+			}
+			catch (ThreadAbortException)
+			{
+				//Progress.WriteVerbose("Advertiser Thread Aborting (that's normal)");
+				_client.Close();
+			}
+			catch (Exception error)
+			{
+				//EventLog.WriteEntry("Application", string.Format("Error in Advertiser: {0}", error.Message), EventLogEntryType.Error);
+			}
+		}
+		public static void SendCallback(IAsyncResult args)
+		{
+		}
+
+		/// <summary>
+		/// Since this migt not be a real "server", its ipaddress could be assigned dynamically,
+		/// and could change each time someone "wakes up the server laptop" each morning
+		/// </summary>
+		private void UpdateAdvertisementBasedOnCurrentIpAddress()
+		{
+			if (_currentIpAddress != GetLocalIpAddress())
+			{
+				_currentIpAddress = GetLocalIpAddress();
+				dynamic dataObj = new DynamicJson();
+				dataObj.Title = BookTitle;
+				dataObj.Version = BookVersion;
+
+				_sendBytes = Encoding.UTF8.GetBytes(dataObj.ToString());
+				//EventLog.WriteEntry("Application", "Serving at http://" + _currentIpAddress + ":" + ChorusHubOptions.MercurialPort, EventLogEntryType.Information);
+			}
+		}
+
+		private string GetLocalIpAddress()
+		{
+			string localIp = null;
+			var host = Dns.GetHostEntry(Dns.GetHostName());
+
+			foreach (var ipAddress in host.AddressList.Where(ipAddress => ipAddress.AddressFamily == AddressFamily.InterNetwork))
+			{
+				if (localIp != null)
+				{
+					if (host.AddressList.Length > 1)
+					{
+						//EventLog.WriteEntry("Application", "Warning: this machine has more than one IP address", EventLogEntryType.Warning);
+					}
+				}
+				localIp = ipAddress.ToString();
+			}
+			return localIp ?? "Could not determine IP Address!";
+		}
+
+		public void Stop()
+		{
+			if (_thread == null)
+				return;
+
+			//EventLog.WriteEntry("Application", "Advertiser Stopping...", EventLogEntryType.Information);
+			_thread.Abort();
+			_thread.Join(2 * 1000);
+			_thread = null;
+		}
+
+		public void Dispose()
+		{
+			Stop();
+		}
+	}
+}
--- src/BloomExe/web/controllers/PublishToAndroidApi.cs
+++ src/BloomExe/web/controllers/PublishToAndroidApi.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
 using Bloom.Collection;
 using Bloom.Publish;
 using Bloom.web;
@@ -14,6 +19,8 @@ namespace Bloom.Api
 		private const string kWebsocketStateId = "publish/android/state";
 		private readonly BloomReaderPublisher _bloomReaderPublisher;
 		private readonly BloomWebSocketServer _webSocketServer;
+		private WiFiAdvertiser _advertiser;
+		private UDPListener m_listener;
 
 		public PublishToAndroidApi(CollectionSettings collectionSettings, BloomWebSocketServer bloomWebSocketServer)
 		{
@@ -25,7 +32,8 @@ namespace Bloom.Api
 		public void RegisterWithServer(EnhancedImageServer server)
 		{
 			server.RegisterEndpointHandler(kApiUrlPart + "connectUsb/start", ConnectUsbStartHandler, true);
-			server.RegisterEndpointHandler(kApiUrlPart + "connectUsb/cancel", ConnectUsbCancelHandler, true);
+			server.RegisterEndpointHandler(kApiUrlPart + "connectWiFi/start", request => ConnectWiFiStartHandler(server, request), true);
+			server.RegisterEndpointHandler(kApiUrlPart + "connect/cancel", ConnectCancelHandler, true);
 			// Not yet
 			//server.RegisterEndpointHandler(kApiUrlPart + "connectWifi/start", ConnectWiFiStartHandler, true);
 			server.RegisterEndpointHandler(kApiUrlPart + "sendBook/start", request =>
@@ -48,9 +56,52 @@ namespace Bloom.Api
 			request.SucceededDoNotNavigate();
 		}
 
-		private void ConnectUsbCancelHandler(ApiRequest request)
+		private void ConnectWiFiStartHandler(EnhancedImageServer server, ApiRequest request)
 		{
-			_bloomReaderPublisher.CancelConnect();
+			if (_advertiser != null)
+				return; // repeat clicks do nothing.
+			// This listens for a BloomReader to request a book.
+			// It requires a firewall hole allowing Bloom to receive messages on _portToListen.
+			// We initialize it before starting the Advertiser to avoid any chance of a race condition
+			// where a BloomReader manages to request an advertised book before we start the listener.
+			m_listener = new UDPListener();
+			m_listener.NewMessageReceived += (sender, args) =>
+			{
+				var androidIpAddress = Encoding.UTF8.GetString(args.data);
+				SendBookTo(server.CurrentBook, androidIpAddress);
+			};
+			_advertiser = new WiFiAdvertiser();
+			_advertiser.BookTitle = server.CurrentBook.Title;
+			// Review: not sure this is what we want for a version. Basically, it allows the Android (by saving it) to avoid downloading
+			// a book that is exactly what it has already...with the risk that it might miss binary changes to images, if nothing changes
+			// in the HTML. However, this doesn't prevent overwriting a newer book with an older one. Another option would be to
+			// send the file modify time (as well or instead). Or we can institute some system of versioning books...
+			_advertiser.BookVersion = Convert.ToBase64String(SHA256Managed.Create().ComputeHash(Encoding.UTF8.GetBytes(server.CurrentBook.RawDom.OuterXml)));
+			_advertiser.Start();
+
+			request.Succeeded();
+		}
+
+		private void SendBookTo(Book.Book book, string androidIpAddress)
+		{
+			_bloomReaderPublisher.SendBookToWiFi(book, androidIpAddress);
+		}
+
+		private void ConnectCancelHandler(ApiRequest request)
+		{
+			if (_advertiser == null)
+			{
+				// either closed without pressing any connect button, or used the USB one.
+				_bloomReaderPublisher.CancelConnect();
+			}
+			else {
+				// Enhance: should we do something if a transfer is in progress? Here or in Dispose?
+				_advertiser.Stop();
+				_advertiser.Dispose();
+				_advertiser = null;
+				m_listener.StopListener();
+				m_listener = null;
+			}
 			request.Succeeded();
 		}
 
@@ -64,6 +115,87 @@ namespace Bloom.Api
 		{
 			_bloomReaderPublisher.Connected -= OnConnected;
 			_webSocketServer.Send(kWebsocketStateId, "ReadyToSend");
+		}
+	}
+
+	/// <summary>
+	/// Helper class to listen for a single packet from the Android. Construct an instance to start
+	/// listening (on another thread); hook NewMessageReceived to receive a packet each time a client sends it.
+	/// </summary>
+	class UDPListener
+	{
+		// must match BloomReader.NewBookListenerService.desktopPort
+		// and be different from WiFiAdvertiser.Port and port in BloomReaderPublisher.SendBookToWiFi
+		private int _portToListen = 5915;
+		Thread _ListeningThread;
+		public event EventHandler<MyMessageArgs> NewMessageReceived;
+		UdpClient _listener = null;
+		private bool _listening;
+
+		//constructor: starts listening.
+		public UDPListener()
+		{
+			_ListeningThread = new Thread(ListenForUDPPackages);
+			_ListeningThread.IsBackground = true;
+			_ListeningThread.Start();
+			_listening = true;
+		}
+
+		/// <summary>
+		/// Run on a background thread; returns only when done listening.
+		/// </summary>
+		public void ListenForUDPPackages()
+		{
+			try
+			{
+				_listener = new UdpClient(_portToListen);
+			}
+			catch (SocketException e)
+			{
+				//do nothing
+			}
+
+			if (_listener != null)
+			{
+				IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 0);
+
+				while (_listening)
+				{
+					try
+					{
+						byte[] bytes = _listener.Receive(ref groupEP); // waits for packet from Android.
+
+						//raise event
+						NewMessageReceived?.Invoke(this, new MyMessageArgs(bytes));
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e.ToString());
+					}
+				}
+			}
+
+		}
+		public void StopListener()
+		{
+			if (_listening)
+			{
+				_listening = false;
+				_listener.Close(); // forcibly end communication
+			}
+		}
+	}
+
+	/// <summary>
+	/// Helper class to hold the data we got from the Android, for the NewMessageReceived event of UDPListener
+	/// </summary>
+	class MyMessageArgs : EventArgs
+	{
+		public byte[] data { get; set; }
+
+		public MyMessageArgs(byte[] newData)
+		{
+			data = newData;
 		}
 	}
 }
