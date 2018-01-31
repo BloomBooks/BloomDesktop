b3a351cd66c6fd79c5aeb50577a152de47bf2cea
DALLAS\Thomson <john_thomson@sil.org>
2018-01-31 11:45:37 -0600

    One more fix the Windows compiler should have caught

--- src/BloomExe/MiscUI/ProblemReporterDialog.cs
+++ src/BloomExe/MiscUI/ProblemReporterDialog.cs
@@ -426,7 +426,7 @@ namespace Bloom.MiscUI
 				return false;
 			foreach (var tool in Book.BookInfo.Tools)
 			{
-				if (tool is Bloom.Edit.DecodableReaderToolSettings || tool.ToolId == "leveledReader")
+				if (tool.ToolId == "decodableReader" || tool.ToolId == "leveledReader")
 					return true;
 			}
 			return false;
