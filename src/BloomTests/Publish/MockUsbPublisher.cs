c9c23e99fa4a135b5015ae154ea2a62c27c283ae
Gordon Martin <martigor@gmail.com>
2018-03-06 10:48:02 -0800

    BL-5710 Not enough space on device
    * fixed misspelled function
    * added WebSocketServerSpy to test output to
       Bloom's WebSocket
    * added test for Disk is Full Exception
    * added test for file size calculation

---
+++ src/BloomTests/Publish/MockUsbPublisher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using Bloom.Book;
+using Bloom.Publish.Android.usb;
+using Bloom.web;
+
+namespace BloomTests.Publish
+{
+	class MockUsbPublisher : UsbPublisher
+	{
+		const int HR_ERROR_DISK_FULL = unchecked((int)0x80070070);
+
+		public MockUsbPublisher(WebSocketProgress progress, BookServer bookServer)
+			:base(progress, bookServer)
+		{
+			Stopped = () => SetState("dummy");
+		}
+
+		internal override void SendBookDoWork(Bloom.Book.Book book, Color backColor)
+		{
+			throw new IOException("MockUsbPublisher threw a fake IOException (Disk is full) in SendBookDoWork", HR_ERROR_DISK_FULL);
+		}
+
+		private void SetState(string message)
+		{
+			// do nothing.
+		}
+
+		public void SetLastBloomdFilePath(string filePath)
+		{
+			_lastPublishedBloomdPath = filePath;
+		}
+
+		public string GetBloomdFileSize()
+		{
+			return GetSizeOfBloomdFile(_lastPublishedBloomdPath);
+		}
+	}
+}
