c1ea5a955e9f0834c53f6002aeef4fa3f45dd73e
John Thomson <john_thomson@sil.org>
2018-03-27 09:21:29 -0500

    Fix folio book image paths

--- src/BloomExe/Book/Book.cs
+++ src/BloomExe/Book/Book.cs
@@ -2193,7 +2193,7 @@ namespace Bloom.Book
 					{
 						var bookFolderName = Path.GetFileName(bookInfo.FolderPath);
 						var path = HtmlDom.GetImageElementUrl(img);
-						var pathRelativeToFolioFolder = ".../" + bookFolderName + "/" + path.NotEncoded;
+						var pathRelativeToFolioFolder = "../" + bookFolderName + "/" + path.NotEncoded;
 						//NB: URLEncode would replace spaces with '+', which is ok in the parameter section, but not the URL
 						//So we are using UrlPathEncode
 
