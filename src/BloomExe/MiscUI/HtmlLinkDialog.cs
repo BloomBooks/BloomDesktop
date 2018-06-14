0cc81419f4d3fefbd17049c0410901aef2ed9973
Stephen McConnel <stephen_mcconnel@sil.org>
2018-06-14 11:36:53 -0600

    Give user link to suggestions when out of memory making PDFs (BL-5467)

---
+++ src/BloomExe/MiscUI/HtmlLinkDialog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bloom.MiscUI
+{
+	/// <summary>
+	/// This dialog can be use in place of a MessageBox when the text has
+	/// a link to an HTML page on the network.  It uses an internal geckofx
+	/// based web display to show the message, including the HTML link.
+	/// </summary>
+	public partial class HtmlLinkDialog : Form
+	{
+		public HtmlLinkDialog(string msg, string title = "")
+		{
+			InitializeComponent();
+			this.htmlLabel.HTML = msg;
+			this.Text = title;
+		}
+	}
+}
