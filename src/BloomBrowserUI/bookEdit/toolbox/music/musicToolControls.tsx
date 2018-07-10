c025713bc0d15903ab67559bac4a81a5ebcbb796
WendyG <wendy_gale@sil.org>
2018-07-10 17:11:45 -0400

    BL-5846 Move component-specific images
    * Move component-specific images
       to the component's folder

--- src/BloomBrowserUI/bookEdit/toolbox/music/musicToolControls.tsx
+++ src/BloomBrowserUI/bookEdit/toolbox/music/musicToolControls.tsx
@@ -189,7 +189,7 @@ export class MusicToolControls extends React.Component<{}, IMusicState> {
                     <div id="musicSetVolume">
                         <img
                             className="speaker-volume"
-                            src="/bloom/bookEdit/toolbox/music/speaker-volume.svg"
+                            src="speaker-volume.svg"
                         />
                         <div className="bgSliderWrapper">
                             <Slider
