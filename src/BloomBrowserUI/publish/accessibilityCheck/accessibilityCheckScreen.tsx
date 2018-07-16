2cd4aee8ec99ff1aa41d56c5dbfa995a97433273
Gordon Martin <martigor@gmail.com>
2018-07-16 10:23:07 -0700

    BL-6188 Localize Accessibility screen tabs

--- src/BloomBrowserUI/publish/accessibilityCheck/accessibilityCheckScreen.tsx
+++ src/BloomBrowserUI/publish/accessibilityCheck/accessibilityCheckScreen.tsx
@@ -7,30 +7,79 @@ import { AccessibilityChecklist } from "./accessibilityChecklist";
 import { DaisyChecks } from "./daisyChecks";
 import WebSocketManager from "../../utils/WebSocketManager";
 import { BloomApi } from "../../utils/bloomApi";
+import theOneLocalizationManager from "../../lib/localizationManager/localizationManager";
 
 // This is a screen of controls that gives the user instructions and controls
 // for creating epubs
 interface IState {
     bookName: string;
+    learnTabName: string;
+    checklistTabName: string;
+    aceTabName: string;
 }
+
 class AccessibilityCheckScreen extends React.Component<{}, IState> {
     constructor(props) {
         super(props);
-        this.state = { bookName: "?" };
+        this.state = {
+            bookName: "?",
+            learnTabName: "Learn About Accessibility",
+            checklistTabName: "Accessibility Checklist",
+            aceTabName: "ACE by Daisy Automated Checks"
+        };
     }
+
     public componentDidMount() {
         // Listen for changes to state from C#-land
         WebSocketManager.addListener("a11yChecklist", e => {
             if (e.message === "bookSelectionChanged") this.refresh();
         });
+        this.localizeTabNames();
         this.refresh();
     }
+
     private refresh() {
         BloomApi.get("accessibilityCheck/bookName", result => {
             this.setState({
                 bookName: result.data
             });
         });
+    }
+
+    // After trying unsuccessfully to wrap react-tabs Tab in a l10n-aware component or create
+    // a HOC to inject l10n, I'm resorting to this method of localizing the tab names.
+    private localizeTabNames() {
+        this.localizeOneTabName(
+            "AccessibilityCheck.LearnAbout",
+            this.state.learnTabName,
+            "learnTabName"
+        );
+        this.localizeOneTabName(
+            "AccessibilityCheck.Checklist",
+            this.state.checklistTabName,
+            "checklistTabName"
+        );
+        this.localizeOneTabName(
+            "AccessibilityCheck.ACEByDaisy",
+            this.state.aceTabName,
+            "aceTabName"
+        );
+    }
+
+    private localizeOneTabName(
+        id: string,
+        englishText: string,
+        stateMemberName: string
+    ) {
+        theOneLocalizationManager
+            .asyncGetText(
+                id,
+                englishText,
+                "Used as the name on a tab of the Accessibility Checks screen."
+            )
+            .done(result => {
+                this.setState({ [stateMemberName]: result } as IState);
+            });
     }
 
     public render() {
@@ -42,9 +91,9 @@ class AccessibilityCheckScreen extends React.Component<{}, IState> {
                 </div>
                 <Tabs defaultIndex={1}>
                     <TabList>
-                        <Tab>Learn About Accessibility</Tab>
-                        <Tab>Accessibility Checklist</Tab>
-                        <Tab>ACE by Daisy Automated Checks</Tab>
+                        <Tab>{this.state.learnTabName}</Tab>
+                        <Tab>{this.state.checklistTabName}</Tab>
+                        <Tab>{this.state.aceTabName}</Tab>
                     </TabList>
                     <TabPanel>
                         <LearnAboutAccessibility />
