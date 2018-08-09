aa4a87795548b40e3ad352039a318b887d24fe67
John Hatton <hattonjohn@gmail.com>
2018-08-08 18:50:38 -0600

    BL-6174SomeEditableRowsForMetadataDialog

--- src/BloomExe/Book/BookInfo.cs
+++ src/BloomExe/Book/BookInfo.cs
@@ -881,6 +881,19 @@ namespace Bloom.Book
 		[JsonProperty("toolboxIsOpen")]
 		[DefaultValue(false)]
 		public bool ToolboxIsOpen { get; set; }
+
+		[JsonProperty("author")]
+		public string Author { get; set; }
+
+		// Global Digital Library: Indicates reading level
+		// NB: this is just "level" in the Global Digital Library
+		// e.g. "Pratham Level 1"
+		[JsonProperty("readlingLevelDescription")]
+		public string ReadlingLevelDescription { get; set; }
+
+		// Global Digital Library: The typical range of ages the content’s intended end user.
+		[JsonProperty("typicalAgeRange")]
+		public string TypicalAgeRange { get; set; }
 	}
 
 	/// <summary>
