d0f0f289122b85b3259edcd8d456db16cd53e508
John Hatton <hattonjohn@gmail.com>
2018-08-10 16:51:28 -0600

    BL-6180 Add hazard & feature checkboxes to metadata dialog
    So this is a bit complicated because these checkboxes have two features:
    1) They operate on a comma-delimited set of tokens
    2) The hazards one uses 3-state checkboxes

--- src/BloomBrowserUI/react_components/checkbox.tsx
+++ src/BloomBrowserUI/react_components/checkbox.tsx
@@ -1,31 +1,46 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import {
-    ILocalizationProps,
-    LocalizableElement,
-    Label,
-    ILabelProps
-} from "./l10n";
+import { ILocalizationProps, LocalizableElement } from "./l10n";
 
 interface ICheckboxProps extends ILocalizationProps {
     id?: string;
     name?: string;
-    checked: boolean;
-    onCheckChanged?: (boolean) => void;
     className?: string;
     disabled?: boolean;
     wrapClassName?: string;
+    // toggle between on, off, and unknown
+    tristate?: boolean;
+    // Note: checked can be undefined, if tristate==true
+    checked?: boolean;
+    // Note: the parameter given will be undefined if  tristate==true and the checkbox is in the indeterminate state
+    onCheckChanged?: (boolean) => void;
 }
 
-// A checkbox that is localizable.
+// A checkbox that is localizable and can toggle between either a 2 or 3 states.
 export class Checkbox extends LocalizableElement<ICheckboxProps, {}> {
     private input: HTMLInputElement;
+    private previousTriState: boolean;
+    public constructor(props: ICheckboxProps) {
+        super(props);
+        this.previousTriState = props.checked;
+    }
+
+    public componentDidMount() {
+        // set the initial "unknown" state
+        if (this.props.tristate) {
+            this.input.indeterminate = this.props.checked == undefined;
+        }
+    }
+
     private onLabelClicked() {
         // We expect the effect of clicking the label will be to set the check to the
         // opposite state, so that's what we pass. (But whether it really changes is
         // up to the owner changing the prop value. So it won't have happened yet.)
-        this.props.onCheckChanged(!this.input.checked);
+        if (!this.props.tristate) {
+            this.input.checked = !this.input.checked;
+        }
+        this.onChange(this.input);
     }
+
     public render() {
         return (
             <div className={this.props.wrapClassName}>
@@ -36,9 +51,9 @@ export class Checkbox extends LocalizableElement<ICheckboxProps, {}> {
                     name={this.props.name}
                     disabled={this.props.disabled}
                     checked={this.props.checked}
-                    onChange={event =>
-                        this.props.onCheckChanged(event.target.checked)
-                    }
+                    onChange={event => {
+                        this.onChange(event.target);
+                    }}
                     ref={input => (this.input = input)}
                 />
                 <label onClick={() => this.onLabelClicked()}>
@@ -46,5 +61,28 @@ export class Checkbox extends LocalizableElement<ICheckboxProps, {}> {
                 </label>
             </div>
         );
+    }
+
+    private onChange(target: HTMLInputElement) {
+        if (!this.props.tristate) {
+            this.props.onCheckChanged(target.checked);
+        } else {
+            // we're in on/off/unknown mode, so handle the progress ourselves
+            switch (this.previousTriState) {
+                case undefined:
+                    this.previousTriState = target.checked = true;
+                    target.indeterminate = false;
+                    break;
+                case true:
+                    this.previousTriState = target.checked = false;
+                    target.indeterminate = false;
+                    break;
+                case false:
+                    target.indeterminate = true;
+                    this.previousTriState = undefined;
+                    break;
+            }
+            this.props.onCheckChanged(this.previousTriState);
+        }
     }
 }
