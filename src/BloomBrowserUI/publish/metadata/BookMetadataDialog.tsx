03c1186f832618ef380ce50908f757a512ce3965
John Hatton <hattonjohn@gmail.com>
2018-08-10 16:51:28 -0600

    BL-6180 Add hazard & feature checkboxes to metadata dialog
    So this is a bit complicated because these checkboxes have two features:
    1) They operate on a comma-delimited set of tokens
    2) The hazards one uses 3-state checkboxes

--- src/BloomBrowserUI/publish/metadata/BookMetadataDialog.tsx
+++ src/BloomBrowserUI/publish/metadata/BookMetadataDialog.tsx
@@ -4,24 +4,35 @@ import "./BookMetadataDialog.less";
 import CloseOnEscape from "react-close-on-escape";
 import BookMetadataTable from "./BookMetadataTable";
 import { BloomApi } from "../../utils/bloomApi";
+import * as mobx from "mobx";
+import * as mobxReact from "mobx-react";
 
 // tslint:disable-next-line:no-empty-interface
 interface IProps {}
+
+// @observer means mobx will automatically track which observables this component uses
+// in its render() function, and then re-render when they change.
+@mobxReact.observer
 export default class BookMetadataDialog extends React.Component<IProps> {
     private static singleton: BookMetadataDialog;
-    public readonly state = { isOpen: false, data: Object };
+    public readonly state = { isOpen: false };
+
+    // we want mobx to watch this, because we will pass it to the BookMetadataTable, which can change it.
+    @mobx.observable
+    private metadata: any = { test: { type: "readOnlyText", value: "test" } };
+
     constructor(props: IProps) {
         super(props);
         BookMetadataDialog.singleton = this;
     }
     public componentDidMount() {
         BloomApi.get("book/metadata", result => {
-            this.setState({ data: result.data });
+            this.metadata = result.data;
         });
     }
     private handleCloseModal(doSave: boolean) {
         if (doSave) {
-            BloomApi.postData("book/metadata", this.state.data);
+            BloomApi.postData("book/metadata", this.metadata);
         }
         this.setState({ isOpen: false });
     }
@@ -47,7 +58,7 @@ export default class BookMetadataDialog extends React.Component<IProps> {
                 >
                     <div className={"dialogTitle"}>Book Metadata</div>
                     <div className="dialogContent">
-                        <BookMetadataTable data={this.state.data} />
+                        <BookMetadataTable metadata={this.metadata} />
                         <div className={"bottomButtonRow"}>
                             <button id="helpButton" disabled={true}>
                                 Help
