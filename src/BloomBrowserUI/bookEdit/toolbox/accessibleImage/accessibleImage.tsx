0a8e5fd4299e5f8bf4ce1215ee03e8ffaa6ef6d2
John Thomson <john_thomson@sil.org>
2018-08-15 17:03:19 -0500

    BL-6183 Color-blindness accessibility check

--- src/BloomBrowserUI/bookEdit/toolbox/accessibleImage/accessibleImage.tsx
+++ src/BloomBrowserUI/bookEdit/toolbox/accessibleImage/accessibleImage.tsx
@@ -6,19 +6,33 @@ import { ToolBox, ITool } from "../toolbox";
 import { ApiBackedCheckbox } from "../../../react_components/apiBackedCheckbox";
 import "./accessibleImage.less";
 import { RequiresBloomEnterpriseWrapper } from "../../../react_components/requiresBloomEnterprise";
+import { RadioGroup, Radio } from "../../../react_components/radio";
+import { deuteranopia, tritanopia, achromatopsia } from "color-blind";
+
+interface IState {
+    kindOfColorBlindness: string;
+}
 
 // This react class implements the UI for the accessible images toolbox.
 // Note: this file is included in toolboxBundle.js because webpack.config says to include all
 // tsx files in bookEdit/toolbox.
 // The toolbox is included in the list of tools because of the one line of immediately-executed code
 // which  passes an instance of AccessibleImageToolAdaptor to ToolBox.registerTool();
-export class AccessibleImageControls extends React.Component<{}> {
+export class AccessibleImageControls extends React.Component<{}, IState> {
+    constructor(state: IState) {
+        super({}, state);
+        this.state = { kindOfColorBlindness: "redGreen" };
+        BloomApi.get("accessibilityCheck/kindOfColorBlindness", result => {
+            this.setState({ kindOfColorBlindness: result.data });
+        });
+    }
     // This wants to be part of our state, passed as a prop to ApiBackedCheckbox.
     // But then we, and all the other clients of that class, have to be responsible
     // for interacting with the api to get and set that state. So, for the moment,
     // we just let the check box tell us what its value should be using onCheckChanged,
     // and use it to update the appearance of the page. Better solution wanted!
     private simulatingCataracts: boolean;
+    private simulatingColorBlindness: boolean;
 
     public render() {
         return (
@@ -38,6 +52,39 @@ export class AccessibleImageControls extends React.Component<{}> {
                     >
                         Cataracts
                     </ApiBackedCheckbox>
+                    <ApiBackedCheckbox
+                        className="checkBox colorBlindCheckBox"
+                        apiEndpoint="accessibilityCheck/colorBlindness"
+                        l10nKey="EditTab.Toolbox.AccessibleImage/ColorBlindness"
+                        onCheckChanged={simulate =>
+                            this.updateCbCheck(simulate)
+                        }
+                    >
+                        Color Blindness
+                    </ApiBackedCheckbox>
+                    <RadioGroup
+                        onChange={val => this.updateCbRadio(val)}
+                        value={this.state.kindOfColorBlindness}
+                    >
+                        <Radio
+                            l10nKey="EditTab.Toolbox.AccessibleImage.RedGreen"
+                            value="RedGreen"
+                        >
+                            Red-Green
+                        </Radio>
+                        <Radio
+                            l10nKey="EditTab.Toolbox.AccessibleImage.BlueYellow"
+                            value="BlueYellow"
+                        >
+                            Blue-Yellow
+                        </Radio>
+                        <Radio
+                            l10nKey="EditTab.Toolbox.AccessibleImage.Complete"
+                            value="Complete"
+                        >
+                            Complete
+                        </Radio>
+                    </RadioGroup>
                 </div>
             </RequiresBloomEnterpriseWrapper>
         );
@@ -45,6 +92,25 @@ export class AccessibleImageControls extends React.Component<{}> {
 
     private updateCataracts(simulate: boolean) {
         this.simulatingCataracts = simulate;
+        this.updateSimulations();
+    }
+
+    private updateCbCheck(simulate: boolean) {
+        this.simulatingColorBlindness = simulate;
+        this.updateSimulations();
+    }
+
+    private updateCbRadio(mode: string) {
+        BloomApi.postDataWithConfig(
+            "accessibilityCheck/kindOfColorBlindness",
+            mode,
+            { headers: { "Content-Type": "application/json" } }
+        );
+        this.setState({ kindOfColorBlindness: mode });
+        // componentDidUpdate will call updateSimulations when state is stable
+    }
+
+    public componentDidUpdate(prevProps, prevState: IState) {
         this.updateSimulations();
     }
 
@@ -56,6 +122,111 @@ export class AccessibleImageControls extends React.Component<{}> {
         } else {
             body.classList.remove("simulateCataracts");
         }
+        AccessibleImageControls.removeCbMarkup();
+        if (this.simulatingColorBlindness) {
+            body.classList.add("simulateColorBlindness");
+            // For now limit it to these images because the positioning depends
+            // on the img being the first thing in its parent and the parent
+            // being positioned, which we can't count on for other images.
+            var containers = page.getElementsByClassName(
+                "bloom-imageContainer"
+            );
+            for (var i = 0; i < containers.length; i++) {
+                var images = containers[i].getElementsByTagName("img");
+                for (var imgIndex = 0; imgIndex < images.length; imgIndex++) {
+                    this.makeCbOverlay(images[imgIndex]);
+                }
+            }
+        } else {
+            body.classList.remove("simulateColorBlindness");
+        }
+    }
+
+    public static removeAcessibleImageMarkup() {
+        AccessibleImageControls.removeCbMarkup();
+        var page = ToolboxToolReactAdaptor.getPage();
+        var body = page.ownerDocument.body;
+        body.classList.remove("simulateColorBlindness");
+        body.classList.remove("simulateCataracts");
+    }
+
+    private static removeCbMarkup() {
+        var page = ToolboxToolReactAdaptor.getPage();
+        [].slice
+            .call(page.getElementsByClassName("ui-cbOverlay"))
+            .map(x => x.parentElement.removeChild(x));
+    }
+
+    private componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    private rgbToHex(r, g, b) {
+        return (
+            "#" +
+            this.componentToHex(r) +
+            this.componentToHex(g) +
+            this.componentToHex(b)
+        );
+    }
+
+    private makeCbOverlay(img: HTMLImageElement) {
+        var page = ToolboxToolReactAdaptor.getPage();
+        var canvas = page.ownerDocument.createElement("canvas");
+        // Make the canvas be the size the image is actually drawn.
+        // Typically that means fewer pixels to calculate than doing the whole
+        // image. To avoid distortion, we do have to make it the right shape.
+        let canvasWidth = img.width;
+        let canvasHeight = img.height;
+        if (img.naturalWidth / img.naturalHeight < canvasWidth / canvasHeight) {
+            // available space is too wide: make narrower
+            canvasWidth = (canvasHeight * img.naturalWidth) / img.naturalHeight;
+        } else {
+            // available space may be too tall: make shorter
+            canvasHeight = (canvasWidth * img.naturalHeight) / img.naturalWidth;
+        }
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        // Make the canvas fill the image container, like the img.
+        // This allows object-fit and object-position to put it where we want.
+        canvas.style.position = "absolute";
+        canvas.style.left = "0";
+        canvas.style.top = "0";
+        canvas.style.height = "100%";
+        canvas.style.width = "100%";
+        // But then, make it shrink enough to keep its aspect ratio
+        canvas.style.objectFit = "contain";
+        // And position it within the container the same as the img.
+        img.style.objectPosition = window
+            .getComputedStyle(img)
+            .getPropertyValue("object-position");
+        canvas.classList.add("ui-cbOverlay"); // used to remove them
+        var context = canvas.getContext("2d");
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+        // imgData is a byte array with 4 bytes for each pixel in RGBA order
+        var imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+        var data = imgData.data;
+        var cbAdapter = deuteranopia;
+        if (this.state.kindOfColorBlindness == "BlueYellow") {
+            cbAdapter = tritanopia;
+        } else if (this.state.kindOfColorBlindness == "Complete") {
+            cbAdapter = achromatopsia;
+        }
+        for (var ipixel = 0; ipixel < data.length / 4; ipixel++) {
+            var r = data[ipixel * 4];
+            var g = data[ipixel * 4 + 1];
+            var b = data[ipixel * 4 + 2];
+            var colorString = this.rgbToHex(r, g, b);
+            var newRgb = cbAdapter(colorString, true);
+            data[ipixel * 4] = newRgb.R;
+            data[ipixel * 4 + 1] = newRgb.G;
+            data[ipixel * 4 + 2] = newRgb.B;
+            //data[ipixel * 4 + 3] = 255; // make the new image opaque.
+        }
+        context.putImageData(imgData, 0, 0);
+
+        img.parentElement.appendChild(canvas);
     }
 }
 
@@ -84,6 +255,10 @@ export class AccessibleImageToolAdaptor extends ToolboxToolReactAdaptor {
 
     public newPageReady() {
         this.controlsElement.updateSimulations();
+    }
+
+    public detachFromPage() {
+        AccessibleImageControls.removeAcessibleImageMarkup();
     }
 
     public isExperimental(): boolean {
