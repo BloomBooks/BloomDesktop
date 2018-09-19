5276c3cd455efe96a37e003c0b9d6fab4fdd94dc
John Hatton <hattonjohn@gmail.com>
2018-09-19 14:38:58 -0600

    Change unit tests for new branding system

--- src/BloomExe/web/controllers/ImageApi.cs
+++ src/BloomExe/web/controllers/ImageApi.cs
@@ -256,7 +256,9 @@ namespace Bloom.web.controllers
 			var imageNameToPages = new Dictionary<string, List<string>>();
 			foreach (XmlElement img in HtmlDom.SelectChildImgAndBackgroundImageElements(domBody as XmlElement))
 			{
-				if (IsElementNotInAPage(img))
+				if (IsImgNotInAPage(img))
+					continue;
+				if (IsImgInsideBrandingElement(img))
 					continue;
 				var name = HtmlDom.GetImageElementUrl(img).PathOnly.NotEncoded;
 				var pageNum = HtmlDom.GetNumberOrLabelOfPageWhereElementLives(img);
@@ -278,9 +280,14 @@ namespace Bloom.web.controllers
 			return imageNameToPages;
 		}
 
-		private static bool IsElementNotInAPage(XmlElement element)
+		private static bool IsImgNotInAPage(XmlElement imgElement)
+		{ 
+			return !(imgElement.SelectSingleNode("ancestor-or-self::div[contains(@class,'bloom-page')]") is XmlElement);
+		}
+
+		private static bool IsImgInsideBrandingElement(XmlElement imgElement)
 		{
-			return !(element.SelectSingleNode("ancestor-or-self::div[contains(@class,'bloom-page')]") is XmlElement);
+			return imgElement.SelectSingleNode("ancestor-or-self::div[contains(@data-book,'branding')]") is XmlElement;
 		}
 
 		/// <summary>
