960847a0d14fe3a4f08f5503ba3c5da631e9e011
Gordon Martin <martigor@gmail.com>
2019-02-20 14:21:54 -0800

    BL-6898 Remove Preview from orient. buttons
    * Publish - Android

--- src/BloomBrowserUI/publish/android/androidPublishUI.tsx
+++ src/BloomBrowserUI/publish/android/androidPublishUI.tsx
@@ -141,8 +141,8 @@ class AndroidPublishUI extends React.Component<IUILanguageAwareProps, IState> {
     }
 
     private getIsLinuxFromUrl(): boolean {
-        let searchString = window.location.search;
-        let i = searchString.indexOf("isLinux=");
+        const searchString = window.location.search;
+        const i = searchString.indexOf("isLinux=");
         if (i >= 0) {
             return searchString.substr(i + "isLinux=".length, 4) === "true";
         }
@@ -205,9 +205,9 @@ class AndroidPublishUI extends React.Component<IUILanguageAwareProps, IState> {
                                                 reportBookProperties={arg =>
                                                     this.gotBookProps(arg)
                                                 }
-                                                pageSelected={page =>
-                                                    this.pageSelected(page)
-                                                }
+                                                // pageSelected={page =>
+                                                //     this.pageSelected(page)
+                                                // }
                                             />
                                         </div>
                                     </div>
@@ -685,40 +685,40 @@ class AndroidPublishUI extends React.Component<IUILanguageAwareProps, IState> {
         // todo: implement.
     }
 
-    private setButtonContents(
-        page: HTMLElement,
-        id: string,
-        landscape: boolean
-    ): void {
-        const copy = page.cloneNode(true) as HTMLElement;
-        const button: HTMLElement = document.getElementById(id)!;
-        if (button.firstElementChild) {
-            button.removeChild(button.firstElementChild);
-        }
-        // The preview may have elements marked up as being the audio element
-        // currently playing. We don't want this in the button. For one thing,
-        // it wouldn't update as the audio changes. For another, at one stage
-        // it was playing with an echo...probably from trying to play BOTH
-        // current audio elements.
-        const currentAudio = copy.getElementsByClassName("ui-audioCurrent");
-        for (let i = 0; i < currentAudio.length; i++) {
-            currentAudio[i].classList.remove("ui-audioCurrent");
-        }
-        if (this.state.bookCanRotate) {
-            BloomPlayer.forceDevicePageSize(
-                copy.getElementsByClassName("bloom-page")[0] as HTMLElement,
-                true,
-                landscape
-            );
-        }
-        button.appendChild(copy);
-    }
+    // private setButtonContents(
+    //     page: HTMLElement,
+    //     id: string,
+    //     landscape: boolean
+    // ): void {
+    //     const copy = page.cloneNode(true) as HTMLElement;
+    //     const button: HTMLElement = document.getElementById(id)!;
+    //     if (button.firstElementChild) {
+    //         button.removeChild(button.firstElementChild);
+    //     }
+    //     // The preview may have elements marked up as being the audio element
+    //     // currently playing. We don't want this in the button. For one thing,
+    //     // it wouldn't update as the audio changes. For another, at one stage
+    //     // it was playing with an echo...probably from trying to play BOTH
+    //     // current audio elements.
+    //     const currentAudio = copy.getElementsByClassName("ui-audioCurrent");
+    //     for (let i = 0; i < currentAudio.length; i++) {
+    //         currentAudio[i].classList.remove("ui-audioCurrent");
+    //     }
+    //     if (this.state.bookCanRotate) {
+    //         BloomPlayer.forceDevicePageSize(
+    //             copy.getElementsByClassName("bloom-page")[0] as HTMLElement,
+    //             true,
+    //             landscape
+    //         );
+    //     }
+    //     button.appendChild(copy);
+    // }
 
     // Whenever we change pages, update the orientation buttons to match.
-    private pageSelected(page: HTMLElement): void {
-        this.setButtonContents(page, "portrait-button", false);
-        this.setButtonContents(page, "landscape-button", true);
-    }
+    // private pageSelected(page: HTMLElement): void {
+    //     this.setButtonContents(page, "portrait-button", false);
+    //     this.setButtonContents(page, "landscape-button", true);
+    // }
 }
 
 // a bit goofy... currently the html loads everying in publishUIBundlejs. So all the publish screens
