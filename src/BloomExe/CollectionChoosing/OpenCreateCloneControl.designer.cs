d6feb6cf1f8cfcbb6fe7024086f1133e42fcb1c7
Gordon Martin <martigor@gmail.com>
2019-04-17 13:19:26 -0700

    BL-6905 Remember 9 collections

--- src/BloomExe/CollectionChoosing/OpenCreateCloneControl.designer.cs
+++ src/BloomExe/CollectionChoosing/OpenCreateCloneControl.designer.cs
@@ -29,21 +29,15 @@ namespace Bloom.CollectionChoosing
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OpenCreateCloneControl));
 			this._debounceListIndexChangedEvent = new System.Windows.Forms.Timer(this.components);
 			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
 			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
 			this._browseButton = new System.Windows.Forms.Button();
 			this.button9 = new System.Windows.Forms.Button();
 			this._templateButton = new System.Windows.Forms.Button();
-			this.button8 = new System.Windows.Forms.Button();
-			this.button7 = new System.Windows.Forms.Button();
-			this.button6 = new System.Windows.Forms.Button();
-			this._readMoreLink = new System.Windows.Forms.LinkLabel();
 			this._topRightPanel = new System.Windows.Forms.Panel();
 			this._toolStrip1 = new System.Windows.Forms.ToolStrip();
 			this._uiLanguageMenu = new System.Windows.Forms.ToolStripDropDownButton();
-			this._sendReceiveInstructionsLabel = new System.Windows.Forms.Label();
 			this._L10NSharpExtender = new L10NSharp.UI.L10NSharpExtender(this.components);
 			this.tableLayoutPanel2.SuspendLayout();
 			this._topRightPanel.SuspendLayout();
@@ -60,15 +54,11 @@ namespace Bloom.CollectionChoosing
 			this.tableLayoutPanel2.BackColor = System.Drawing.Color.White;
 			this.tableLayoutPanel2.ColumnCount = 3;
 			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 260F));
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 272F));
 			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel2.Controls.Add(this._browseButton, 0, 5);
 			this.tableLayoutPanel2.Controls.Add(this.button9, 0, 0);
 			this.tableLayoutPanel2.Controls.Add(this._templateButton, 0, 1);
-			this.tableLayoutPanel2.Controls.Add(this.button8, 2, 4);
-			this.tableLayoutPanel2.Controls.Add(this.button7, 2, 3);
-			this.tableLayoutPanel2.Controls.Add(this.button6, 2, 2);
-			this.tableLayoutPanel2.Controls.Add(this._readMoreLink, 2, 5);
 			this.tableLayoutPanel2.Controls.Add(this._topRightPanel, 2, 0);
 			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
@@ -148,92 +138,14 @@ namespace Bloom.CollectionChoosing
 			this._templateButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
 			this._templateButton.UseVisualStyleBackColor = true;
 			// 
-			// button8
-			// 
-			this.button8.FlatAppearance.BorderSize = 0;
-			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-			this.button8.Image = global::Bloom.Properties.Resources.cloneFromChorusHub;
-			this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this._L10NSharpExtender.SetLocalizableToolTip(this.button8, null);
-			this._L10NSharpExtender.SetLocalizationComment(this.button8, null);
-			this._L10NSharpExtender.SetLocalizingId(this.button8, "OpenCreateNewCollectionsDialog.CopyFromChorusHub");
-			this.button8.Location = new System.Drawing.Point(363, 198);
-			this.button8.Name = "button8";
-			this.button8.Size = new System.Drawing.Size(391, 36);
-			this.button8.TabIndex = 21;
-			this.button8.Tag = "sendreceive";
-			this.button8.Text = "Copy From Chorus Hub on Local Network";
-			this.button8.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-			this.button8.UseVisualStyleBackColor = true;
-			// 
-			// button7
-			// 
-			this.button7.FlatAppearance.BorderSize = 0;
-			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-			this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
-			this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this._L10NSharpExtender.SetLocalizableToolTip(this.button7, null);
-			this._L10NSharpExtender.SetLocalizationComment(this.button7, null);
-			this._L10NSharpExtender.SetLocalizingId(this.button7, "OpenCreateNewCollectionsDialog.CopyFromInternet");
-			this.button7.Location = new System.Drawing.Point(363, 153);
-			this.button7.Name = "button7";
-			this.button7.Size = new System.Drawing.Size(405, 36);
-			this.button7.TabIndex = 20;
-			this.button7.Tag = "sendreceive";
-			this.button7.Text = "Copy from Internet";
-			this.button7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-			this.button7.UseVisualStyleBackColor = true;
-			// 
-			// button6
-			// 
-			this.button6.FlatAppearance.BorderSize = 0;
-			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-			this.button6.Image = global::Bloom.Properties.Resources.cloneFromUsb;
-			this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this._L10NSharpExtender.SetLocalizableToolTip(this.button6, null);
-			this._L10NSharpExtender.SetLocalizationComment(this.button6, null);
-			this._L10NSharpExtender.SetLocalizingId(this.button6, "OpenCreateNewCollectionsDialog.CopyFromUsbDrive");
-			this.button6.Location = new System.Drawing.Point(363, 108);
-			this.button6.Name = "button6";
-			this.button6.Size = new System.Drawing.Size(404, 39);
-			this.button6.TabIndex = 19;
-			this.button6.Tag = "sendreceive";
-			this.button6.Text = "Copy from USB Drive";
-			this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-			this.button6.UseVisualStyleBackColor = true;
-			// 
-			// _readMoreLink
-			// 
-			this._readMoreLink.AutoSize = true;
-			this._readMoreLink.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this._L10NSharpExtender.SetLocalizableToolTip(this._readMoreLink, null);
-			this._L10NSharpExtender.SetLocalizationComment(this._readMoreLink, "This opens the Chorus Help to learn more about send/receive.");
-			this._L10NSharpExtender.SetLocalizingId(this._readMoreLink, "OpenCreateNewCollectionsDialog.ReadMoreLink");
-			this._readMoreLink.Location = new System.Drawing.Point(363, 265);
-			this._readMoreLink.Margin = new System.Windows.Forms.Padding(3, 15, 3, 0);
-			this._readMoreLink.Name = "_readMoreLink";
-			this._readMoreLink.Size = new System.Drawing.Size(63, 13);
-			this._readMoreLink.TabIndex = 30;
-			this._readMoreLink.TabStop = true;
-			this._readMoreLink.Tag = "sendreceive";
-			this._readMoreLink.Text = "Read More";
-			this._readMoreLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this._readMoreLabel_Click);
-			// 
 			// _topRightPanel
 			// 
 			this._topRightPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this._topRightPanel.Controls.Add(this._toolStrip1);
-			this._topRightPanel.Controls.Add(this._sendReceiveInstructionsLabel);
-			this._topRightPanel.Location = new System.Drawing.Point(363, 3);
+			this._topRightPanel.Location = new System.Drawing.Point(535, 3);
 			this._topRightPanel.Name = "_topRightPanel";
-			this._topRightPanel.Size = new System.Drawing.Size(523, 54);
+			this._topRightPanel.Size = new System.Drawing.Size(351, 54);
 			this._topRightPanel.TabIndex = 31;
 			// 
 			// _toolStrip1
@@ -248,7 +160,7 @@ namespace Bloom.CollectionChoosing
 			this._L10NSharpExtender.SetLocalizableToolTip(this._toolStrip1, "Change user interface language");
 			this._L10NSharpExtender.SetLocalizationComment(this._toolStrip1, null);
 			this._L10NSharpExtender.SetLocalizingId(this._toolStrip1, "OpenCreateNewCollectionsDialog.UILanguageMenu");
-			this._toolStrip1.Location = new System.Drawing.Point(458, 9);
+			this._toolStrip1.Location = new System.Drawing.Point(286, 9);
 			this._toolStrip1.Name = "_toolStrip1";
 			this._toolStrip1.Size = new System.Drawing.Size(65, 25);
 			this._toolStrip1.TabIndex = 25;
@@ -270,24 +182,6 @@ namespace Bloom.CollectionChoosing
 			this._uiLanguageMenu.Text = "English";
 			this._uiLanguageMenu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
-			// _sendReceiveInstructionsLabel
-			// 
-			this._sendReceiveInstructionsLabel.AutoSize = true;
-			this._sendReceiveInstructionsLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F);
-			this._sendReceiveInstructionsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
-			this._L10NSharpExtender.SetLocalizableToolTip(this._sendReceiveInstructionsLabel, null);
-			this._L10NSharpExtender.SetLocalizationComment(this._sendReceiveInstructionsLabel, null);
-			this._L10NSharpExtender.SetLocalizingId(this._sendReceiveInstructionsLabel, "OpenCreateNewCollectionsDialog.TextAboutGetUsingChorus");
-			this._sendReceiveInstructionsLabel.Location = new System.Drawing.Point(0, -3);
-			this._sendReceiveInstructionsLabel.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
-			this._sendReceiveInstructionsLabel.Name = "_sendReceiveInstructionsLabel";
-			this._sendReceiveInstructionsLabel.Size = new System.Drawing.Size(425, 51);
-			this._sendReceiveInstructionsLabel.TabIndex = 24;
-			this._sendReceiveInstructionsLabel.Tag = "sendreceive";
-			this._sendReceiveInstructionsLabel.Text = "Has someone else used Send/Receive to share a collection with you?\r\nUse one of th" +
-    "ese red buttons to copy their collection to your computer.\r\nLater, use Send/Rece" +
-    "ive to share your work back with them.";
-			// 
 			// _L10NSharpExtender
 			// 
 			this._L10NSharpExtender.LocalizationManagerId = "Bloom";
@@ -306,7 +200,6 @@ namespace Bloom.CollectionChoosing
 			this.Size = new System.Drawing.Size(889, 343);
 			this.Load += new System.EventHandler(this.OnLoad);
 			this.tableLayoutPanel2.ResumeLayout(false);
-			this.tableLayoutPanel2.PerformLayout();
 			this._topRightPanel.ResumeLayout(false);
 			this._topRightPanel.PerformLayout();
 			this._toolStrip1.ResumeLayout(false);
@@ -321,14 +214,9 @@ namespace Bloom.CollectionChoosing
 		private System.Windows.Forms.Timer _debounceListIndexChangedEvent;
 		private System.Windows.Forms.ToolTip toolTip1;
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-		private System.Windows.Forms.Button button6;
-		private System.Windows.Forms.Button button7;
-		private System.Windows.Forms.Button button8;
 		private System.Windows.Forms.Button button9;
 		private System.Windows.Forms.Button _templateButton;
-		private System.Windows.Forms.Label _sendReceiveInstructionsLabel;
 		private System.Windows.Forms.Button _browseButton;
-		private System.Windows.Forms.LinkLabel _readMoreLink;
 		private L10NSharp.UI.L10NSharpExtender _L10NSharpExtender;
 		private System.Windows.Forms.Panel _topRightPanel;
 		private System.Windows.Forms.ToolStrip _toolStrip1;
