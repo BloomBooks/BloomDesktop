9692c8112a5ba4e03962ad3ef1022509db2f3a04
Unknown <jeffrey_su@sil.org>
2019-05-07 15:12:34 -0500

    Fix book selection on collection switch (BL-6965)

---
+++ src/BloomTests/TestDoubles/Book/FakeBookServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bloom.Book;
+
+namespace BloomTests.TestDoubles.Book
+{
+	// A simplified implementation of BookServer that cuts some corners in order to ease setup.
+	class FakeBookServer : BookServer
+	{
+		public FakeBookServer() : base(null, null, null, null)
+		{
+		}
+
+		/// <summary>
+		/// A dumbed down implementation that is able to return a book with the BookInfo and set the book's FolderPath.
+		/// </summary>
+		/// <returns></returns>
+		public override Bloom.Book.Book GetBookFromBookInfo(BookInfo bookInfo, bool forSelectedBook = false)
+		{
+			var collectionSettings = new Bloom.Collection.CollectionSettings();
+			var fileLocator = new Bloom.BloomFileLocator(collectionSettings, new XMatterPackFinder(Enumerable.Empty<string>()), Enumerable.Empty<string>(), Enumerable.Empty<string>(),
+			Enumerable.Empty<string>());
+
+			// Setting storage is neeeded to get it to populate the book's FolderPath.
+			var storage = new BookStorage(bookInfo.FolderPath, fileLocator, new Bloom.BookRenamedEvent(), collectionSettings);
+
+			var book = new Bloom.Book.Book(bookInfo, storage);
+			return book;
+		}
+	}
+}
---
+++ src/BloomTests/TestDoubles/CollectionTab/FakeLibraryModel.cs
@@ -0,0 +1,40 @@
+﻿using Bloom;
+using Bloom.Book;
+using Bloom.Collection;
+using Bloom.CollectionTab;
+using SIL.TestUtilities;
+
+using BloomTests.TestDoubles.Book;
+
+namespace BloomTests.TestDoubles.CollectionTab
+{
+	class FakeLibraryModel: LibraryModel
+	{
+		public readonly string TestFolderPath;
+
+		public FakeLibraryModel(TemporaryFolder testFolder)
+			: base(testFolder.Path, new CollectionSettings(), new BookSelection(), GetDefaultSourceCollectionsList(),
+			BookCollectionFactory, null, new CreateFromSourceBookCommand(), new FakeBookServer(), new CurrentEditableCollectionSelection(), null)
+		{
+			TestFolderPath = testFolder.Path;
+		}
+
+		public void RunCompressDirectoryTest(string outputPath, bool forReaderTools = false)
+		{
+			BookCompressor.CompressDirectory(outputPath, TestFolderPath, "", forReaderTools);
+		}
+
+		protected static SourceCollectionsList GetDefaultSourceCollectionsList()
+		{
+			var parentFolder = new TemporaryFolder("Parent");
+			var templateCollectionFolder = new TemporaryFolder(parentFolder, "Templates");
+
+			return new SourceCollectionsList(null, null, null, new string[] { parentFolder.Path });
+		}
+
+		public static BookCollection BookCollectionFactory(string path, BookCollection.CollectionType collectionType)
+		{
+			return new BookCollection(path, collectionType, null);
+		}
+	}
+}
