acab9f98fac27a149689ae8e784a2f011fdbf39c
andrew-polk <andrew_polk@sil.org>
2019-05-30 16:41:18 -0700

    Try to fix TeamCity build timeout by disabling WebpackBuildNotifierPlugin

--- src/BloomBrowserUI/webpack.core.js
+++ src/BloomBrowserUI/webpack.core.js
@@ -5,7 +5,7 @@
 // then we can move those webpack rules here.
 
 var webpack = require("webpack");
-var WebpackBuildNotifierPlugin = require("webpack-build-notifier");
+//var WebpackBuildNotifierPlugin = require("webpack-build-notifier");
 module.exports = {
     module: {
         rules: [
@@ -53,8 +53,8 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery",
             "window.jQuery": "jquery"
-        }),
-        new WebpackBuildNotifierPlugin({})
+        })
+        //new WebpackBuildNotifierPlugin({})
     ],
     resolve: {
         extensions: [".ts", ".tsx"]
