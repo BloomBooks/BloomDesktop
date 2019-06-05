a4a88c0abb989d91b65ffe9161ec5f0144472026
Stephen McConnel <stephen_mcconnel@sil.org>
2019-06-05 09:43:38 -0600

    Make only cover images transparent (BL-4816)

--- src/BloomExe/web/BloomServer.cs
+++ src/BloomExe/web/BloomServer.cs
@@ -411,7 +411,8 @@ namespace Bloom.Api
 			{
 				// thumbnail requests have the thumbnail parameter set in the query string
 				var thumb = info.GetQueryParameters()["thumbnail"] != null;
-				imageFile = _cache.GetPathToResizedImage(imageFile, thumb);
+				var transparent = CurrentBook?.ImageFileShouldBeRenderedWithTransparency(imageFile);
+				imageFile = _cache.GetPathToResizedImage(imageFile, thumb, transparent ?? false);
 
 				if (String.IsNullOrEmpty(imageFile)) return false;
 			}
