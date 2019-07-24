f84618e43609d5a7cc2246ec6462938a069e4579
Stephen McConnel <stephen_mcconnel@sil.org>
2019-07-24 17:14:15 -0600

    Add meta[@name="creator"] for use by harvester in bloomd files (BL-7409)
    The default value is "bloom" to indicate the .bloomd was built by the
    Bloom desktop app.  BloomHarvester will set the value as "harvester". (in
    a separate bloom-harvester commit, of course)

--- src/BloomExe/Publish/Android/BloomReaderFileMaker.cs
+++ src/BloomExe/Publish/Android/BloomReaderFileMaker.cs
@@ -51,11 +51,12 @@ namespace Bloom.Publish.Android
 		/// <param name="progress"></param>
 		/// <param name="tempFolder">A temporary folder. This function will not dispose of it when done</param>
 		/// <param name="hasEnterpriseFeatures"></param>
+		/// <param name="creator">value for &lt;meta name="creator" content="..."/&gt; (defaults to "bloom")</param>
 		/// <returns>Path to the unzipped .bloomd</returns>
 		public static string CreateBloomReaderBook(string outputPath, string bookFolderPath, BookServer bookServer, Color backColor,
-			WebSocketProgress progress, TemporaryFolder tempFolder, bool hasEnterpriseFeatures = false)
+			WebSocketProgress progress, TemporaryFolder tempFolder, bool hasEnterpriseFeatures = false, string creator="bloom")
 		{
-			var modifiedBook = PrepareBookForBloomReader(bookFolderPath, bookServer, tempFolder, progress, hasEnterpriseFeatures);
+			var modifiedBook = PrepareBookForBloomReader(bookFolderPath, bookServer, tempFolder, progress, hasEnterpriseFeatures, creator);
 			// We want at least 256 for Bloom Reader, because the screens have a high pixel density. And (at the moment) we are asking for
 			// 64dp in Bloom Reader.
 
@@ -68,7 +69,7 @@ namespace Bloom.Publish.Android
 		}
 
 		public static Book.Book PrepareBookForBloomReader(string bookFolderPath, BookServer bookServer, TemporaryFolder temp,
-			WebSocketProgress progress, bool hasEnterpriseFeatures)
+			WebSocketProgress progress, bool hasEnterpriseFeatures, string creator="bloom")
 		{
 			// MakeDeviceXmatterTempBook needs to be able to copy customCollectionStyles.css etc into parent of bookFolderPath
 			// And bloom-player expects folder name to match html file name.
@@ -114,6 +115,7 @@ namespace Bloom.Publish.Android
 			modifiedBook.SetAnimationDurationsFromAudioDurations();
 
 			modifiedBook.OurHtmlDom.SetMedia("bloomReader");
+			modifiedBook.OurHtmlDom.AddOrReplaceMetaElement("creator", creator);
 			EmbedFonts(modifiedBook, progress, new FontFileFinder());
 
 			var bookFile = BookStorage.FindBookHtmlInFolder(modifiedBook.FolderPath);
