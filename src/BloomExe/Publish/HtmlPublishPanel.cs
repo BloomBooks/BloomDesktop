4485c025452cc6879ba2089b99d381460971232c
Stephen McConnel <stephen_mcconnel@sil.org>
2019-08-26 10:31:29 -0600

    Handle hyperlinks in Bloom Reader Preview inside Bloom (BL-7569)

--- src/BloomExe/Publish/HtmlPublishPanel.cs
+++ src/BloomExe/Publish/HtmlPublishPanel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
 using SIL.PlatformUtilities;
+using Gecko;
+using Gecko.DOM;
 
 namespace Bloom.Publish
 {
@@ -22,6 +24,7 @@ namespace Bloom.Publish
 			// Has to be in front of the panel docked top for Fill to work.
 			_browser.BringToFront();
 			_browser.Navigate(pathToHtmlFile.ToLocalhost() + GetUrlParams(), false);
+			_browser.OnBrowserClick += HandleExternalLinkClick;
 
 			VisibleChanged += OnVisibleChanged;
 		}
@@ -39,6 +42,44 @@ namespace Bloom.Publish
 			{
 				Deactivate();
 			}
+		}
+
+		/// <summary>
+		/// Detect clicks on anchor elements and handle them by passing the href to the default system browser.
+		/// </summary>
+		/// <remarks>
+		/// See https://issues.bloomlibrary.org/youtrack/issue/BL-7569.
+		/// Note that Readium handles these clicks internally and we never get to see them.  The relevant Readium
+		/// code is apparently in readium-js-viewer/readium-js/readium-shared-js/js/views/internal_links_support.js
+		/// in the function this.processLinkElements.
+		/// </remarks>
+		private void HandleExternalLinkClick(object sender, EventArgs e)
+		{
+			var ge = e as DomEventArgs;
+			if (ge == null || ge.Target == null)
+				return;
+			GeckoHtmlElement target;
+			try
+			{
+				target = (GeckoHtmlElement)ge.Target.CastToGeckoElement();
+			}
+			catch (InvalidCastException)
+			{
+				return;
+			}
+			var anchor = target as GeckoAnchorElement;
+			if (anchor == null)
+				anchor = target.Parent as GeckoAnchorElement;	// Might be a span inside an anchor
+			if (anchor != null && !String.IsNullOrEmpty(anchor.Href) &&
+				// Handle only http(s) and mailto protocols.
+				(anchor.Href.ToLowerInvariant().StartsWith("http") || anchor.Href.ToLowerInvariant().StartsWith("mailto")) &&
+				// Don't try to handle localhost Bloom requests.
+				!anchor.Href.ToLowerInvariant().StartsWith(Bloom.Api.BloomServer.ServerUrlWithBloomPrefixEndingInSlash))
+			{
+				SIL.Program.Process.SafeStart(anchor.Href);
+				ge.Handled = true;
+			}
+			// All other clicks get normal processing...
 		}
 
 		private void Deactivate()
