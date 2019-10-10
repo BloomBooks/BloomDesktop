d103a2723fb04729e04f5c57e905427d27ea2de1
Unknown <jeffrey_su@sil.org>
2019-10-10 13:35:45 -0500

    Convert Callout Tool from React Class Component to React Hooks
    Early Prototype of Callout Tool using React hooks
    
    Turning on/off bubble editing when the toolbox is opened/closed works. Can switch between bubbles on canvas and it'll be updated in toolbox. You can update the options in toolbox and it'll update the canvas, but some wonky bubble-switching behavior if modifying not the 1st bubble selected.

--- src/BloomBrowserUI/bookEdit/toolbox/callout/calloutControls.tsx
+++ src/BloomBrowserUI/bookEdit/toolbox/callout/calloutControls.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import ToolboxToolReactAdaptor from "../toolboxToolReactAdaptor";
 import * as ReactDOM from "react-dom";
 import "./Callout.less";
@@ -6,72 +7,174 @@ import { getPageFrameExports } from "../../js/bloomFrames";
 import { TextOverPictureManager } from "../../js/textOverPicture";
 import { RadioGroup } from "../../../react_components/RadioGroup";
 import { BubbleSpec } from "comical-js//bubbleSpec";
+import { ToolBottomHelpLink } from "../../../react_components/helpLink";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { MenuItem } from "@material-ui/core";
+import { values } from "mobx";
+import { Div } from "../../../react_components/l10nComponents";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"; // TODO: Am I really needed?
+import InputLabel from "@material-ui/core/InputLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
 
-interface IComponentState {
+interface ICalloutToolProps {
     style: string;
     bubbleActive: boolean;
 }
 
-// These classes support the Callouts toolbox, which allows control of callouts (e.g. cartoon bubbles) added to images.
-export class CalloutToolControls extends React.Component<{}, IComponentState> {
-    constructor(props: Readonly<{}>) {
-        super(props);
-    }
+const CalloutToolControls: React.FunctionComponent<ICalloutToolProps> = (
+    props: ICalloutToolProps
+) => {
+    const [style, setStyle] = useState(props.style);
+    const [bubbleActive, setBubbleActive] = useState(props.bubbleActive);
 
-    public readonly state: IComponentState = {
-        style: "speech",
-        bubbleActive: true
+    // TODO: Where should i move?
+    const oneTimeBubbleSpecInitialization = () => {
+        const bubbleManager = CalloutTool.bubbleManager();
+        if (!bubbleManager) {
+            // probably the toolbox just finished loading before the page.
+            // No clean way to fix this
+            window.setTimeout(() => this.newPageReady(), 100);
+            return;
+        }
+        bubbleManager.turnOnBubbleEditing();
+
+        const bubbleSpec = bubbleManager.getSelectedItemBubbleSpec();
+
+        bubbleManager.requestBubbleChangeNotification(
+            (bubble: BubbleSpec | undefined) => {
+                // TODO: is this getting called an excessive amount of times?
+                console.log("RequestBubbleChangeNotification callback called.");
+
+                // TODO: Ugh, this is a circular dependency-like scenario :(
+                //setActiveBubbleSpec(bubble);
+
+                if (bubble) {
+                    setStyle(bubble.style);
+                    setBubbleActive(true);
+                }
+            }
+        );
+        return bubbleSpec;
     };
 
-    public updateBubbleState(bubble: BubbleSpec | undefined) {
-        this.setState({
-            style: bubble ? bubble.style : "none",
-            bubbleActive: !!bubble
-        });
-    }
+    const [activeBubbleSpec, setActiveBubbleSpec] = useState(
+        oneTimeBubbleSpecInitialization()
+    );
+    useEffect(() => {
+        if (activeBubbleSpec) {
+            // TODO: Do nice things with me.
+            setStyle(activeBubbleSpec.style);
+            setBubbleActive(true);
+        }
 
-    public render() {
-        return (
-            <div className={this.state.bubbleActive ? "" : "disabled"}>
-                <RadioGroup
-                    value={this.state.style}
-                    onChange={s => this.setBubbleStyle(s)}
-                    choices={{
-                        none: "No bubble",
-                        speech: "An oval bubble",
-                        shout: "A jagged bubble for shouting"
-                    }}
-                />
+        // Return value is the Cleanup function
+        return () => {
+            CalloutTool.bubbleManager().turnOffBubbleEditing();
+        };
+    }, [activeBubbleSpec]);
+
+    const handleStyleChanged = event => {
+        const newStyle = event.target.value;
+        setStyle(newStyle);
+        CalloutTool.bubbleManager().updateSelectedItemBubbleSpec({
+            style: newStyle
+        });
+    };
+
+    return (
+        <div>
+            <div id={"calloutControlShapeChooserRegion"}>
+                Drag one of these on top of an image: TODO: Implement dragger
             </div>
-        );
-    }
-
-    public static bubbleManager(): TextOverPictureManager {
-        return getPageFrameExports().getTheOneBubbleManager();
-    }
-
-    private setBubbleStyle(s: string): void {
-        this.setState({ style: s });
-        CalloutToolControls.bubbleManager().updateSelectedItemBubbleSpec({
-            style: s
-        });
-    }
-
-    public static setup(root): CalloutToolControls {
-        return (ReactDOM.render(
-            <CalloutToolControls />,
-            root
-        ) as unknown) as CalloutToolControls;
-    }
-}
+            <div
+                id={"calloutControlOptionsRegion"}
+                className={bubbleActive ? "" : "disabled"}
+            >
+                <Div l10nKey="EditTab.Toolbox.CalloutTool.Options.ControlsAvailable">
+                    The selected item has these controls:
+                </Div>
+                <br />
+                <Div l10nKey="EditTab.Toolbox.CalloutTool.Options.Style">
+                    Style
+                </Div>
+                <form autoComplete="off">
+                    <FormControl>
+                        {/* TODO: How to localize these? */}
+                        <InputLabel htmlFor="callout-style-dropdown">
+                            Style
+                        </InputLabel>
+                        <Select
+                            value={style}
+                            onChange={event => {
+                                handleStyleChanged(event);
+                            }}
+                            className="calloutOptionDropdown"
+                            inputProps={{
+                                name: "style",
+                                id: "callout-style-dropdown"
+                            }}
+                        >
+                            <MenuItem value="caption">Caption (TODO)</MenuItem>
+                            <MenuItem value="shout">Exclamation</MenuItem>
+                            <MenuItem value="none">Just Text</MenuItem>
+                            <MenuItem value="speech">Speech</MenuItem>
+                            <MenuItem value="thought">Thought (TODO)</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="callout-textColor-dropdown">
+                            Text Color
+                        </InputLabel>
+                        <Select
+                            value={"white"}
+                            className="calloutOptionDropdown"
+                            inputProps={{
+                                name: "textColor",
+                                id: "callout-textColor-dropdown"
+                            }}
+                        >
+                            <MenuItem value="white">White</MenuItem>
+                            <MenuItem value="black">Black</MenuItem>
+                        </Select>
+                    </FormControl>
+                </form>
+            </div>
+            <div id={"calloutControlFooterRegion"}>
+                {/* TODO: Update the help link */}
+                <ToolBottomHelpLink helpId="Tasks/Edit_tasks/Impairment_Visualizer/Impairment_Visualizer_overview.htm" />
+            </div>
+        </div>
+    );
+};
+export default CalloutToolControls;
 
 export class CalloutTool extends ToolboxToolReactAdaptor {
-    private reactControls: CalloutToolControls;
+    //private reactControls: CalloutToolControls;
+    private componentProps: ICalloutToolProps;
+
+    public constructor() {
+        super();
+
+        // Just a default value
+        this.componentProps = {
+            style: "none",
+            bubbleActive: false
+        };
+    }
 
     public makeRootElement(): HTMLDivElement {
         const root = document.createElement("div");
         root.setAttribute("class", "CalloutBody");
-        this.reactControls = CalloutToolControls.setup(root);
+
+        //ReactDOM.render(<CalloutToolControls {...this.componentProps} />, root);
+        ReactDOM.render(
+            <CalloutToolControls
+                style={this.componentProps.style}
+                bubbleActive={this.componentProps.bubbleActive}
+            />,
+            root
+        );
         return root as HTMLDivElement;
     }
 
@@ -95,27 +198,58 @@ export class CalloutTool extends ToolboxToolReactAdaptor {
     }
 
     public newPageReady() {
-        const bubbleManager = CalloutToolControls.bubbleManager();
+        console.log("New page ready");
+        CalloutTool.newPageReadyStaticHelper();
+
+        // Or should it attempt to force re-render?
+        // What if the bubbleSpec changes?
+
+        // const bubbleManager = CalloutTool.bubbleManager();
+        // if (!bubbleManager) {
+        //     // probably the toolbox just finished loading before the page.
+        //     // No clean way to fix this
+        //     window.setTimeout(() => this.newPageReady(), 100);
+        //     return;
+        // }
+        // bubbleManager.turnOnBubbleEditing();
+        // const bubbleSpec = bubbleManager.getSelectedItemBubbleSpec();
+        // if (bubbleSpec) {
+        //     this.componentProps.bubbleActive = true;
+        //     this.componentProps.style = bubbleSpec.style;
+        // }
+        //     <CalloutToolControls {...props} />;
+        // }
+        // if (this.reactControls) {
+        //     this.reactControls.updateBubbleState(
+        //         bubbleManager.getSelectedItemBubbleSpec()
+        //     );
+        // }
+        // bubbleManager.requestBubbleChangeNotification(bubble => {
+        //     if (this.reactControls) {
+        //         this.reactControls.updateBubbleState(bubble);
+        //     }
+        // });
+    }
+
+    public static newPageReadyStaticHelper() {
+        const bubbleManager = CalloutTool.bubbleManager();
         if (!bubbleManager) {
             // probably the toolbox just finished loading before the page.
             // No clean way to fix this
-            window.setTimeout(() => this.newPageReady(), 100);
+            window.setTimeout(
+                () => CalloutTool.newPageReadyStaticHelper(),
+                100
+            );
             return;
         }
         bubbleManager.turnOnBubbleEditing();
-        if (this.reactControls) {
-            this.reactControls.updateBubbleState(
-                bubbleManager.getSelectedItemBubbleSpec()
-            );
-        }
-        bubbleManager.requestBubbleChangeNotification(bubble => {
-            if (this.reactControls) {
-                this.reactControls.updateBubbleState(bubble);
-            }
-        });
     }
 
     public detachFromPage() {
-        CalloutToolControls.bubbleManager().turnOffBubbleEditing();
+        CalloutTool.bubbleManager().turnOffBubbleEditing();
+    }
+
+    public static bubbleManager(): TextOverPictureManager {
+        return getPageFrameExports().getTheOneBubbleManager();
     }
 }
