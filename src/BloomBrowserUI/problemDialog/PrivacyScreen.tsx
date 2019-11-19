56cc13589cf8ae18bf8cf23db07cf32c5eaa16ef
Gordon Martin <martigor@gmail.com>
2019-11-19 13:45:45 -0800

    Use new React hook for localization

--- src/BloomBrowserUI/problemDialog/PrivacyScreen.tsx
+++ src/BloomBrowserUI/problemDialog/PrivacyScreen.tsx
@@ -3,6 +3,7 @@ import { Button, Typography } from "@material-ui/core";
 import "./ProblemDialog.less";
 import ArrowBack from "@material-ui/icons/ArrowBack";
 import { BloomApi } from "../utils/bloomApi";
+import { useL10n } from "../react_components/l10nHooks";
 
 export const PrivacyScreen: React.FunctionComponent<{
     includeBook: boolean;
@@ -16,22 +17,35 @@ export const PrivacyScreen: React.FunctionComponent<{
         }&email=${props.email}&userInput=${props.userInput}`,
         "Loading..."
     );
+    const privateEmailAddress = "private@bloomlibrary.org";
+    const localizedBack = useL10n("Back", "Common.BackButton");
+    const localizedPrivacyInfoMessage = useL10n(
+        "Your report will go into our issue tracking system and will be visible via the web. If you have something private to say, please email to '{0}'.",
+        "ReportProblemDialog.PrivacyInfo",
+        undefined,
+        privateEmailAddress
+    );
+    const localizedPrivacyIntro = useL10n(
+        "Bloom will include the following diagnostic information with your report:",
+        "ReportProblemDialog.DiagnosticReportIntro"
+    );
+
     return (
         <div id="privacyDetails">
             <Button
+                // We are not using BloomButton here because it doesn't handle the Mui ArrowBack icon.
                 variant="contained"
                 color="primary"
                 onClick={() => props.onBack()}
             >
                 <ArrowBack />
-                Back
+                {localizedBack}
             </Button>
             <Typography className="privacy_info">
-                {`Your report will go into our issue tracking system and will be visible via the the web. If you have something private to say, please email to private@${"bloomlibrary"}.org.`}
+                {localizedPrivacyInfoMessage}
             </Typography>
             <Typography className="intro_to_report">
-                Bloom will include the following diagnostic information with
-                your report:
+                {localizedPrivacyIntro}
             </Typography>
             <code id="report">{log}</code>
         </div>
