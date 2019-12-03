c43ed9e1461dd2063b9726d7ce510dd024d9d686
John Thomson <john_thomson@sil.org>
2019-12-03 11:07:29 -0600

    Generate FeaturesRequired record for comic elements (BL-7795)

--- src/BloomExe/Book/BookStorage.cs
+++ src/BloomExe/Book/BookStorage.cs
@@ -330,11 +330,10 @@ namespace Bloom.Book
 			}
 			BookInfo.FormatVersion = formatVersion;
 
-			VersionRequirement[] requiredVersions = GetRequiredVersions(Dom).ToArray();
-			if (requiredVersions != null && requiredVersions.Length >= 1)
+			var requiredVersions = GetRequiredVersionsString(Dom);
+			if (!string.IsNullOrEmpty(requiredVersions))
 			{
-				string json = JsonConvert.SerializeObject(requiredVersions);
-				Dom.UpdateMetaElement("FeatureRequirement", json);
+				Dom.UpdateMetaElement("FeatureRequirement", requiredVersions);
 			}
 			else
 			{
@@ -657,21 +656,85 @@ namespace Bloom.Book
 			}
 		}
 
+		// It would be nice if this could simply extend VersionRequirement, but structs don't have
+		// inheritance, and I'm not sure why it was made a struct: it may make some difference
+		// to how it is serialized to JSON. And I don't want to risk that because the JSON has
+		// to be read by older versions of Bloom.
+		class Feature
+		{
+			public string BloomDesktopMinVersion { get; set; }
+			public string BloomReaderMinVersion { get; set; }
+			public string FeatureId { get; set; }
+			public string FeaturePhrase { get; set; }
+
+			public string XPath { get; set; }
+		}
+
+		static Feature[] _features =
+		{
+			new Feature() {FeatureId = "wholeTextBoxAudio",
+				FeaturePhrase = "Whole Text Box Audio",
+				BloomDesktopMinVersion = "4.4",
+				BloomReaderMinVersion = "1.0",
+				XPath = "//*[@data-audiorecordingmode='TextBox']"
+			},
+			new Feature() {FeatureId = "comical-1",
+				FeaturePhrase = "Support for Comics",
+				BloomDesktopMinVersion = "4.7",
+				BloomReaderMinVersion = "1.0",
+				// We're looking for a text-over-picture box with non-trivial data-bubble,
+				// but as a special case ones with style none are OK, as these can continue to work
+				// as old-style TOP boxes, with the data-bubble ignored.
+				XPath = "//*[contains(@class, 'bloom-textOverPicture') and contains(@data-bubble, '`style`:') and not(contains(@data-bubble, '`style`:`none`'))]"
+			}
+		};
+
+		// We should keep somewhat aware of how long this takes to run.
+		// It's part of every page save, even the 'current page only' ones.
+		// Currently (Dec 2019, with two features), it's usually too fast to measure (reported as 0ms) in a 20-page book,
+		// but around 50ms in a 200-page one (the picture dictionary from BL-7253).
+		// That's on a fast machine, where a full save of the picture dictionary takes
+		// about 2.5s, and a page-only save about half a second, so it's running 10% of
+		// our page-change time.
+		// An easy optimization would be to calculate this string on the current page DOM
+		// when we start and stop editing it. Both should take less than a millisecond.
+		// If it doesn't change, I _think_ it's safe to conclude that edits to this page
+		// would not affect the result for the whole book. However, calculating for the whole
+		// book and comparing with what's currently stored is simpler and safer if we
+		// can live with a modest performance cost for very large books.
+		public static string GetRequiredVersionsString(HtmlDom dom)
+		{
+			var watch = new Stopwatch();
+			watch.Start();
+			var result = "";
+			var requiredVersions = GetRequiredVersions(dom).ToArray();
+			if (requiredVersions != null && requiredVersions.Length >= 1)
+			{
+				result = JsonConvert.SerializeObject(requiredVersions);
+			}
+			watch.Stop();
+			Debug.WriteLine("GetRequiredVersionsString took " + watch.ElapsedMilliseconds + "ms");
+			return result;
+		}
+
 		// Determines which features will have serious breaking effects if not opened in the proper version of any relevant Bloom products
 		// Note: This should include not only BloomDesktop considerations, but needs to insert enough information for things like BloomReader to be able to figure it out too
 		public static IOrderedEnumerable<VersionRequirement> GetRequiredVersions(HtmlDom dom)
 		{
 			var reqList = new List<VersionRequirement>();
 
-			if (dom.DoesContainNarrationAudioRecordedUsingWholeTextBox())
+			foreach (var feature in _features)
 			{
-				reqList.Add(new VersionRequirement()
+				if (dom.SelectSingleNode(feature.XPath) != null)
 				{
-					FeatureId = "wholeTextBoxAudio",
-					FeaturePhrase = "Whole Text Box Audio",
-					BloomDesktopMinVersion = "4.4",
-					BloomReaderMinVersion = "1.0"
-				});
+					reqList.Add(new VersionRequirement()
+					{
+						FeatureId = feature.FeatureId,
+						FeaturePhrase = feature.FeaturePhrase,
+						BloomDesktopMinVersion = feature.BloomDesktopMinVersion,
+						BloomReaderMinVersion = feature.BloomReaderMinVersion
+					});
+				}
 			}
 
 			return reqList.OrderByDescending(x => x.BloomDesktopMinVersion);
