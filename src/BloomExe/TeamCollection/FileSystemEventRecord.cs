78bfa8d729d207ea244b93402e3e54b52c9225c1
Jeffrey Su <41497174+sujeffreyl@users.noreply.github.com>
2021-02-24 17:17:25 -0600

    TC: Try to filter out Changed filesystem events right after Created ones

---
+++ src/BloomExe/TeamCollection/FileSystemEventRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Bloom.TeamCollection
+{
+	internal class FileSystemEventRecord
+	{
+		public FileSystemEventArgs  EventArgs { get; set; }
+		public DateTime Timestamp { get; set; }
+
+		public FileSystemEventRecord(FileSystemEventArgs eventArgs, DateTime? timestamp = null)
+		{
+			EventArgs = eventArgs;
+			Timestamp = timestamp ?? DateTime.Now;
+		}
+	}
+}
