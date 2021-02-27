3370fb24fb2913a07bc0d2a3641a58befcf06243
John Hatton <hattonjohn@gmail.com>
2021-02-26 17:49:56 -0700

    Add basic dummy buttons to react book list

--- src/BloomBrowserUI/collectionTab/BookListPane.tsx
+++ src/BloomBrowserUI/collectionTab/BookListPane.tsx
@@ -1,5 +1,39 @@
+import Grid from "@material-ui/core/Grid";
 import React = require("react");
+import "BookListPane.less";
+import Paper from "@material-ui/core/Paper";
 
 export const BookListPane: React.FunctionComponent<{}> = () => {
-    return <div>Hello BookListPane</div>;
+    return (
+        <div className="bookButtonPane">
+            <h1>FooBar Books</h1>
+            <Grid
+                container={true}
+                spacing={3}
+                direction="row"
+                justify="flex-start"
+                alignItems="flex-start"
+            >
+                <BookButton />
+                <BookButton />
+                <BookButton />
+                <BookButton />
+                <BookButton />
+                <BookButton />
+                <BookButton />
+            </Grid>
+        </div>
+    );
+};
+
+export const BookButton: React.FunctionComponent<{}> = () => {
+    return (
+        <Grid item={true}>
+            <div className="bookButton">
+                <div className="imgFake" />
+                <div className="bookTitle">The Moon and the Cap3</div>
+            </div>
+            {/* <div className="BookButton">Book</div>; */}
+        </Grid>
+    );
 };
