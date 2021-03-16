8bc1b63218c791c653780609513884a813485a7c
Gordon Martin <gordon_martin@sil.org>
2021-03-16 11:40:04 -0700

    BL-9044 another auto update tweak

--- src/BloomBrowserUI/react_components/AutoUpdateSoftwareDialog.tsx
+++ src/BloomBrowserUI/react_components/AutoUpdateSoftwareDialog.tsx
@@ -4,7 +4,7 @@ import { BloomApi } from "../utils/bloomApi";
 import "./AutoUpdateSoftwareDialog.less";
 import theme from "../bloomMaterialUITheme";
 import { ThemeProvider } from "@material-ui/styles";
-import { FormControlLabel, Radio, RadioGroup } from "@material-ui/core";
+import { FormControlLabel, Paper, Radio, RadioGroup } from "@material-ui/core";
 import { L10nLabel } from "./L10nLabel";
 import { useEffect, useState } from "react";
 
@@ -27,68 +27,73 @@ export const AutoUpdateSoftwareDialog: React.FunctionComponent = () => {
 
     return (
         <ThemeProvider theme={theme}>
-            <div className="auto-update-dialog">
-                <L10nLabel
-                    variant="h4"
-                    className="dialog-title"
-                    english="Software Updates"
-                    l10nKey="AutoUpdateSoftwareDialog.SoftwareUpdates"
-                />
-                <L10nLabel
-                    variant="h5"
-                    className="main-question"
-                    english="What should Bloom do when a new version is available?"
-                    l10nKey="AutoUpdateSoftwareDialog.WhatShouldBloomDo"
-                />
-                <RadioGroup
-                    aria-label="software update choices"
-                    name="choices"
-                    value={chosenRadio}
-                    onChange={handleChange}
-                >
-                    <FormControlLabel
-                        value="automatic"
-                        control={<Radio color="primary" />}
-                        label={
-                            <L10nLabel
-                                variant="h5"
-                                english="Automatically download and install it"
-                                l10nKey="AutoUpdateSoftwareDialog.AutomaticUpdate"
-                            />
-                        }
+            <Paper elevation={3} className="canvas">
+                <div className="auto-update-dialog">
+                    <L10nLabel
+                        variant="h4"
+                        className="dialog-title"
+                        english="Software Updates"
+                        l10nKey="AutoUpdateSoftwareDialog.SoftwareUpdates"
                     />
-                    <FormControlLabel
-                        className="inform-label"
-                        value="inform"
-                        control={<Radio color="primary" />}
-                        label={
-                            <L10nLabel
-                                variant="h5"
-                                english="Just let me know about the new version"
-                                l10nKey="AutoUpdateSoftwareDialog.LetMeKnow"
-                            />
-                        }
+                    <L10nLabel
+                        variant="h5"
+                        className="main-question"
+                        english="What should Bloom do when a new version is available?"
+                        l10nKey="AutoUpdateSoftwareDialog.WhatShouldBloomDo"
                     />
-                </RadioGroup>
-                <div className="spacer" />
-                <div className="align-right-bottom">
-                    <BloomButton
-                        className="ok-button"
-                        l10nKey="Common.OK"
-                        enabled={true}
-                        hasText={true}
-                        onClick={() => {
-                            BloomApi.postData("app/autoUpdateSoftwareChoice", {
-                                dialogShown: 1,
-                                autoUpdate: isAutoUpdate
-                            });
-                            BloomApi.post("common/closeReactDialog");
-                        }}
+                    <RadioGroup
+                        aria-label="software update choices"
+                        name="choices"
+                        value={chosenRadio}
+                        onChange={handleChange}
                     >
-                        OK
-                    </BloomButton>
+                        <FormControlLabel
+                            value="automatic"
+                            control={<Radio color="primary" />}
+                            label={
+                                <L10nLabel
+                                    variant="h5"
+                                    english="Automatically download and install it"
+                                    l10nKey="AutoUpdateSoftwareDialog.AutomaticUpdate"
+                                />
+                            }
+                        />
+                        <FormControlLabel
+                            className="inform-label"
+                            value="inform"
+                            control={<Radio color="primary" />}
+                            label={
+                                <L10nLabel
+                                    variant="h5"
+                                    english="Just let me know about the new version"
+                                    l10nKey="AutoUpdateSoftwareDialog.LetMeKnow"
+                                />
+                            }
+                        />
+                    </RadioGroup>
+                    <div className="spacer" />
+                    <div className="align-right-bottom">
+                        <BloomButton
+                            className="ok-button"
+                            l10nKey="Common.OK"
+                            enabled={true}
+                            hasText={true}
+                            onClick={() => {
+                                BloomApi.postData(
+                                    "app/autoUpdateSoftwareChoice",
+                                    {
+                                        dialogShown: 1,
+                                        autoUpdate: isAutoUpdate
+                                    }
+                                );
+                                BloomApi.post("common/closeReactDialog");
+                            }}
+                        >
+                            OK
+                        </BloomButton>
+                    </div>
                 </div>
-            </div>
+            </Paper>
         </ThemeProvider>
     );
 };
