2af63bf54c7e5c1aa0709a214b2f1b947ed7f4b4
Stephen McConnel <stephen_mcconnel@sil.org>
2021-03-23 15:01:40 -0600

    Fix system memory check for Linux (20210323)

--- src/BloomExe/Utils/MemoryUtils.cs
+++ src/BloomExe/Utils/MemoryUtils.cs
@@ -16,22 +16,25 @@ namespace Bloom.Utils
 		public static bool SystemIsShortOfMemory()
 		{
 			// A rather arbitrary limit of 750M...a bit more than Bloom typically uses for a large book
-			// before memory leaks start to mount up.
-			return GetPrivateBytes() > 750000000;
+			// before memory leaks start to mount up.  A larger value is wanted for 64-bit processes
+			// since they can start out above the 750M level.
+			var triggerLevel = Environment.Is64BitProcess ? 2000000000L : 750000000;
+			return GetPrivateBytes() > triggerLevel;
 		}
 
 		/// <summary>
-		/// Significance: This counter indicates the current number of bytes allocated to this process that cannot be shared with
-		/// other processes. This counter has been useful for identifying memory leaks.
+		/// Significance: This value indicates the current number of bytes allocated to this process that cannot be shared with
+		/// other processes. This value has been useful for identifying memory leaks.
 		/// </summary>
 		/// <remarks>We've had other versions of this method which, confusingly, returned results in KB. This one actually answers bytes.</remarks>
-		/// <returns></returns>
 		public static long GetPrivateBytes()
 		{
-			using (var perfCounter = new PerformanceCounter("Process", "Private Bytes",
-				Process.GetCurrentProcess().ProcessName))
+			// Using a PerformanceCounter does not work on Linux and gains nothing on Windows.
+			// After using the PerformanceCounter once on Windows, it always returns the same
+			// value as getting it directly from the Process property.
+			using (var process = Process.GetCurrentProcess())
 			{
-				return perfCounter.RawValue;
+				return process.PrivateMemorySize64;
 			}
 		}
 	}
