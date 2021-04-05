5f2caffc86552440b452ace02014ec7ea6de7704
John Hatton <hattonjohn@gmail.com>
2021-04-05 16:54:42 -0600

    Give actions semi-distinct colors in PerfLog

--- src/BloomBrowserUI/performance/PerformanceLogPage.tsx
+++ src/BloomBrowserUI/performance/PerformanceLogPage.tsx
@@ -82,7 +82,8 @@ const MemoryGraph: React.FunctionComponent<{
 }> = props => {
     const memoryLevels = props.measurements.map((m, index) => ({
         x: index,
-        y: m.privateBytes / 1000000
+        y: m.privateBytes / 1000000,
+        action: m.action
     }));
     return (
         <div className="graph">
@@ -147,16 +148,19 @@ const MemoryGraph: React.FunctionComponent<{
                         data: memoryLevels
                     }
                 ]}
+                renderNode={CustomNodeWithColorDependingOnAction}
             />
         </div>
     );
 };
+
 const DurationGraph: React.FunctionComponent<{
     measurements: IMeasurement[];
 }> = props => {
     const durations = props.measurements.map((m, index) => ({
         x: index,
-        y: m.duration
+        y: m.duration,
+        action: m.action
     }));
     return (
         <div className="graph">
@@ -214,8 +218,77 @@ const DurationGraph: React.FunctionComponent<{
                         data: durations
                     }
                 ]}
+                renderNode={CustomNodeWithColorDependingOnAction}
             />
         </div>
+    );
+};
+
+interface IHash {
+    [actionLabel: string]: string;
+}
+const ActionToColor: IHash = {};
+
+const CustomNodeWithColorDependingOnAction = ({
+    node,
+    x,
+    y,
+    size,
+    color,
+    blendMode,
+    onMouseEnter,
+    onMouseMove,
+    onMouseLeave,
+    onClick
+}) => {
+    const colors = [
+        "#a6cee3",
+        "#6a3d9a",
+        "#1f78b4",
+        "#33a02c",
+        "#ff7f00",
+        "#e31a1c",
+        "#d33682",
+        "#6c71c4",
+        "#268bd2",
+        "#2aa198",
+        "#a09e2c",
+        "#b15928",
+        "#fb8072",
+        "#b2df8a",
+        "#f5a3a1",
+        "#fdbf6f",
+        "#cab2d6",
+        "#8dd3c7",
+        "#bebada",
+
+        "#80b1d3",
+        "#fdb462",
+        "#b3de69",
+        "#fccde5",
+        "#d9d9d9",
+        "#bc80bd",
+        "#ccebc5"
+    ];
+    const action = node.data.action;
+    const l: number = Object.keys(ActionToColor).length;
+    if (!ActionToColor[action]) {
+        // Each new distinct action type gets the next color, until we run out of colors, then we start over
+        ActionToColor[action] = colors[l % colors.length];
+    }
+    const customColor = ActionToColor[action];
+    return (
+        <g transform={`translate(${x},${y})`}>
+            <circle
+                r={size / 2}
+                fill={customColor}
+                style={{ mixBlendMode: blendMode }}
+                onMouseEnter={onMouseEnter}
+                onMouseMove={onMouseMove}
+                onMouseLeave={onMouseLeave}
+                onClick={onClick}
+            />
+        </g>
     );
 };
 
