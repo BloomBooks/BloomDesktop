4f5d54cef47d3cac69d94ef8f4f0ac7e84a76d67
John Thomson <john_thomson@sil.org>
2021-05-24 14:34:27 -0500

    Report if user has old version of ACE (BL-9924)

--- src/BloomExe/web/controllers/AccessibilityCheckApi.cs
+++ src/BloomExe/web/controllers/AccessibilityCheckApi.cs
@@ -2,12 +2,15 @@
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Windows.Forms;
 using Bloom.Api;
 using Bloom.Book;
 using Bloom.Publish.AccessibilityChecker;
 using Bloom.Publish.Epub;
 using SIL.CommandLineProcessing;
+using SIL.IO;
 using SIL.PlatformUtilities;
 using SIL.Progress;
 
@@ -168,7 +171,8 @@ namespace Bloom.web.controllers
 			// https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css.
 			if (!UrlLookup.CheckGeneralInternetAvailability(true))
 			{
-				_webSocketProgress.ErrorWithoutLocalizing("Sorry, you must have an internet connection in order to view the Ace by DAISY report.");
+				_webSocketProgress.ErrorWithoutLocalizing(
+					"Sorry, you must have an internet connection in order to view the Ace by DAISY report.");
 				request.Failed();
 				return;
 			}
@@ -185,69 +189,118 @@ namespace Bloom.web.controllers
 			var haveReportedError = false;
 			var errorMessage = "Unknown Error";
 
+			var version = GetAceByDaisyVersion(daisyDirectory);
+			if (version.old)
+			{
+				_webSocketProgress.MessageWithoutLocalizing(
+					$"You appear to have an older version ({version.version}) of ACE by Daisy. This may cause problems.",
+					ProgressKind.Warning);
+				_webSocketProgress.MessageWithoutLocalizing(
+					"We recommend you run \"npm install @daisy/ace -g\" from a command line to get the latest.",
+					ProgressKind.Warning);
+			}
+
+			var started = DateTime.Now;
+
 			var epubPath = MakeEpub(reportRootDirectory, _webSocketProgress);
 			// Try 3 times. It could be that this is no longer needed, but working on a developer
 			// machine isn't proof.
 			for (var i = 0; i < 3; i++)
+			{
+				var randomName = Guid.NewGuid().ToString();
+				var reportDirectory = Path.Combine(reportRootDirectory, randomName);
+
+				var arguments = $"ace.js --verbose -o \"{reportDirectory}\" \"{epubPath}\"";
+				const int kSecondsBeforeTimeout = 60;
+				var progress = new NullProgress();
+				_webSocketProgress.MessageWithoutLocalizing("Running Ace by DAISY");
+
+				ExecutionResult res = null;
+				string ldpath = null;
+				try
 				{
-					var randomName = Guid.NewGuid().ToString();
-					var reportDirectory = Path.Combine(reportRootDirectory, randomName);
+					// Without this variable switching on Linux, the chrome inside ace finds the
+					// wrong version of a library as part of our mozilla code.
+					ldpath = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH");
+					Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", null);
+					res = CommandLineRunner.Run("node", arguments, Encoding.UTF8, daisyDirectory, kSecondsBeforeTimeout,
+						progress,
+						(dummy) => { });
+				}
+				finally
+				{
+					// Restore the variable for our next geckofx browser to find.
+					if (!String.IsNullOrEmpty(ldpath))
+						Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", ldpath);
+				}
 
-					var arguments = $"ace.js --verbose -o \"{reportDirectory}\" \"{epubPath}\"";
-					const int kSecondsBeforeTimeout = 60;
-					var progress = new NullProgress();
-					_webSocketProgress.MessageWithoutLocalizing("Running Ace by DAISY");
+				if (res.DidTimeOut)
+				{
+					errorMessage = $"Daisy Ace timed out after {kSecondsBeforeTimeout} seconds.";
+					_webSocketProgress.ErrorWithoutLocalizing(errorMessage);
+					continue;
+				}
 
-					ExecutionResult res = null;
-					string ldpath = null;
-					try
-					{
-						// Without this variable switching on Linux, the chrome inside ace finds the
-						// wrong version of a library as part of our mozilla code.
-						ldpath = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH");
-						Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", null);
-						res = CommandLineRunner.Run("node", arguments, Encoding.UTF8, daisyDirectory, kSecondsBeforeTimeout, progress,
-							(dummy) => { });
-					}
-					finally
-					{
-						// Restore the variable for our next geckofx browser to find.
-						if (!String.IsNullOrEmpty(ldpath))
-							Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", ldpath);
-					}
+				var answerPath = Path.Combine(reportDirectory, "report.html");
+				if (!File.Exists(answerPath))
+				{
+					// This hasn't been effectively reproduced, but there was a case where this would fail at least
+					// half the time on a book, reproducable. That book had 2 pages pointing at placeholder.png,
+					// and we were getting an error related to it being locked. So we deduce that ace was trying
+					// to copy the file twice, at the same time (normal nodejs code is highly async).
+					// Now the problem is not reproducable, but I'm leaving in this code that tried to deal with it.
+					errorMessage = $"Exit code{res.ExitCode}{Environment.NewLine}" +
+					               $"Standard Error{Environment.NewLine}{res.StandardError}{Environment.NewLine}" +
+					               $"Standard Out{res.StandardOutput}";
 
-					if (res.DidTimeOut)
-					{
-						errorMessage = $"Daisy Ace timed out after {kSecondsBeforeTimeout} seconds.";
-						_webSocketProgress.ErrorWithoutLocalizing(errorMessage);
-						continue;
-					}
+					_webSocketProgress.ErrorWithoutLocalizing(errorMessage);
 
-					var answerPath = Path.Combine(reportDirectory, "report.html");
-					if (!File.Exists(answerPath))
-					{
-						// This hasn't been effectively reproduced, but there was a case where this would fail at least
-						// half the time on a book, reproducable. That book had 2 pages pointing at placeholder.png,
-						// and we were getting an error related to it being locked. So we deduce that ace was trying
-						// to copy the file twice, at the same time (normal nodejs code is highly async).
-						// Now the problem is not reproducable, but I'm leaving in this code that tried to deal with it.
-						errorMessage = $"Exit code{res.ExitCode}{Environment.NewLine}" +
-						               $"Standard Error{Environment.NewLine}{res.StandardError}{Environment.NewLine}" +
-						               $"Standard Out{res.StandardOutput}";
-
-						_webSocketProgress.ErrorWithoutLocalizing(errorMessage);
-
-						continue; // something went wrong, try again
-					}
+					continue; // something went wrong, try again
+				}
 
 				// The html client is set to treat a text reply as a url of the report. Make sure it's valid for being a URL.
 				// See https://silbloom.myjetbrains.com/youtrack/issue/BL-6197.
 				request.ReplyWithText("/bloom/" + answerPath.EscapeFileNameForHttp());
+				if (version.old)
+				{
+					// If we displayed an important message about updating ACE, make sure the user
+					// has SOME time to see it.
+					// Review: is this long enough? Should we look for some other way to show the
+					// problem? At present the problems caused by an old version (1.1 is the oldest
+					// contemporary with Bloom) is fairly minor.
+					while (DateTime.Now - started < new TimeSpan(0,0,0,5))
+						Thread.Sleep(100);
+				}
 				return;
 			}
 
 			// If we get this far, we give up.
 			ReportErrorAndFailTheRequest(request, errorMessage);
+		}
+
+		private (string version, bool old) GetAceByDaisyVersion(string daisyDirectory)
+		{
+			try
+			{
+				// The path typically ends in something like \ace\bin\, at least on Windows.
+				// We're looking for the ace directory. One GetDirectoryName just strips of the
+				// final slash, so we need two to get to ace.
+				var packagePath = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(daisyDirectory)), "package.json");
+				var packageJson = DynamicJson.Parse(RobustFile.ReadAllText(packagePath));
+				string versionString = packageJson.version;
+				var match = new Regex(@"(\d+)\.(\d+)").Match(versionString);
+				if (match.Success)
+				{
+					var old = int.Parse(match.Groups[1].Value) * 100 + int.Parse(match.Groups[2].Value) < 102;
+					return (versionString, old);
+				}
+			}
+			catch (Exception ex)
+			{
+				// just ignore not being able to get the version.
+			}
+
+			return ("unknown", true);
 		}
 
 		private string MakeEpub(string parentDirectory, WebSocketProgress progress)
