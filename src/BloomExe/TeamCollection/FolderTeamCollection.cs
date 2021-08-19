2d1458b04e74ec33c76932be3d105dba29fc74d8
Stephen McConnel <stephen_mcconnel@sil.org>
2021-08-19 14:21:45 -0600

    Check for LAN sharing when checking Dropbox access (BL-10272)

--- src/BloomExe/TeamCollection/FolderTeamCollection.cs
+++ src/BloomExe/TeamCollection/FolderTeamCollection.cs
@@ -991,22 +991,84 @@ namespace Bloom.TeamCollection
 					_repoFolderPath);
 			}
 
+			if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+			{
+				return new TeamCollectionMessage(MessageAndMilestoneType.Error, "TeamCollection.NoNetwork",
+					"No network is available on this computer.");
+			}
+
+			var isOnLocalNetwork = IsFolderOnLocalNetwork(_repoFolderPath);
 			if (DropboxUtils.IsPathInDropboxFolder(_repoFolderPath))
 			{
 				if (!DropboxUtils.IsDropboxProcessRunning)
 				{
-					return new TeamCollectionMessage(MessageAndMilestoneType.Error, "TeamCollection.NeedDropboxRunning",
-						"Dropbox does not appear to be running.");
+					if (isOnLocalNetwork)
+						_tcManager.MessageLog.WriteMessage(MessageAndMilestoneType.History, "TeamCollection.NeedDropboxRunningButLANOK",
+							"Dropbox does not appear to be running, but the folder has also been shared locally which appears to be okay.");
+					else
+						return new TeamCollectionMessage(MessageAndMilestoneType.Error, "TeamCollection.NeedDropboxRunning",
+							"Dropbox does not appear to be running.");
 				}
 
 				if (!DropboxUtils.CanAccessDropbox())
 				{
-					return new TeamCollectionMessage(MessageAndMilestoneType.Error, "TeamCollection.NeedDropboxAccess",
-						"Bloom cannot reach Dropbox.com.");
+					if (isOnLocalNetwork)
+						_tcManager.MessageLog.WriteMessage(MessageAndMilestoneType.History, "TeamCollection.NeedDropboxAccessButLANOK",
+							"Bloom cannot reach Dropbox.com, but the folder has also been shared locally which appears to be okay.");
+					else
+						return new TeamCollectionMessage(MessageAndMilestoneType.Error, "TeamCollection.NeedDropboxAccess",
+							"Bloom cannot reach Dropbox.com.");
 				}
 			}
 
 			return null;
+		}
+
+		private bool IsFolderOnLocalNetwork(string repoFolderPath)
+		{
+			try
+			{
+#if __MonoCS__
+				// This will require more research and work...
+				// Not sure how to tell if a folder has been shared to the network.  May have to be samba / NFS / ... specific.
+				// Possibly call "/bin/df -T" and parse output for file system type for folders shared from network?
+#else
+				// Check whether the repo folder exists inside a folder we've shared to the local network.
+				var repoFolderLower = repoFolderPath.ToLowerInvariant();
+				var searcher = new System.Management.ManagementObjectSearcher("select * from win32_share");
+				foreach (var share in searcher.Get())
+				{
+					string type = share["Type"].ToString();
+					if (type == "0") // 0 = DiskDrive (1 = Print Queue, 2 = Device, 3 = IPH)
+					{
+						var name = share["Name"].ToString(); //getting share name
+						var path = share["Path"].ToString(); //getting share path
+						if (name.EndsWith("$"))
+							continue;	// skip system shares like print$ or C$
+						if (Directory.Exists(path))
+						{
+							if (repoFolderLower.StartsWith(path.ToLowerInvariant()+"\\"))
+								return true;
+						}
+					}
+				}
+				// Check whether the repo folder is actually shared from the network
+				if (Regex.Match(repoFolderPath, @"^\\{2}[\w-]+(\\{1}(([\w-][\w-\s]*[\w-]+[$$]?)|([\w-][$$]?$)))+").Success)
+					return true;
+				if (Regex.Match(repoFolderPath, "^[A-Za-z]:").Success)
+				{
+					var di = new DriveInfo(repoFolderPath);
+					if (di.DriveType == DriveType.Network)
+						return true;
+				}
+#endif
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine($"Caught exception in IsFolderOnLocalNetwork(\"{repoFolderPath}\"): {e}");
+				NonFatalProblem.ReportSentryOnly(e, $"Caught exception in IsFolderOnLocalNetwork(\"{repoFolderPath}\")");
+			}
+			return false;
 		}
 
 		public override string GetBackendType()
