70be4dd6f197b3e0632884aac03051512780a51b
Gordon Martin <gordon_martin@sil.org>
2021-08-27 10:39:13 -0700

    BL-10329 Rename Comic to Overlay Tool

--- src/BloomExe/Edit/ToolboxView.cs
+++ src/BloomExe/Edit/ToolboxView.cs
@@ -79,7 +79,7 @@ namespace Bloom.Edit
 			yield return BloomFileLocator.GetBrowserDirectory("bookEdit/toolbox/readers/readerSetup");
 			yield return BloomFileLocator.GetBrowserDirectory("bookEdit/toolbox/signLanguage");
 			yield return BloomFileLocator.GetBrowserDirectory("bookEdit/toolbox/imageDescription");
-			yield return BloomFileLocator.GetBrowserDirectory("bookEdit/toolbox/comic");
+			yield return BloomFileLocator.GetBrowserDirectory("bookEdit/toolbox/overlay");
 		}
 
 		public static string MakeToolboxContent(Book.Book book)
