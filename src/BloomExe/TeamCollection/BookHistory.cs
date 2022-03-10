6c678f99fde4137da316cdf87b89a36d9cfa6a5d
John Thomson <john_thomson@sil.org>
2022-03-10 08:03:34 -0600

    Make the When of BookHistory UTC time (BL-11014)

--- src/BloomExe/TeamCollection/BookHistory.cs
+++ src/BloomExe/TeamCollection/BookHistory.cs
@@ -50,6 +50,14 @@ namespace Bloom.TeamCollection
 		[Column(name:"username")] public string UserName { get; set; }
 		[Column("type")] public BookHistoryEventType Type { get; set; }
 		[Column("user_message")] public string Message { get; set; }
+
+		// I don't fully understand why, but this gets stored in the underlying table in such a way that
+		// the DateTimes that come back are Kind=unspecified. From inspecting the binary data, I _think_
+		// they are being stored as a bigint representing ticks. This means the underlying table
+		// doesn't inherently know whether the DateTime is local or UTC.
+		// Prior to March 10, 2022 we were storing local DateTimes (DateTime.Now), and therefore,
+		// sorting by retrieved DateTimes did not reliably put the events in true chronological order
+		// if they were created in different time zones. We now store the dates as UTC.
 		[Column("date")] public DateTime When { get; set; }
 
 		[Indexed]
@@ -172,7 +180,8 @@ public class BookHistory
 					UserId = TeamCollectionManager.CurrentUser,
 					UserName = TeamCollectionManager.CurrentUserFirstName,
 					Type = eventType,
-					When = DateTime.Now
+					// Be sure to use UTC, otherwise, order will not be preserved properly.
+					When = DateTime.UtcNow
 				};
 
 				db.Insert(evt);
