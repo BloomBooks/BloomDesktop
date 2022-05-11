b866ccd5ebb73ba5b55c33f67179c6d4d3951c8b
John Thomson <john_thomson@sil.org>
2022-05-11 16:46:39 -0500

    Minimally fix Ghostscript printing (BL-11113)

--- src/BloomExe/Publish/PDF/PdfViewer.cs
+++ src/BloomExe/Publish/PDF/PdfViewer.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2014 SIL International
+// Copyright (c) 2014 SIL International
 // This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
 
 using System;
@@ -8,11 +8,14 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Bloom.Properties;
+using Bloom.web.controllers;
 using Gecko;
 using Gecko.Interop;
 using L10NSharp;
 using Microsoft.Win32;
 using SIL.IO;
+using SIL.Reporting;
 #if !__MonoCS__
 #endif
 
@@ -46,8 +49,8 @@ namespace Bloom.Publish.PDF
 			// In Windows we would prefer to use Acrobat to display and print PDFs. It avoids various bugs in
 			// PDFjs, such as BL-1177 (Andika sometimes lost when printing directly from Bloom),
 			// BL-1170 Printing stops after certain point
-			// BL-1037 PDFjs sometimes fails to display if use certain jpg images
-			// If Acrobat is not installed, it will fall back to PDFjs, and we hope for the best.
+				// BL-1037 PDFjs sometimes fails to display if use certain jpg images
+				// If Acrobat is not installed, it will fall back to PDFjs, and we hope for the best.
 			// Todo: we need a better solution in Linux, also. Ghostscript might provide something but
 			// has a GPL license.
 			_pdfViewerControl = new AdobeReaderControl();
@@ -96,25 +99,25 @@ namespace Bloom.Publish.PDF
 			var arc = _pdfViewerControl as AdobeReaderControl;
 			if (arc != null) // We haven't yet had a problem displaying with Acrobat...
 			{
-				if (arc.ShowPdf(pdfFile))
-					return true; // success using acrobat
-				// Acrobat not working (probably not installed). Switch to using Gecko to display PDF.
+					if (arc.ShowPdf(pdfFile))
+						return true; // success using acrobat
+					// Acrobat not working (probably not installed). Switch to using Gecko to display PDF.
 				UpdatePdfViewer(new GeckoWebBrowser());
 				// and continue to show it using that.
 			}
 #endif
 			// Escaping the filename twice for characters like # is needed in order to get the
 			// pdf filename through Geckofx/xulrunner to our local server on Linux.  This is to
-			// prevent the filename from being cut short at the # character.  As far as I can
-			// tell, Linux xulrunner strips one level of escaping on input, then before passing
-			// the query on to the localhost server it truncates the query portion at the first
-			// # it sees.  The localhost processor expects one level of encoding, and we deal
-			// with having a # in the query (file path) there without any problem.  You may
-			// regard this double escaping as a hack to get around the Linux xulrunner which
-			// behaves differently than the Windows xulrunner.  It is an exception to the rule
-			// of matching EscapeFileNameForHttp() with UnescapeFileNameForHttp().  See a comment in
-			// https://jira.sil.org/browse/BL-951 for a description of the buggy program
-			// behavior without this hack.
+				// prevent the filename from being cut short at the # character.  As far as I can
+				// tell, Linux xulrunner strips one level of escaping on input, then before passing
+				// the query on to the localhost server it truncates the query portion at the first
+				// # it sees.  The localhost processor expects one level of encoding, and we deal
+				// with having a # in the query (file path) there without any problem.  You may
+				// regard this double escaping as a hack to get around the Linux xulrunner which
+				// behaves differently than the Windows xulrunner.  It is an exception to the rule
+				// of matching EscapeFileNameForHttp() with UnescapeFileNameForHttp().  See a comment in
+				// https://jira.sil.org/browse/BL-951 for a description of the buggy program
+				// behavior without this hack.
 			var file = pdfFile;
 			if (SIL.PlatformUtilities.Platform.IsUnix)
 				file = file.EscapeFileNameForHttp().EscapeFileNameForHttp();
@@ -127,13 +130,13 @@ namespace Bloom.Publish.PDF
 			browser.Navigate(url);
 			browser.DocumentCompleted += (sender, args) =>
 			{
-				// We want to suppress several of the buttons that the control normally shows.
-				// It's nice if we don't have to modify the html and related files, because they are unzipped from a package we install
-				// from a source I'm not sure we control, and installed into a directory we can't modify at runtime.
-				// A workaround is to tweak the stylesheet to hide them. The actual buttons (and two menu items) are easily
-				// hidden by ID.
-				// Unfortunately we're getting rid of a complete group in the pull-down menu, which leaves an ugly pair of
-				// adjacent separators. And the separators don't have IDs so we can't easily select just one to hide.
+					// We want to suppress several of the buttons that the control normally shows.
+					// It's nice if we don't have to modify the html and related files, because they are unzipped from a package we install
+					// from a source I'm not sure we control, and installed into a directory we can't modify at runtime.
+					// A workaround is to tweak the stylesheet to hide them. The actual buttons (and two menu items) are easily
+					// hidden by ID.
+					// Unfortunately we're getting rid of a complete group in the pull-down menu, which leaves an ugly pair of
+					// adjacent separators. And the separators don't have IDs so we can't easily select just one to hide.
 				// Fortunately there are no other divs in the parent (besides the separator) so we just hide the second one.
 				// This is unfortunately rather fragile and may not do exactly what we want if the viewer.html file
 				// defining the pdfjs viewer changes.
@@ -144,13 +147,13 @@ namespace Bloom.Publish.PDF
 
 #if !__MonoCS__
 				if (!_haveShownAdobeReaderRecommendation)
-				{
-					_haveShownAdobeReaderRecommendation = true;
-					var message = LocalizationManager.GetString("PublishTab.Notifications.AdobeReaderRecommendation",
-						"This PDF viewer can be improved by installing the free Adobe Reader on this computer.");
-					RunJavaScript("toastr.remove();" +
-								  "toastr.options = { 'positionClass': 'toast-bottom-right','timeOut': '15000'};" +
-								  "toastr['info']('" + message + "')");
+					{
+						_haveShownAdobeReaderRecommendation = true;
+						var message = LocalizationManager.GetString("PublishTab.Notifications.AdobeReaderRecommendation",
+							"This PDF viewer can be improved by installing the free Adobe Reader on this computer.");
+						RunJavaScript("toastr.remove();" +
+						              "toastr.options = { 'positionClass': 'toast-bottom-right','timeOut': '15000'};" +
+						              "toastr['info']('" + message + "')");
 				}
 #endif
 			};
@@ -227,37 +230,7 @@ namespace Bloom.Publish.PDF
 			var exePath = "/usr/bin/gtklp";
 			systemSpecificArgs = "";
 #else
-			var gsKey = Registry.LocalMachine.OpenSubKey(@"Software\GPL Ghostscript");
-			if (gsKey == null)
-				gsKey = Registry.LocalMachine.OpenSubKey(@"Software\AGPL Ghostscript");
-			// Just possibly the paid version is present?
-			if (gsKey == null)
-			{
-				var hklm64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
-				// maybe the 64-bit version is installed?
-				gsKey = hklm64.OpenSubKey(@"Software\GPL Ghostscript");
-				if (gsKey == null)
-					gsKey = hklm64.OpenSubKey(@"Software\AGPL Ghostscript");
-			}
-			if (gsKey == null)
-				return false; // not able to print this way, GhostScript not present
-			string exePath = null;
-			foreach (var version in gsKey.GetSubKeyNames())
-			{
-				var gsVersKey = gsKey.OpenSubKey(version);
-				var dllPath = gsVersKey.GetValue("GS_DLL") as String;
-				if (dllPath == null)
-					continue;
-				if (!RobustFile.Exists(dllPath))
-					continue; // some junk there??
-				exePath = Path.Combine(Path.GetDirectoryName(dllPath), "gswin32c.exe");
-				if (RobustFile.Exists(exePath))
-					break;
-				exePath = Path.Combine(Path.GetDirectoryName(dllPath), "gswin64c.exe");
-				if (RobustFile.Exists(exePath))
-					break;
-				// some old install in a bad state? Try another subkey
-			}
+			var exePath = ProcessPdfWithGhostscript.FindGhostcriptOnWindows();
 			// -sDEVICE#mswinpr2 makes it display a print dialog so the user can choose printer.
 			// -dBATCH -dNOPAUSE -dQUIET make it go ahead without waiting for user input on each page or after last
 			// -dQUIET was an attempt to prevent it display per-page messages. Didn't work. Not sure it does any good.
@@ -273,7 +246,9 @@ namespace Bloom.Publish.PDF
 					FileName = exePath,
 					Arguments = systemSpecificArgs + "\"" + _pdfPath + "\"",
 					UseShellExecute = false, // enables CreateNoWindow
-					CreateNoWindow = true // don't need a DOS box (does not suppress print dialog)
+					CreateNoWindow = true, // don't need a DOS box (does not suppress print dialog)
+					RedirectStandardOutput = true,
+					RedirectStandardError = true
 				}
 			};
 #if __MonoCS__
@@ -284,6 +259,17 @@ namespace Bloom.Publish.PDF
 			proc.StartInfo.Environment.Remove("LD_LIBRARY_PATH");
 #endif
 			proc.Start();
+			proc.WaitForExit();
+			var errors =proc.StandardError.ReadToEnd();
+			var output = proc.StandardOutput.ReadToEnd();
+			// I think containing this string is the best I can do to identify the error that
+			// it returns if the user cancels when selecting a file on something like write to XPS.
+			if (!string.IsNullOrEmpty(errors) && !errors.Contains("000004c7"))
+			{
+				Logger.WriteEvent($"Ghostscript printing failed with error '{errors}' and output '{output}'");
+				ErrorReport.NotifyUserOfProblem(
+					"Printing seems to have failed. It may help to install Adobe Acrobat. To print without Acrobat, you must set a correct page size that your printer is capable of printing.");
+			}
 			return true; // we at least think we printed it (unless the user cancels...anyway, don't try again some other way).
 		}
 
