69e8c526dc0713b3751d772aae6585c5b78b741f
Stephen McConnel <stephen_mcconnel@sil.org>
2022-06-08 11:50:37 -0600

    Publishing "Audio or Video" is unavailable on Linux (BL-11011)

--- src/BloomBrowserUI/utils/isLinux.ts
+++ src/BloomBrowserUI/utils/isLinux.ts
@@ -2,7 +2,10 @@ export function isLinux(): boolean {
     const searchString = window.location.search;
     const i = searchString.indexOf("isLinux=");
     if (i >= 0) {
-        return searchString.substr(i + "isLinux=".length, 4) === "true";
+        const start = i + "isLinux=".length;
+        return (
+            searchString.substring(start, start + 4).toLowerCase() === "true"
+        );
     }
     return window.navigator.userAgent.indexOf("Linux") >= 0;
 }
