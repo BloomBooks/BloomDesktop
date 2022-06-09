4f0d46ecb23a913bef7a4ce0a4a5cce7557b14c1
Jeffrey Su <41497174+sujeffreyl@users.noreply.github.com>
2022-06-09 13:16:39 -0500

    Fix SL placeholder click handler often not working (BL-11279)

---
+++ src/BloomBrowserUI/bookEdit/toolbox/toolIds.ts
@@ -0,0 +1,7 @@
+// DO NOT import anything into this file!
+// (unless you're sure you know what you're doing)
+//
+// Note: This file was created to have a short, easy to reference thing
+// that wouldn't cause a lot of other things to get dragged into your bundle.
+// (Referencing a static variable in the relevant tool would cause a lot of things to get dragged into certain bundles)
+export const kOverlayToolId = "overlay";
