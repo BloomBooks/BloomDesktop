f40d5541b2bc48bba0e724eb83e888d363fdf0dd
Gordon Martin <gordon_martin@sil.org>
2022-06-30 13:20:20 -0700

    BL-11257 Fix Option Panel layout in BloomPUB

--- src/BloomBrowserUI/publish/ReaderPublish/LanguageSelectionSettingsGroup.tsx
+++ src/BloomBrowserUI/publish/ReaderPublish/LanguageSelectionSettingsGroup.tsx
@@ -64,7 +64,8 @@ export const LanguageSelectionSettingsGroup: React.FunctionComponent<{
                     css={css`
                         overflow-y: auto;
                         max-height: 150px;
-                        flex-wrap: nowrap; // mockup has a single column, but it works quite well with default wrap
+                        // The 'important' overrides the MUI default so we only get one column of checkboxes.
+                        flex-wrap: nowrap !important;
                         margin-right: 20px;
                         // These two "cancel out" except that they defeat a bug in the HTML spec that prevents
                         // overflow-x from being visible when overflow-y is hidden. The check box shadows can
