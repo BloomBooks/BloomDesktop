e041ac7770338d72a99fae15532184e471996b12
Jeffrey Su <41497174+sujeffreyl@users.noreply.github.com>
2022-07-08 12:03:46 -0500

    Make a clearer model for IBloomErrorReporter (BL-11375, BL-11193)
    Instead of having Bloom have a separate ErrorReportUtils class to add functionality to libpalaso's ErrorReport class, we can now make a subclass of ErrorReport. (The subclass is called BloomErrorReport). This allows us to avoid setting function parameters in class instance vars in a mult-stage, stateful scenario. Instead, we can now instead pass the function parameters directly through, so now it's more direct, less stateful, and alleviates worry about the instance vars getting overwritten if multiple error notifies come up.

---
+++ src/BloomExe/ErrorReporter/BloomErrorReport.cs
@@ -0,0 +1,53 @@
+﻿using SIL.Reporting;
+using System;
+using System.Diagnostics;
+
+namespace Bloom.ErrorReporter
+{
+	internal class BloomErrorReport : SIL.Reporting.ErrorReport
+	{
+		/// <summary>
+		/// Notifies the user of problem, with customized button labels and button click handlers
+		/// </summary>
+		/// <param name="message">The message to show the user</param>
+		/// <param name="exception">Any accompanying exception</param>
+		/// <param name="policy">Optional: The policy to use to decide whether to show the notification. Defaults to ShowAlwaysPolicy</param>
+		/// <param name="allowSendReport">Whether or not to show the Report button.</param>
+		/// <param name="extraButtonLabel">Optional: The label of the Report button. Pass "" to disable. Defaults to disabled.</param>
+		/// <param name="onExtraButtonPressed">Optional: The action to take when the extra button is pressed.</param>
+		public static void NotifyUserOfProblemCustom(string message, Exception exception = null, IRepeatNoticePolicy policy = null,
+			AllowSendReport allowSendReport = AllowSendReport.AllowIfException,
+			string extraButtonLabel = null, Action<string, Exception> onExtraButtonPressed = null)
+		{
+			if (policy == null)
+				policy = new ShowAlwaysPolicy();
+
+			NotifyUserOfProblemWrapper(message, exception, () =>
+			{
+				IErrorReporter errorReporter = GetErrorReporter();
+				if (errorReporter is IBloomErrorReporter)
+				{
+					// Normal situation
+					((IBloomErrorReporter)errorReporter).NotifyUserOfProblemCustom(message, exception, policy,
+						allowSendReport, extraButtonLabel, onExtraButtonPressed);
+				}
+				else
+				{
+					// Exceptional situation
+					// One case where this can be expected to appear is if you're using Bloom to manually test libpalaso's ErrorReporters,
+					// but otherwise, we shouldn't expect to reach here during normal operation!
+					Debug.Fail("Warning: Expected SetErrorReporter() to be called with an IBloomErrorReporter, but actual object was not an instance of that type.");
+
+					// Unexpected to not have the derived type, but just do our best using the base type
+					string alternateButton1Label = exception != null ? "Details" : "";
+
+					var result = errorReporter.NotifyUserOfProblem(policy, alternateButton1Label, ErrorResult.Yes, message);
+					if (result == ErrorResult.Yes)
+						HtmlErrorReporter.DefaultOnReportClicked(message, exception);
+				}
+
+				return ErrorResult.OK;
+			});
+		}
+	}
+}
---
+++ src/BloomExe/ErrorReporter/IBloomErrorReporter.cs
@@ -0,0 +1,24 @@
+﻿using SIL.Reporting;
+using System;
+
+namespace Bloom.ErrorReporter
+{
+	/// <summary>
+	/// An IErrorReporter which is also able to handle additional error reporting dialog scenarios used in Bloom 
+	/// </summary>
+	internal interface IBloomErrorReporter : IErrorReporter
+	{
+		/// <summary>
+		/// Notifies the user of problem, with a Report button (customizable) and an optional extra action in addition to the normal Close button.
+		/// </summary>
+		/// <param name="message">The message to show the user</param>
+		/// <param name="exception">Any accompanying exception</param>
+		/// <param name="policy">The policy to use to decide whether to show the notification.</param>
+		/// <param name="allowSendReport">Whether or not to show the Report button.</param>
+		/// <param name="extraButtonLabel">The label of the extra button. "" means disabled. null means use the default label.</param>
+		/// <param name="onExtraButtonClicked">The action to take when the extra button is clicked.</param>
+		void NotifyUserOfProblemCustom(string message, Exception exception, IRepeatNoticePolicy policy,
+			AllowSendReport allowSendReport,
+			string extraButtonLabel, Action<string, Exception> onExtraButtonClicked);
+	}
+}
