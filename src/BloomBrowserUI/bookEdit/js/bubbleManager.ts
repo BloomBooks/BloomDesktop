d04ccc7aa1a1aacbd3cbcd18cbd592fd5f69ef6a
John Thomson <john_thomson@sil.org>
2022-10-20 14:31:19 -0500

    Fix more problems dragging bubbles (BL-11617)
    - Restore drag-by-handle function
    - Give that the same contraint as ctrl-drag
    - Increase min visibility to 10px

--- src/BloomBrowserUI/bookEdit/js/bubbleManager.ts
+++ src/BloomBrowserUI/bookEdit/js/bubbleManager.ts
@@ -878,6 +878,19 @@ export class BubbleManager {
             );
         });
     }
+    // This is pretty small, but it's the amount of the text box that has to be visible;
+    // typically a bit more of the actual bubble can be seen.
+    // Arguably it would be better to use a slightly larger number and make it apply to the
+    // actual bubble outline, but
+    // - this is much harder; we'd need ComicalJs enhancments to know exactly where the edge
+    //   of the bubble is.
+    // - the two dimensions would not be independent; a bubble whose top is above the bottom
+    //   of the container and whose right is to the right of the contaniner's left
+    //   might still be entirely invisible as its curve places it entirely beyond the bottom
+    //   left corner.
+    // - The constraint would actually be different depending on the type of bubble,
+    //   which means a bubble might need to move as a result of changing its bubble type.
+    private minBubbleVisible = 10;
 
     // Conceptually, move the bubble to the specified location (which may be where it is already).
     // However, first adjust the location to make sure at least a little of the bubble is visible
@@ -895,32 +908,19 @@ export class BubbleManager {
         const right = left + bubbleRect.width;
         const top = location.getScaledY();
         const bottom = top + bubbleRect.height;
-        // This is pretty small, but it's the amount of the text box that has to be visible;
-        // typically a bit more of the actual bubble can be seen.
-        // Arguably it would be better to use a slightly larger number and make it apply to the
-        // actual bubble outline, but
-        // - this is much harder; we'd need ComicalJs enhancments to know exactly where the edge
-        //   of the bubble is.
-        // - the two dimensions would not be independent; a bubble whose top is above the bottom
-        //   of the container and whose right is to the right of the contaniner's left
-        //   might still be entirely invisible as its curve places it entirely beyond the bottom
-        //   left corner.
-        // - The constraint would actually be different depending on the type of bubble,
-        //   which means a bubble might need to move as a result of changing its bubble type.
-        const minVisible = 5;
         let x = left;
         let y = top;
-        if (right < parentRect.left + minVisible) {
-            x = parentRect.left + minVisible - bubbleRect.width;
+        if (right < parentRect.left + this.minBubbleVisible) {
+            x = parentRect.left + this.minBubbleVisible - bubbleRect.width;
         }
-        if (left > parentRect.right - minVisible) {
-            x = parentRect.right - minVisible;
+        if (left > parentRect.right - this.minBubbleVisible) {
+            x = parentRect.right - this.minBubbleVisible;
         }
-        if (bottom < parentRect.top + minVisible) {
-            y = parentRect.top + minVisible - bubbleRect.height;
+        if (bottom < parentRect.top + this.minBubbleVisible) {
+            y = parentRect.top + this.minBubbleVisible - bubbleRect.height;
         }
-        if (top > parentRect.bottom - minVisible) {
-            y = parentRect.bottom - minVisible;
+        if (top > parentRect.bottom - this.minBubbleVisible) {
+            y = parentRect.bottom - this.minBubbleVisible;
         }
         // The 0.1 here is rather arbitrary. On the one hand, I don't want to do all the work
         // of placeElementAtPosition in the rather common case that we're just checking bubble
@@ -947,7 +947,7 @@ export class BubbleManager {
     private onMouseDown = (event: MouseEvent) => {
         const container = event.currentTarget as HTMLElement;
         // Let standard clicks on the bloom editable only be processed on the editable
-        if (this.isEventForEditableOnly(event)) {
+        if (this.isEventForEditableOrMoveHandle(event)) {
             return;
         }
         // since that returned false, either ctrl or alt is down, or we clicked outside the
@@ -1080,7 +1080,7 @@ export class BubbleManager {
             return;
         }
 
-        if (this.isEventForEditableOnly(event)) {
+        if (this.isEventForEditableOrMoveHandle(event)) {
             this.cleanupMouseMoveHover(container);
             return;
         }
@@ -1372,12 +1372,17 @@ export class BubbleManager {
         );
     }
 
-    private isEventForEditableOnly(ev): boolean {
+    private isEventForEditableOrMoveHandle(ev): boolean {
+        const targetElement = ev.target as HTMLElement;
+        if (targetElement.classList.contains("bloom-dragHandle")) {
+            // The drag handle is outside the bubble, so dragging it with the mouse
+            // events we handle doesn't work. Returning true lets its own event handler
+            // deal with things, and is a good thing even when ctrl or alt is down.
+            return true;
+        }
         if (ev.ctrlKey || ev.altKey) {
             return false;
         }
-
-        const targetElement = ev.target as HTMLElement;
         // I'm not sure what else targetElement can be, but have seen JS errors
         // when closest is not defined.
         const isInsideEditable = !!(
@@ -2571,14 +2576,16 @@ export class BubbleManager {
             $(thisOverPictureElement).draggable({
                 // Adjust containment by scaling
                 containment: [
+                    // arguably we could add this.minBubbleVisible ti the first two, but in practice,
+                    // since the handle is in the top left, it can't be used to drag it even that close.
                     containerPos.left,
                     containerPos.top,
                     containerPos.left +
                         containerPos.width -
-                        wrapperBoxRectangle.width,
+                        this.minBubbleVisible,
                     containerPos.top +
                         containerPos.height -
-                        wrapperBoxRectangle.height
+                        this.minBubbleVisible
                 ],
                 revertDuration: 0,
                 handle: ".bloom-dragHandle",
