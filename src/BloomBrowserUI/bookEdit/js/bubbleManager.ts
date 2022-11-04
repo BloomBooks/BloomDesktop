cc4f0812994f70f985db77f78188c0af0b025ceb
John Thomson <john_thomson@sil.org>
2022-11-04 17:46:53 -0500

    Restore video playback in video overlays (BL-11725)

--- src/BloomBrowserUI/bookEdit/js/bubbleManager.ts
+++ src/BloomBrowserUI/bookEdit/js/bubbleManager.ts
@@ -1502,6 +1502,10 @@ export class BubbleManager {
         if (ev.ctrlKey || ev.altKey) {
             return false;
         }
+        if (targetElement.closest(".bloom-videoContainer")) {
+            // want playback-related behavior in video containers
+            return true;
+        }
         const isInsideEditable = !!targetElement.closest(".bloom-editable");
         return isInsideEditable;
     }
