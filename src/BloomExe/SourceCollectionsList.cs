573443bec46aca8d29234bf6029646fcbc4362c3
Gordon Martin <gordon_martin@sil.org>
2023-03-14 12:48:09 -0700

    Fix missing template crash
    * if the template a book is based on doesn't exist on this
       machine, this keeps the add page dialog from crashing

--- src/BloomExe/SourceCollectionsList.cs
+++ src/BloomExe/SourceCollectionsList.cs
@@ -65,7 +65,12 @@ namespace Bloom
 			// source collections we search. So just create it directly.
 			// This makes the method name somewhat deceptive. But I was trying not to
 			// disrupt things too badly. And there's still a consistency with the fileName version.
-			return CreateTemplateBookByFolderPath(Path.GetDirectoryName(path));
+			var folderPath = Path.GetDirectoryName(path);
+			if (!Directory.Exists(folderPath))
+				// This can happen if we're opening the AddPage dialog and the template that this book is
+				// derived from doesn't exist on this machine.
+				return null;
+			return CreateTemplateBookByFolderPath(folderPath);
 		}
 
 		private Book.Book CreateTemplateBookByFolderPath(string folderPath)
