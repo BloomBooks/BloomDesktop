f73374e97c6db644f7df43b48e7b07868bd13c7c
Hatton <hattonjohn@gmail.com>
2023-04-24 09:57:45 -0600

    wip

--- src/BloomExe/Book/AppearanceSettings.cs
+++ src/BloomExe/Book/AppearanceSettings.cs
@@ -11,7 +11,7 @@ abstract class PropertyDef
 {
 	public string Name;
 	public abstract void SetDefault(ref dynamic prop);
-
+	public string OverrideGroup;
 }
 abstract class CssPropertyDef : PropertyDef
 {
@@ -20,28 +20,30 @@ abstract class CssPropertyDef : PropertyDef
 class StringPropertyDef : PropertyDef
 {
 	public string DefaultValue;
-	public StringPropertyDef(string name, string defaultValue)
+	public StringPropertyDef(string name, string defaultValue, string overrideGroup)
 	{
-		Name = name; DefaultValue = defaultValue;
+		Name = name;
+		DefaultValue = defaultValue;
+		OverrideGroup = overrideGroup;
 	}
 	public override void SetDefault(ref dynamic prop)
 	{
 		prop = DefaultValue;
 	}
-
 }
 
 class CssStringVariableDef : CssPropertyDef
 {
 	public string DefaultValue;
-	public CssStringVariableDef(string name, string defaultValue)
+	public CssStringVariableDef(string name, string defaultValue, string overrideGroup)
 	{
 		Name = name; DefaultValue = defaultValue;
+		OverrideGroup = overrideGroup;
 	}
 
 	public override string GetCssVariableDeclaration(dynamic property)
 	{
-		return $"--{Name}:{property.value}";
+		return $"--{Name}: {property.value};";
 	}
 	public override void SetDefault(ref dynamic prop)
 	{
@@ -50,23 +52,26 @@ class CssStringVariableDef : CssPropertyDef
 }
 
 /// <summary>
-/// variables that can be used in rules like `display: var(--something)`
+/// variables that can be used in rules like ` .something { display: var(--coverShowTopic) }`
 /// </summary>
 class CssDisplayVariableDef : CssPropertyDef
 {
 	public string TrueValue;
 	public string FalseValue;
 	public bool DefaultValue;
-	public CssDisplayVariableDef(string name, bool defaultValue)
+	public CssDisplayVariableDef(string name, bool defaultValue, string overrideGroup)
 	{
-		Name = name; TrueValue = "ignore-this"; FalseValue = "none";
+		Name = name;
+		TrueValue = "ignore-this"; // by using an illegal value, we just get a no-op rule, which is what we want
+		FalseValue = "none";
 		DefaultValue = defaultValue;
+		OverrideGroup = overrideGroup;
 	}
 
 	public override string GetCssVariableDeclaration(dynamic property)
 	{
 		var value = property.value ? TrueValue : FalseValue;
-		return $"--{Name}:{value}";
+		return $"--{Name}: {value};";
 	}
 	public override void SetDefault(ref dynamic prop)
 	{
@@ -77,26 +82,32 @@ public class AppearanceSettings
 {
 
 	private dynamic _properties;
+	public dynamic TestOnlyPropertyAccess(string name)
+	{
+		return _properties.Get(name);
+	}
 
 	// create an array of properties and fill it in
 	private PropertyDef[] propertyDefinitions = new PropertyDef[]
 	{
-		new StringPropertyDef("presetName","default"),
-		new CssStringVariableDef("coverColor","yellow"),
-		new CssDisplayVariableDef("coverShowTitleL2",true),
-		new CssDisplayVariableDef("coverShowTitleL3",false),
-		new CssDisplayVariableDef("coverShowTopic",true),
-		new CssDisplayVariableDef("coverShowLanguageName",false),
+		new StringPropertyDef("cssThemeName","default", "cssThemeName"), // this one is special because it doesn't correspond to a CSS variable. Instead, we will copy the contents of named file as rules at the end of the CSS file.
+		new CssStringVariableDef("coverColor","yellow","colors"),
+		new CssDisplayVariableDef("coverShowTitleL2",true, "coverFields"),
+		new CssDisplayVariableDef("coverShowTitleL3",false,"coverFields"),
+		new CssDisplayVariableDef("coverShowTopic",true, "coverFields"),
+		new CssDisplayVariableDef("coverShowLanguageName",false, "coverFields"),
 	};
-	private string PresetName { get { return _properties.presetName; } }
+	private string CssThemeName { get { return _properties.cssThemeName; } }
 
 	public AppearanceSettings()
 	{
 		_properties = new ExpandoObject();
+
+		// copy in the default values from each definition
 		foreach (var definition in propertyDefinitions)
 		{
 			var prop = ((IDictionary<string, object>)_properties)[definition.Name];
-			definition.SetDefault(ref prop);
+			definition.SetDefault(ref prop); // this is a workaround using ref because c# doesn't have union types, or `any`, sigh
 		}
 	}
 
@@ -128,6 +139,20 @@ public class AppearanceSettings
 
 		return settings;
 	}
+	public string GetCssRootDeclaration(AppearanceSettings parent = null)
+	{
+		var cssBuilder = new StringBuilder();
+		cssBuilder.AppendLine(":root{");
+		foreach (var property in _properties)
+		{
+			//if (key == "overrides") continue;
+			var definition = propertyDefinitions.First(d => d.Name == property.Name);
+			if (definition is CssPropertyDef)
+				cssBuilder.AppendLine(((CssPropertyDef)definition).GetCssVariableDeclaration(property));
+		}
+		cssBuilder.AppendLine("}");
+		return cssBuilder.ToString();
+	}
 	public void WriteAppearanceCss(string folder)
 	{
 		var targetPath = AppearanceCssPath(folder);
@@ -140,32 +165,25 @@ public class AppearanceSettings
 		}
 
 		var cssBuilder = new StringBuilder();
-		cssBuilder.AppendLine(":root{");
-		foreach (var property in _properties)
+		cssBuilder.AppendLine(GetCssRootDeclaration(null));
+		if (!string.IsNullOrEmpty(CssThemeName))
 		{
-			var definition = propertyDefinitions.First(d => d.Name == property.Name);
-			if (definition is CssPropertyDef)
-				cssBuilder.AppendLine(((CssPropertyDef)definition).GetCssVariableDeclaration(property));
-		}
-		cssBuilder.AppendLine("}");
-		if (!string.IsNullOrEmpty(PresetName))
-		{
-			var sourcePath = Path.Combine(ProjectContext.GetFolderContainingAppearancePresetFiles(), PresetName + ".css");
+			var sourcePath = Path.Combine(ProjectContext.GetFolderContainingAppearancePresetFiles(), CssThemeName + ".css");
 			if (!RobustFile.Exists(sourcePath))
 			{
 				// TODO: We should toast I suppose?
 			}
 			else
 			{
-				cssBuilder.AppendLine($"/* From the current appearance preset, '{PresetName}' */");
+				cssBuilder.AppendLine($"/* From the current appearance preset, '{CssThemeName}' */");
 				cssBuilder.AppendLine(RobustFile.ReadAllText(sourcePath, Encoding.UTF8));
 			}
 		}
 		RobustFile.WriteAllText(targetPath, cssBuilder.ToString());
 	}
 
-	internal void Update(dynamic appearance)
+	internal void Update(dynamic replacement)
 	{
-		_properties = appearance;
+		_properties = replacement;
 	}
 }
