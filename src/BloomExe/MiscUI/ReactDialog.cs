d87eacd7b431801fec9f898940def7e202e98cd7
Andrew Polk <andrew_polk@sil.org>
2023-04-26 16:20:29 -0700

    Remove obsolete BrowserDialog (BL-12144)

--- src/BloomExe/MiscUI/ReactDialog.cs
+++ src/BloomExe/MiscUI/ReactDialog.cs
@@ -6,6 +6,27 @@ using System.Windows.Forms;
 
 namespace Bloom.MiscUI
 {
+	// This interface allows the unit tests to mock a BrowserDialog
+	// when it's undesirable to spin up a real one.
+	public interface IBrowserDialog : IDisposable
+	{
+		string CloseSource { get; set; }
+
+		// Various properties/methods from the Form class (Sadly, it doesn't have an interface)
+		// ENHANCE: Add more methods from Form as needed, or if you have patience to add all of them
+		#region Properties from Form class
+		bool ControlBox { get; set; }
+		FormBorderStyle FormBorderStyle { get; set; }
+		int Height { get; set; }
+		string Text { get; set; }
+		int Width { get; set; }
+		#endregion
+
+		#region Methods from Form class
+		DialogResult ShowDialog();  // Desirable to be mocked out by unit tests
+		#endregion
+	}
+
 	/// <summary>
 	/// A dialog whose entire content is a react control. The constructor specifies
 	/// the js bundle.
