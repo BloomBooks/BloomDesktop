a97a1107558c874d84ddf4cc3e92bf9fa7ee3848
Noel Chou <22460975+nabalone@users.noreply.github.com>
2023-06-22 17:17:23 -0400

    BL-11583 fix draft icon and text alignment

--- src/BloomBrowserUI/publish/commonPublish/PublishVisibilityGroup.tsx
+++ src/BloomBrowserUI/publish/commonPublish/PublishVisibilityGroup.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx **/
-import { jsx } from "@emotion/react";
+import { css, jsx } from "@emotion/react";
 import * as React from "react";
 import { SettingsGroup } from "./PublishScreenBaseComponents";
 import { useL10n } from "../../react_components/l10nHooks";
@@ -14,11 +14,24 @@ export const PublishVisibilityGroup: React.FunctionComponent<{}> = props => {
             <ApiCheckbox
                 label={
                     <React.Fragment>
-                        <img src="/bloom/publish/LibraryPublish/DRAFT-Stamp.svg" />
-                        <Span l10nKey="PublishTab.Upload.Draft">
-                            Show this book only to reviewers with whom I share
-                            the URL of this book.
-                        </Span>
+                        <div
+                            css={css`
+                                display: flex;
+                                align-items: start; // align image with top of checkbox
+                            `}
+                        >
+                            <img
+                                css={css`
+                                    margin-left: -9px; //The checkbox has some right-padding that we want to overlap
+                                    padding-right: 3px;
+                                `}
+                                src="/bloom/publish/LibraryPublish/DRAFT-Stamp.svg"
+                            />
+                            <Span l10nKey="PublishTab.Upload.Draft">
+                                Show this book only to reviewers with whom I
+                                share the URL of this book.
+                            </Span>
+                        </div>
                     </React.Fragment>
                 }
                 apiEndpoint="publish/markAsDraft"
