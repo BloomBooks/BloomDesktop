ce4a97c897e2778e578aeb0ae469a3b153cf19cc
Gordon Martin <gordon_martin@sil.org>
2023-07-12 13:29:04 -0700

    Fix bad merge
    * we ended up with 2 copies of this method

--- src/BloomExe/Collection/CollectionSettings.cs
+++ src/BloomExe/Collection/CollectionSettings.cs
@@ -957,19 +957,5 @@ namespace Bloom.Collection
 				return CollectionSettingsApi.EnterpriseStatus.Community;
 			return CollectionSettingsApi.EnterpriseStatus.Subscription;
 		}
-
-		public CollectionSettingsApi.EnterpriseStatus GetEnterpriseStatus()
-		{
-			if (CollectionSettingsApi.FixEnterpriseSubscriptionCodeMode)
-			{
-				// We're displaying the dialog to fix a branding code...select that option
-				return CollectionSettingsApi.EnterpriseStatus.Subscription;
-			}
-			if (BrandingProjectKey == "Default")
-				return CollectionSettingsApi.EnterpriseStatus.None;
-			else if (BrandingProjectKey == "Local-Community")
-				return CollectionSettingsApi.EnterpriseStatus.Community;
-			return CollectionSettingsApi.EnterpriseStatus.Subscription;
-		}
 	}
 }
