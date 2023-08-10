2a6b5b156717bf15555b71777267d4c48be81c2d
Noel Chou <22460975+nabalone@users.noreply.github.com>
2023-08-10 19:28:45 -0400

    BL-12495 Improve "show invisibles"

---
+++ DistFiles/fonts/BloomShowInv/README.txt
@@ -0,0 +1,15 @@
+Bloom Show Inv
+
+Bloom Show Inv is an open font used by Bloom (https://bloomlibrary.org/) for showing
+invisible characters in text. Font sources are in UFO3 format.
+
+The build process requires smith (https://github.com/silnrsi/smith) and project
+build parameters are set in the wscript.
+
+For copyright and licensing - including any Reserved Font Names - see OFL.txt.
+
+For practical information about modifying and redistributing this font see OFL-FAQ.txt.
+
+For more details about this project, including changelog and acknowledgements see FONTLOG.txt.
+
+If you want to contribute to the project let us know.
--- src/BloomBrowserUI/bookEdit/js/bloomEditing.ts
+++ src/BloomBrowserUI/bookEdit/js/bloomEditing.ts
@@ -1,4 +1,5 @@
 ///<reference path="./jquery.hasAttr.d.ts" />
+/// <reference path="../../typings/jquery.qtip.d.ts" />
 import * as $ from "jquery";
 import bloomQtipUtils from "./bloomQtipUtils";
 import {
@@ -158,13 +159,33 @@ function SetupDeletable(containerDiv) {
     return $(containerDiv);
 }
 
-function getUnicodePoint(char) {
-    const codePoint = char.charCodeAt(0);
+function invisibleToCode(invisible) {
+    if (invisible.length > 1) {
+        // This is not a single character; assume it is an html entity of a special whitespace character
+        return invisible;
+    }
+    const codePoint = invisible.charCodeAt(0);
     let unicodePoint = codePoint.toString(16);
     while (unicodePoint.length < 4) {
         unicodePoint = "0" + unicodePoint;
     }
     return "\\u" + unicodePoint;
+}
+
+function invisibleFromCode(unicodeCode, entity) {
+    if (unicodeCode && entity) {
+        console.error(
+            "Something went wrong - we got both a unicode code and an html entity: " +
+                unicodeCode +
+                " and " +
+                entity
+        );
+        return "";
+    }
+    if (unicodeCode) {
+        return String.fromCharCode(parseInt(unicodeCode, 16));
+    }
+    return entity;
 }
 
 // Add various editing key handlers
@@ -224,50 +245,137 @@ function AddEditKeyHandlers(container) {
         });
 
     const invisibles = [
-        //{ re: "a|b|[0-9]", symbol: "🍎" },
-        { re: "&nbsp;|\u00A0", symbol: "🚀" }, // space (rocket == space)
-        { re: "\u202f", symbol: "🥐" }, // narrow space (croissant == french)
-        { re: "&ZeroWidthSpace;|\u200B", symbol: "0️⃣" },
-        { re: "&zwj;|\u200D", symbol: "🔗" }, // zero width joiner
+        { name: "Zero Width Joiner", re: "&zwj;|\u200D", symbol: "\uE077" },
+        { name: "Word Joiner", re: "&NoBreak;|\u2060", symbol: "\uE078" },
         {
+            name: "Zero Width Non-Joiner",
+            re: "&zwnj;|\u200C",
+            symbol: "\uE079"
+        },
+        { name: "Combining Grapheme Joiner", re: "\u034F", symbol: "\uE07A" },
+        {
+            name: "Zero Width Space",
             re:
-                "[\u0009-\u000D]|\u0085|\u1680|\u180E|[\u2000-\u200A]|\u2028|\u2029|\u205F|\u3000|\uFEFF", //review: some list from some random dude
-            symbol: "🐻‍❄️" // random other whitespace
-        }
+                "&ZeroWidthSpace;|&NegativeMediumSpace;|&NegativeThickSpace;|&NegativeThinSpace;|&NegativeVeryThinSpace;|\u200B",
+            symbol: "\uE081"
+        },
+        {
+            name: "No-Break Space",
+            re: "&nbsp;|&NonBreakingSpace;|\u00A0",
+            symbol: "\uE082"
+        },
+        { name: "Narrow No-Break Space", re: "\u202F", symbol: "\uE083" },
+        { name: "En Quad", re: "\u2000", symbol: "\uE084" },
+        { name: "Em Quad", re: "\u2001", symbol: "\uE085" },
+        { name: "En Space", re: "&ensp;|\u2002", symbol: "\uE086" },
+        { name: "Em Space", re: "&emsp;|\u2003", symbol: "\uE087" },
+        { name: "Three-Per-Em Space", re: "&emsp13;|\u2004", symbol: "\uE088" },
+        { name: "Four-Per-Em Space", re: "&emsp14;|\u2005", symbol: "\uE089" },
+        { name: "Six-Per-Em Space", re: "\u2006", symbol: "\uE08A" },
+        { name: "Figure Space", re: "&numsp;|\u2007", symbol: "\uE08B" },
+        { name: "Punctuation Space", re: "&puncsp;|\u2008", symbol: "\uE08C" },
+        {
+            name: "Thin Space",
+            re: "&thinsp;|&ThinSpace;|\u2009",
+            symbol: "\uE08D"
+        },
+        {
+            name: "Hair Space",
+            re: "&VeryThinSpace;|&hairsp;|\u200A",
+            symbol: "\uE08E"
+        },
+        { name: "Left-To-Right Mark", re: "&lrm;|\u200E", symbol: "\uE090" },
+        { name: "Right-To-Left Mark", re: "&rlm;|\u200F", symbol: "\uE091" },
+        { name: "Soft Hyphen", re: "&shy;|\u00AD", symbol: "\uE0A1" },
+        { name: "Horizontal Tabulation", re: "&Tab;|\u0009", symbol: "\uE0A2" }
     ];
 
     // for testing only: show invisibles
+    let inShowInvisiblesMode = false;
     $(container)
         .find("div.bloom-editable")
-        .on("keydown", null, "CTRL+ALT+SPACE", e => {
+        .on("keydown", null, "CTRL+SHIFT+SPACE", e => {
             e.preventDefault();
             // get the parent that has the class bloom-editable
             const editable = $(e.target).closest(".bloom-editable");
+            if (inShowInvisiblesMode) {
+                return;
+            }
+            inShowInvisiblesMode = true;
             editable.html((i, html) => {
-                // for each replacement, replace all instances of the char with the symbol
-                invisibles.forEach(function(pair) {
-                    const re = new RegExp(pair.re, "g");
+                // for each replacement, replace all instances of the invisible char/entity with the symbol
+                invisibles.forEach(function(invisibleType) {
+                    // Just in case there were invisibles-replacement spans left in the document,
+                    // don't match anything immediately following data-original="
+                    const re = new RegExp(
+                        `(?<!data-original=\")(${invisibleType.re})`,
+                        "g"
+                    );
                     html = html.replace(re, match => {
-                        const code = getUnicodePoint(match); // get something like \u00A0
-                        return `<span data-original="${code}">${pair.symbol}</span>`;
+                        const code = invisibleToCode(match); // get something like \u00A0 for unicode chars. leaves html entities as is.
+                        return `<span class="invisibles" data-name-of-invisible="${invisibleType.name}" data-original="${code}">${invisibleType.symbol}</span>`;
                     });
                     return html;
                 });
                 return html;
             });
-        })
-        .on("keyup", null, "CTRL+ALT+SPACE", e => {
-            // restore all the original characters
-            e.preventDefault();
-            const editable = $(e.target).closest(".bloom-editable");
-            editable.html((i, html) => {
-                return html.replace(
-                    /<span data-original="\\u(....)">..?.?<\/span>/g,
-                    function(match, p1) {
-                        return String.fromCharCode(parseInt(p1, 16));
-                    }
-                );
+            // Make one qtip per type of invisible character to explain the symbol to the user without cluttering the page too much
+            const invisibleCharTypesWithQtips = new Set();
+
+            $(".invisibles").each(function() {
+                const $this: JQuery = $(this);
+                // check if it has a qtip already TODO do we still need to do this?
+                if (
+                    !$this.data("hasqtip") &&
+                    !invisibleCharTypesWithQtips.has(
+                        $this.attr("data-original")
+                    )
+                ) {
+                    invisibleCharTypesWithQtips.add(
+                        $this.attr("data-original")
+                    );
+                    $this.qtip({
+                        position: {
+                            my: "top right",
+                            at: "bottom left",
+                            container: bloomQtipUtils.qtipZoomContainer()
+                        },
+                        content: $this.attr("data-name-of-invisible"),
+                        show: {
+                            ready: true
+                        },
+                        hide: {
+                            leave: false
+                        }
+                    });
+                }
             });
+        })
+        // when user releases any key or clicks away from the editable
+        // (if we only listen for keyup of the CTL+SHIFT+SPACE, it doesn't trigger if user lifts keys in wrong order)
+        .on("keyup blur", null, e => {
+            if (inShowInvisiblesMode) {
+                e.preventDefault();
+                inShowInvisiblesMode = false;
+
+                // destroy all qtips
+                $(".invisibles").each(function() {
+                    const $this: JQuery = $(this);
+                    $this.qtip("destroy");
+                });
+
+                // restore all the original characters
+                const editable = $(e.target).closest(".bloom-editable");
+                editable.html((i, html) => {
+                    return html.replace(
+                        /<span class="invisibles"[^<>]*data-original="(?:\\u(....)|(&[a-z,0-9]*;))"[^<>]*>.<\/span>/g,
+                        // p1 will match unicode char codes, p2 will html entities
+                        function(match, p1, p2) {
+                            return invisibleFromCode(p1, p2);
+                        }
+                    );
+                });
+            }
         });
 
     $(document).keydown(e => {
