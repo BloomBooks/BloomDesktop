a75d8f176f0b1a89956ea804daef05151b38332b
Hatton <hattonjohn@gmail.com>
2023-08-23 19:19:23 -0600

    Get inheritance working again with tests

--- src/BloomTests/Book/AppearanceSettingsTests.cs
+++ src/BloomTests/Book/AppearanceSettingsTests.cs
@@ -15,39 +15,56 @@ namespace BloomTests.Book
 		{
 			var appearance = new AppearanceSettings();
 			Assert.AreEqual("default", appearance.TestOnlyPropertiesAccess.cssThemeName);
-			Assert.AreEqual("yellow", appearance.TestOnlyPropertiesAccess.coverColor);
 			Assert.IsTrue(appearance.TestOnlyPropertiesAccess.coverShowTitleL2);
 			Assert.IsFalse(appearance.TestOnlyPropertiesAccess.coverShowTitleL3);
 		}
 
-		[Test]
-		public void GetCssRootDeclaration_HasCoverColorVariable()
-		{
-			var appearance = new AppearanceSettings();
-			Assert.IsTrue(appearance.GetCssRootDeclaration().IndexOf("--coverColor: yellow;") > -1);
-		}
 
 		[Test]
 		public void GetCssRootDeclaration_HasCorrectTitleFieldsVariableValues()
 		{
 			var appearance = new AppearanceSettings();
-			Assert.IsTrue(appearance.GetCssRootDeclaration().IndexOf("--coverShowTitleL2: ignore-this;") > -1);
+			Assert.IsTrue(appearance.GetCssRootDeclaration().IndexOf($"--coverShowTitleL2: {AppearanceSettings.kDoShowValueForDisplay};") > -1);
 			//appearance.Update(new { coverShowTitleL2 = false, foo = "blah" });
 			appearance.UpdateFromJson("{coverShowTitleL2:false}");
 			Assert.IsTrue(appearance.GetCssRootDeclaration().IndexOf("--coverShowTitleL2: none;") > -1);
 		}
 
 		[Test]
-		public void GetCssRootDeclaration_ParentOverridesByDefault()
+		public void GetCssRootDeclaration_ItemVisibility_ChildOverrides_UsesChildValue()
 		{
 			var collectionAppearance = new AppearanceSettings();
-			collectionAppearance.UpdateFromJson("{coverColor:\"theCollectionsColor\"}");
+			collectionAppearance.UpdateFromJson("{\"coverShowTitleL2\":false}");
 			var bookAppearance = new AppearanceSettings();
-			bookAppearance.UpdateFromJson("{coverColor:\"ourCustomBookColor\", overrides:[]}");
-			Assert.IsTrue(bookAppearance.GetCssRootDeclaration(collectionAppearance).IndexOf("--coverColor: theCollectionsColor;") > -1);
-			bookAppearance.UpdateFromJson("{coverColor:\"ourCustomBookColor\", overrides:[\"colors\" ]}");
-			var css = bookAppearance.GetCssRootDeclaration(collectionAppearance);
-			Assert.IsTrue(css.IndexOf("--coverColor: ourCustomBookColor;") > -1);
+			bookAppearance.UpdateFromJson("{\"groupsToOverrideFromParent\":[\"coverFields\"], \"coverShowTitleL2\":true}");
+			Assert.IsTrue(bookAppearance.GetCssRootDeclaration(collectionAppearance).IndexOf($"--coverShowTitleL2: {AppearanceSettings.kDoShowValueForDisplay};") > -1);
+
+			bookAppearance.UpdateFromJson("{\"groupsToOverrideFromParent\":[\"coverFields\"],\"coverShowTitleL2\":false}");
+
+			Assert.IsTrue(bookAppearance.GetCssRootDeclaration(collectionAppearance).IndexOf($"--coverShowTitleL2: {AppearanceSettings.kHideValueForDisplay};") > -1, bookAppearance.GetCssRootDeclaration(collectionAppearance).ToString());
 		}
+		[Test]
+		public void GetCssRootDeclaration_ItemVisibility_NoOverride_UsesParentValue()
+		{
+			var collectionAppearance = new AppearanceSettings();
+			
+			var bookAppearance = new AppearanceSettings();
+			bookAppearance.UpdateFromJson("{\"groupsToOverrideFromParent\":[], \"coverShowTitleL2\":true}");
+			collectionAppearance.UpdateFromJson("{\"coverShowTitleL2\":false}");
+			Assert.IsTrue(bookAppearance.GetCssRootDeclaration(collectionAppearance).IndexOf($"--coverShowTitleL2: {AppearanceSettings.kHideValueForDisplay};") > -1);
+
+			collectionAppearance.UpdateFromJson("{\"coverShowTitleL2\":true}");
+			bookAppearance.UpdateFromJson("{\"groupsToOverrideFromParent\":[\"some-randome-thing\"],\"coverShowTitleL2\":false}");
+			Assert.IsTrue(bookAppearance.GetCssRootDeclaration(collectionAppearance).IndexOf($"--coverShowTitleL2: {AppearanceSettings.kDoShowValueForDisplay};") > -1, bookAppearance.GetCssRootDeclaration(collectionAppearance).ToString());
+		}
+
+
+		/* [Test]
+		public void GetCssRootDeclaration_ItemVisibility_ChildOverridesButDoesnotHaveProperty_UsesParentValue()
+		{
+			// I'm leaving this empty test here just to remind us that actually if a child doesn't have a property at all, currently the code will not notice that the parent has
+			// such a property, and at will not be emitted at all. This may not be sufficient for our needs, but until that is clear, this case just isn't handled.
+		}
+		*/
 	}
 }
