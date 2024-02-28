e29f49149819e484e5b5d819d70fb8ee1ab2a2d5
Noel Chou <noel_chou@sil.org>
2024-02-28 12:45:34 -0500

    BL-13133 narrow OneDrive error codes to prevent false positives

--- src/BloomExe/Utils/OneDriveUtils.cs
+++ src/BloomExe/Utils/OneDriveUtils.cs
@@ -14,22 +14,18 @@ namespace Bloom.Utils
         {
             "80070184", // This one is not in Microsoft's list. I got it by disconnecting my internet.
             // The rest are from https://support.microsoft.com/en-au/office/what-do-the-onedrive-error-codes-mean-f7a68338-e540-4ebf-ad5d-56c5633acded#ID0EBBH=Error_codes
-            "80010007",
             "80040c81",
             "8004de80",
             "8004de86",
             "8004de85",
             "8004de8a",
             "8004de90",
-            "8004de96",
             "8004ded7",
             "8004dedc",
             "8004def0",
             "8004def7",
-            "80070005",
             "8007016a",
             "80070194",
-            "80071128",
             "80071129"
         };
 
