8da35deff29cc33643b1906e4138b4b2b0f286b8
Steve McConnel <stephen_mcconnel@sil.org>
2024-08-14 16:30:13 -0600

    Add a basic Fonts tab to the Book Settings dialog (BL-13763)

--- src/BloomBrowserUI/bookEdit/bookSettings/BookSettingsDialog.tsx
+++ src/BloomBrowserUI/bookEdit/bookSettings/BookSettingsDialog.tsx
@@ -43,11 +43,12 @@ import {
 } from "../../utils/bloomApi";
 import { ShowEditViewDialog } from "../editViewFrame";
 import { useL10n } from "../../react_components/l10nHooks";
-import { Div } from "../../react_components/l10nComponents";
+import { Div, P } from "../../react_components/l10nComponents";
 import { NoteBox, WarningBox } from "../../react_components/boxes";
 import { default as TrashIcon } from "@mui/icons-material/Delete";
 import { PWithLink } from "../../react_components/pWithLink";
 import { FieldVisibilityGroup } from "./FieldVisibilityGroup";
+import { StyleAndFontTable } from "./StyleAndFontTable";
 
 let isOpenAlready = false;
 
@@ -579,6 +580,24 @@ export const BookSettingsDialog: React.FunctionComponent<{
                                 label={resolutionLabel}
                                 path={`publish.bloomPUB.imageSettings`}
                             />
+                        </ConfigrGroup>
+                        <ConfigrGroup label="Fonts" level={1}>
+                            <NoteBox>
+                                <div>
+                                    <P l10nKey="BookSettings.Fonts.Problematic">
+                                        When you publish a book to the web or as
+                                        an ebook, Bloom will flag any
+                                        problematic fonts. For example, we
+                                        cannot legally host most Microsoft fonts
+                                        on BloomLibrary.org.
+                                    </P>
+                                    <P l10nKey="BookSettings.Fonts.TableDescription">
+                                        The following table shows where fonts
+                                        have been used.
+                                    </P>
+                                </div>
+                            </NoteBox>
+                            <StyleAndFontTable />
                         </ConfigrGroup>
                     </ConfigrPane>
                 )}
---
+++ src/BloomBrowserUI/bookEdit/bookSettings/StyleAndFontTable.tsx
@@ -0,0 +1,65 @@
+/** @jsx jsx **/
+import { jsx, css } from "@emotion/react";
+
+import React = require("react");
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { Span } from "../../react_components/l10nComponents";
+import { useApiObject } from "../../utils/bloomApi";
+
+// This interface must be kept in sync with the StyleAndFont class in BookSettingsApi.cs.
+export interface IStyleAndFont {
+    style: string;
+    styleName: string;
+    languageName: string;
+    languageTag: string;
+    fontName: string;
+    pageId: string;
+    pageDescription: string;
+}
+export const StyleAndFontTable: React.FunctionComponent<{}> = () => {
+    const rows: IStyleAndFont[] = useApiObject<IStyleAndFont[]>(
+        "book/settings/stylesAndFonts",
+        []
+    );
+
+    return (
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell sx={{ border: 1 }}>
+                        <Span l10nKey="EditTab.FormatDialog.Style">Style</Span>
+                    </TableCell>
+                    <TableCell sx={{ border: 1 }}>
+                        <Span l10nKey="EditTab.TextBoxProperties.LanguageTab">
+                            Language
+                        </Span>
+                    </TableCell>
+                    <TableCell sx={{ border: 1 }}>
+                        <Span l10nKey="EditTab.FormatDialog.Font">Font</Span>
+                    </TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {rows.map(row => (
+                    <TableRow key={row.style + row.languageName}>
+                        <TableCell
+                            component="th"
+                            scope="row"
+                            sx={{ border: 1 }}
+                        >
+                            {row.styleName}
+                        </TableCell>
+                        <TableCell sx={{ border: 1 }}>
+                            {row.languageName}
+                        </TableCell>
+                        <TableCell sx={{ border: 1 }}>{row.fontName}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    );
+};
