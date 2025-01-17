ca664a55f5e2a0e7ee610a5f0cc0dfd0b16470b6
John Thomson <john_thomson@sil.org>
2025-01-17 16:01:50 -0600

    Make image descriptions work again
    Overlay on the image can now be shown in the shrunk image. Shrink is always 50% even if larger would fit. Commands that would change the image container size are disabled or hidden while in this mode.

--- src/BloomBrowserUI/bookEdit/toolbox/imageDescription/imageDescriptionUtils.ts
+++ src/BloomBrowserUI/bookEdit/toolbox/imageDescription/imageDescriptionUtils.ts
@@ -6,13 +6,43 @@ import { OverlayTool } from "../overlay/overlayTool";
 export function showImageDescriptions(bodyOfPageIframe: HTMLElement) {
     const bubbleManager = OverlayTool.bubbleManager();
     bubbleManager?.suspendComicEditing("forTool");
-    // turn on special layout to make image descriptions visible (might already be on)
-    bodyOfPageIframe.classList.add("bloom-showImageDescriptions");
+    // turn on special layout to make image descriptions visible (might already be on, so check first)
+    if (!bodyOfPageIframe.classList.contains("bloom-showImageDescriptions")) {
+        bodyOfPageIframe.classList.add("bloom-showImageDescriptions");
+        // for each bloom-imageContainer that is not a child of another bloom-imageContainer,
+        // wrap the contents (except the description) with another division with class bloom-describedImage
+        // See comment in editMode.less under bloom-describedImage for why we do this.
+        for (const imageContainer of Array.from(
+            bodyOfPageIframe.getElementsByClassName("bloom-imageContainer")
+        )) {
+            if (
+                !imageContainer.parentElement?.closest(".bloom-imageContainer")
+            ) {
+                const describedImage = document.createElement("div");
+                describedImage.classList.add("bloom-describedImage");
+                for (const child of Array.from(imageContainer.children)) {
+                    if (!child.classList.contains("bloom-imageDescription")) {
+                        describedImage.appendChild(child);
+                    }
+                }
+                imageContainer.appendChild(describedImage);
+            }
+        }
+    }
 }
 export function hideImageDescriptions(bodyOfPageIframe: HTMLElement) {
     const bubbleManager = OverlayTool.bubbleManager();
-    // removing the class must be done first; resume won't work right while the overlays
-    // are hidden
+    // removing the class and wrapper should be done first; resume may not work
+    // right while the extra wrapper is present.
     bodyOfPageIframe.classList.remove("bloom-showImageDescriptions");
+    // unwrap the contents of each bloom-describedImage
+    for (const describedImage of Array.from(
+        bodyOfPageIframe.getElementsByClassName("bloom-describedImage")
+    )) {
+        for (const child of Array.from(describedImage.children)) {
+            describedImage.parentElement!.appendChild(child);
+        }
+        describedImage.remove();
+    }
     bubbleManager?.resumeComicEditing();
 }
