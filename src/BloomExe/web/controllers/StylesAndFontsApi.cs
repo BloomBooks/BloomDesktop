8a034a1fcc4d385cd103111d037c3aa1fe4f49b4
Andrew Polk <andrew_polk@sil.org>
2025-02-24 17:08:00 -0700

    Ensure default font is shown in Book Settings -> Fonts (BL-14359)

--- src/BloomExe/web/controllers/StylesAndFontsApi.cs
+++ src/BloomExe/web/controllers/StylesAndFontsApi.cs
@@ -1,6 +1,7 @@
-﻿using Bloom.Api;
+using Bloom.Api;
 using Bloom.Book;
 using Bloom.SafeXml;
+using L10NSharp;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -38,6 +39,12 @@ namespace Bloom.web.controllers
             public string fontName;
             public string pageId;
             public string pageDescription;
+
+            // just helpful for debugging at this point
+            public override string ToString()
+            {
+                return $"{style}, {fontName}";
+            }
         }
 
         public struct PageInfo
@@ -144,6 +151,45 @@ namespace Bloom.web.controllers
                         return namesCompared;
                 }
             );
+
+            // Ensure every collection-level default font is included in the list
+            var collectionLevelFonts = new HashSet<(string fontName, string languageTag)>
+            {
+                (book.CollectionSettings.Language1.FontName, book.CollectionSettings.Language1Tag),
+                (book.CollectionSettings.Language2.FontName, book.CollectionSettings.Language2Tag)
+            };
+            if (!String.IsNullOrEmpty(book.CollectionSettings.Language3Tag))
+            {
+                collectionLevelFonts.Add(
+                    (
+                        book.CollectionSettings.Language3.FontName,
+                        book.CollectionSettings.Language3Tag
+                    )
+                );
+            }
+            foreach (var font in collectionLevelFonts.Reverse()) // reverse because we're inserting at the beginning each time
+            {
+                if (!stylesAndFonts.Exists(s => s.fontName == font.fontName))
+                {
+                    var languageName = GetLanguageName(font.languageTag);
+                    stylesAndFonts.Insert(
+                        0,
+                        new StyleAndFont
+                        {
+                            styleName = string.Format(
+                                LocalizationManager.GetString(
+                                    "CollectionSettingsDialog.BookMakingTab.DefaultFontFor",
+                                    "Default Font for {0}",
+                                    "{0} is a language name."
+                                ),
+                                languageName
+                            ),
+                            languageName = languageName,
+                            fontName = font.fontName,
+                        }
+                    );
+                }
+            }
             var jsonData = JsonConvert.SerializeObject(stylesAndFonts.ToArray());
             request.ReplyWithJson(jsonData);
         }
