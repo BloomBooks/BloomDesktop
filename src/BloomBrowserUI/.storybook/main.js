a2a4bb94bb33af747a366b5b9d5d3dea4ee70598
John Thomson <john_thomson@sil.org>
2025-02-28 14:55:14 -0600

    ran npx migration, did a bit of cleanup

--- src/BloomBrowserUI/.storybook/main.js
+++ src/BloomBrowserUI/.storybook/main.js
@@ -1,14 +1,30 @@
 module.exports = {
     stories: ["../**/stories.tsx", "../**/*.stories.tsx"],
+
     addons: [
         "@storybook/addon-docs",
         "@storybook/addon-controls",
-        "@storybook/addon-a11y"
+        "@storybook/addon-a11y",
+        "@storybook/addon-webpack5-compiler-swc",
+        "@chromatic-com/storybook"
     ],
-    core: {
-        builder: "webpack5"
-    },
+
     features: {
         emotionAlias: false
-    }
+    },
+
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {}
+    },
+
+    docs: {
+        autodocs: true
+    },
+    staticDirs: [
+        "../../../output/browser",
+        ".",
+        "../react_components",
+        "../teamCollection"
+    ]
 };
