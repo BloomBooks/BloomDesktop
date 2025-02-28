2ac769ba0ba6cb7c0b3daaae3f24f8dd190aca5f
Andrew Polk <andrew_polk@sil.org>
2025-02-28 16:03:47 -0700

    Revert "Remove some stories temporarily"
    This reverts commit 942c8e8608c383bc3fed26aff80ad24ef4871893.

---
+++ src/BloomBrowserUI/react_components/BloomCheckBox.stories.tsx
@@ -0,0 +1,170 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+// Don't add /** @jsxFrag React.Fragment */ or these stories won't show up in StoryBook! (at least in Aug 2022)
+/** @jsx jsx **/
+import { jsx, css } from "@emotion/react";
+
+import * as React from "react";
+import { BloomCheckbox } from "./BloomCheckBox";
+import { useState } from "react";
+import { ApiCheckbox } from "./ApiCheckbox";
+
+import {
+    FormControlLabel,
+    Checkbox as OriginalMuiCheckbox
+} from "@mui/material";
+import { VisuallyImpairedIcon } from "./icons/VisuallyImpairedIcon";
+import { MotionIcon } from "./icons/MotionIcon";
+
+const kLongText =
+    "Bacon ipsum dolor amet ribeye spare ribs bresaola t-bone. Strip steak turkey shankle pig ground round, biltong t-bone kevin alcatra flank ribeye beef ribs meatloaf filet mignon. Buffalo ham t-bone short ribs.";
+
+export default {
+    title: "BloomCheckbox"
+};
+
+export const Various = () =>
+    React.createElement(() => {
+        const [checked, setChecked] = useState<boolean | undefined>(false);
+        return (
+            <React.Fragment>
+                <strong>
+                    The original purpose of this story was to compare the
+                    vertical alignment of the text to the checkbox.
+                </strong>
+                <hr />
+                original mui checkbox:
+                <div>
+                    <FormControlLabel
+                        control={<OriginalMuiCheckbox />}
+                        label={"short"}
+                    />
+                    <FormControlLabel
+                        control={<OriginalMuiCheckbox />}
+                        label={kLongText}
+                    />
+                </div>
+                <hr />
+                BloomCheckbox, which allows multi-line labels and icons:
+                <div>
+                    <BloomCheckbox
+                        label="short"
+                        checked={true}
+                        onCheckChanged={() => {}}
+                        l10nKey="bogus"
+                    />
+                    <BloomCheckbox
+                        label={kLongText}
+                        checked={true}
+                        onCheckChanged={() => {}}
+                        l10nKey="bogus"
+                    />
+                    <hr />
+                    <BloomCheckbox
+                        label={"With Icon " + kLongText}
+                        icon={<VisuallyImpairedIcon />}
+                        checked={checked}
+                        tristate={true}
+                        onCheckChanged={newState => setChecked(newState)}
+                        l10nKey="bogus"
+                    />
+                    <div>
+                        <BloomCheckbox
+                            label={"hideBox, checked"}
+                            checked={true}
+                            hideBox={true}
+                            onCheckChanged={() => {}}
+                            l10nKey="bogus"
+                        />
+                    </div>
+                    <div>
+                        <BloomCheckbox
+                            label={"hideBox, not checked, no icon"}
+                            checked={false}
+                            hideBox={true}
+                            onCheckChanged={() => {}}
+                            l10nKey="bogus"
+                        />
+                    </div>
+                    <div>
+                        <BloomCheckbox
+                            label={"hideBox, not checked, with icon"}
+                            icon={<MotionIcon />}
+                            checked={false}
+                            hideBox={true}
+                            onCheckChanged={() => {}}
+                            l10nKey="bogus"
+                        />
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    });
+
+export const Off = () =>
+    React.createElement(() => {
+        const [checked, setChecked] = useState<boolean | undefined>(false);
+        return (
+            <BloomCheckbox
+                label=" Click me"
+                checked={checked}
+                onCheckChanged={newState => setChecked(newState)}
+                l10nKey="bogus"
+            />
+        );
+    });
+
+Off.story = {
+    name: "off"
+};
+
+export const On = () =>
+    React.createElement(() => {
+        const [checked, setChecked] = useState<boolean | undefined>(true);
+        return (
+            <BloomCheckbox
+                label=" Click me"
+                checked={checked}
+                onCheckChanged={newState => setChecked(newState)}
+                l10nKey="bogus"
+            />
+        );
+    });
+
+On.story = {
+    name: "on"
+};
+
+export const Tristate = () =>
+    React.createElement(() => {
+        const [checked, setChecked] = useState<boolean | undefined>(undefined);
+        return (
+            <BloomCheckbox
+                label=" Click me"
+                checked={checked}
+                tristate={true}
+                onCheckChanged={newState => setChecked(newState)}
+                l10nKey="bogus"
+            />
+        );
+    });
+
+Tristate.story = {
+    name: "tristate"
+};
+
+export default {
+    title: "BloomCheckbox/ApiCheckbox"
+};
+
+export const _ApiCheckbox = () =>
+    React.createElement(() => (
+        <ApiCheckbox
+            label="Motion Book"
+            l10nKey="PublishTab.Android.MotionBookMode"
+            apiEndpoint="publish/bloompub/motionBookMode"
+        />
+    ));
+
+_ApiCheckbox.story = {
+    name: "ApiCheckbox"
+};
