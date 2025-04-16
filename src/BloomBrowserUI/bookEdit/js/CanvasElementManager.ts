ad5240723b8369b4edb00d1e23161504a7fde15c
Andrew Polk <andrew_polk@sil.org>
2025-04-16 15:44:48 -0700

    Better approach to applying theme colors to draggable svg (BL-14595)

--- src/BloomBrowserUI/bookEdit/js/CanvasElementManager.ts
+++ src/BloomBrowserUI/bookEdit/js/CanvasElementManager.ts
@@ -4541,7 +4541,9 @@ export class CanvasElementManager {
             kImageContainerClass + " bloom-leadingElement";
         // This svg is basically the same as the one in AudioIcon.tsx.
         // Likely, changes to one should be mirrored in the other.
-        const html = `<div tabindex='0' class='bloom-unmodifiable-image bloom-svg bloom-svg-rect-fill-has-background-color bloom-svg-path-stroke-has-foreground-color ${standardImageClasses}'>
+        //
+        // The data-icon-type is so we can, in the future, find these and migrate/update them.
+        const html = `<div tabindex='0' class='bloom-unmodifiable-image bloom-svg ${standardImageClasses}' data-icon-type='audio'>
     <svg
         viewBox="0 0 31 31"
         fill="none"
@@ -4552,11 +4554,11 @@ export class CanvasElementManager {
             width="31"
             height="31"
             rx="1.81232"
-            fill="black"
+            fill="var(--draggable-background-color, black)"
         />
         <path
             d="M23.0403 9.12744C24.8868 10.8177 25.9241 13.11 25.9241 15.5C25.9241 17.8901 24.8868 20.1823 23.0403 21.8726M19.5634 12.3092C20.4867 13.1544 21.0053 14.3005 21.0053 15.4955C21.0053 16.6906 20.4867 17.8367 19.5634 18.6818M15.0917 9.19054L10.1669 12.796H6.22705V18.2041H10.1669L15.0917 21.8095V9.19054Z"
-            stroke="white"
+            stroke="var(--draggable-color, white)"
             strokeWidth="1.15865"
             strokeLinecap="round"
             strokeLinejoin="round"
