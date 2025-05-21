5af3cb5a92934fa9b581147b14aa329b376aca74
Noel Chou <noel_chou@sil.org>
2025-05-21 09:36:00 -0400

    BL-14768 fix bottom outline of image getting cut off

--- src/BloomBrowserUI/utils/elementUtils.ts
+++ src/BloomBrowserUI/utils/elementUtils.ts
@@ -1,3 +1,5 @@
+import { EditableDivUtils } from "../bookEdit/js/editableDivUtils";
+
 /**
  * Like element.closest(), but instead finds the farthest element (starting from ${startElement}) that matches the selector
  * (self included)
@@ -23,4 +25,49 @@ export function farthest<E extends Element = Element>(
     }
 
     return patriarch;
+}
+
+export function getBorderThickness(element) {
+    const styles = window.getComputedStyle(element);
+    const borderTopWidth = parseFloat(
+        styles.getPropertyValue("border-top-width")
+    );
+    const borderBottomWidth = parseFloat(
+        styles.getPropertyValue("border-bottom-width")
+    );
+    const borderLeftWidth = parseFloat(
+        styles.getPropertyValue("border-left-width")
+    );
+    const borderRightWidth = parseFloat(
+        styles.getPropertyValue("border-right-width")
+    );
+    return {
+        top: borderTopWidth,
+        bottom: borderBottomWidth,
+        left: borderLeftWidth,
+        right: borderRightWidth
+    };
+}
+
+// .clientHeight and .clientWidth return integers, so if rounding errors are a problem we can
+// use this to get the exact fractional values. Like clientHeight and clientWidth, this include
+// the padding, but not the border, margin. However, unlike clientHeight and clientWidth,
+// this size will include the scrollbar since we use getBoundingClientRect().
+// This may also behave differently from .clientHeight and .clientWidth when used on the root element
+export function getExactClientSize(
+    element: HTMLElement
+): { width: number; height: number } {
+    const scalingFactor = EditableDivUtils.getPageScale();
+    const boundingRect = element.getBoundingClientRect();
+    const borderThicknesses = getBorderThickness(element);
+    const exactClientHeight =
+        boundingRect.height / scalingFactor -
+        (borderThicknesses.top + borderThicknesses.bottom);
+    const exactClientWidth =
+        boundingRect.width / scalingFactor -
+        (borderThicknesses.left + borderThicknesses.right);
+    return {
+        width: exactClientWidth,
+        height: exactClientHeight
+    };
 }
