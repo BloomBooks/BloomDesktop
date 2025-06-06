d9619c8674728f90dfa9fb2cc16fa5d5f1cff2eb
Noel Chou <noel_chou@sil.org>
2025-06-06 15:57:22 -0400

    BL-14827 randomly shuffle draggables

--- src/BloomBrowserUI/bookEdit/toolbox/games/GameTool.tsx
+++ src/BloomBrowserUI/bookEdit/toolbox/games/GameTool.tsx
@@ -30,6 +30,7 @@ import {
     getTarget,
     playInitialElements,
     prepareActivity,
+    shuffle,
     undoPrepareActivity
 } from "bloom-player";
 import theOneLocalizationManager from "../../../lib/localizationManager/localizationManager";
@@ -1718,6 +1719,8 @@ export class GameTool extends ToolboxToolReactAdaptor {
 
     public newPageReady() {
         const page = GameTool.getBloomPage();
+        randomlyAssignTargetsIfNeeded(page);
+
         const pageFrameExports = getEditablePageBundleExports();
         if (!getCanvasElementManager() || !page || !pageFrameExports) {
             // probably the toolbox just finished loading before the page.
@@ -1977,3 +1980,24 @@ export const makeTargetForDraggable = (
     adjustTarget(canvasElement, target);
     return target;
 };
+
+function randomlyAssignTargetsIfNeeded(page): void {
+    if (page.classList.contains("draggables-need-shuffling")) {
+        const draggables = Array.from(
+            page.querySelectorAll("[data-draggable-id]")
+        ) as HTMLElement[];
+        shuffle(draggables, (_a, _b) => false);
+        const targets = Array.from(
+            page.querySelectorAll("[data-target-of]")
+        ) as HTMLElement[];
+        for (let i = 0; i < targets.length; i++) {
+            const target = targets[i];
+            target.setAttribute(
+                "data-target-of",
+                draggables[i].getAttribute("data-draggable-id") ?? ""
+            );
+        }
+
+        page.classList.remove("draggables-need-shuffling");
+    }
+}
