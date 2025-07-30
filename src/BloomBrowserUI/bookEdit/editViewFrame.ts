9e4c9bfe5ba93aa684bb17b2b27f3759e06eed4a
John Thomson <john_thomson@sil.org>
2025-07-30 15:48:01 -0500

    Restore function to run About dialog

--- src/BloomBrowserUI/bookEdit/editViewFrame.ts
+++ src/BloomBrowserUI/bookEdit/editViewFrame.ts
@@ -251,6 +251,10 @@ export function showEditViewBookSettingsDialog(
     showBookSettingsDialog(initiallySelectedGroupIndex);
 }
 
+export function showAboutDialogInEditTab() {
+    showAboutDialog();
+}
+
 export function showRequiresSubscriptionDialog(featureName: string): void {
     showRequiresSubscriptionDialogInEditView(featureName);
 }
