086b4a265e8e2b3ba8c1b9e766ff825e497389c9
Hatton <hattonjohn@gmail.com>
2025-09-16 17:47:24 -0600

    wip (longpress, markdown)

--- src/BloomBrowserUI/react_components/markdown.tsx
+++ src/BloomBrowserUI/react_components/markdown.tsx
@@ -5,13 +5,24 @@ import {
     ILocalizationState,
     LocalizableElement,
 } from "./l10nComponents";
-import * as MarkdownIt from "markdown-it";
-import * as MarkdownItAttrs from "markdown-it-attrs";
+import * as MarkdownItNS from "markdown-it";
+import * as MarkdownItAttrsNS from "markdown-it-attrs";
+// Vite provides ESM default at runtime; TS types need namespace import without esModuleInterop.
+// Cast to any to access the runtime default constructor and plugin.
+// Vite runs ESM, while our typings are CommonJS-style. Safely access default.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const MarkdownIt =
+    ((MarkdownItNS as unknown) as { default: any }).default ||
+    ((MarkdownItNS as unknown) as unknown);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const markdownItAttrs =
+    ((MarkdownItAttrsNS as unknown) as { default: any }).default ||
+    ((MarkdownItAttrsNS as unknown) as unknown);
 
 // This component expects its content to be a single string (like all localizable elements) that
 // contains Markdown. It will convert that into HTML and show it.
 const markd = new MarkdownIt();
-markd.use(MarkdownItAttrs, { allowedAttributes: ["id", "class"] });
+markd.use(markdownItAttrs, { allowedAttributes: ["id", "class"] });
 
 export class Markdown extends LocalizableElement<
     ILocalizationProps,
