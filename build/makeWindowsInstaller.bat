0ccf926c716cea4bfddcc8200ecb759547843637
Hatton <hattonjohn@gmail.com>
2025-10-02 08:23:18 -0600

    don't assume cwd

--- build/makeWindowsInstaller.bat
+++ build/makeWindowsInstaller.bat
@@ -7,10 +7,15 @@ REM To test the update process, patch ApplicationUpdateSupport.GetUpdateUrl to t
 REM If there are existing releases in the output directory, you must make sure the BUILD_NUMBER is adjusted to something larger than any
 REM of them. The major and minor numbers here are ignored.
 
-pushd .
+REM Ensure we run from the directory of this script, so msbuild finds Bloom.proj
+pushd "%~dp0"
 
 REM Build the installer (Installer depends on Build and EnsureVelopackCli)
 REM Note: BUILD_NUMBER must be 4 parts (a.b.c.d). Only the 3rd part (BuildCounter) matters for Version; the others can be 0.
-dotnet msbuild /p:channel=LocalBuilt /p:InstallerOutputFolder=../output/installer/result /p:BUILD_NUMBER=0.0.9999.0 /t:Installer /verbosity:detailed
+REM Clean the output folder to avoid vpk complaining about equal/greater existing releases
+set OUTPUT_DIR=..\output\installer\result
+if exist "%OUTPUT_DIR%" rmdir /s /q "%OUTPUT_DIR%"
+mkdir "%OUTPUT_DIR%" 2>nul
+dotnet msbuild "Bloom.proj" /p:channel=LocalBuilt /p:InstallerOutputFolder="%OUTPUT_DIR%" /p:BUILD_NUMBER=0.0.9999.0 /t:Installer /verbosity:detailed
 popd
 
