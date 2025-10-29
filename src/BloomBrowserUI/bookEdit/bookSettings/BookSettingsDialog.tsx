8ea4b3c23cb552f3824cf001f8fc30123c7938d9
Steve McConnel <stephen_mcconnel@sil.org>
2025-10-29 10:56:32 -0600

    Add "Show Credits" to book settings dialog / cover page (BL-15356)

--- src/BloomBrowserUI/bookEdit/bookSettings/BookSettingsDialog.tsx
+++ src/BloomBrowserUI/bookEdit/bookSettings/BookSettingsDialog.tsx
@@ -166,6 +166,10 @@ export const BookSettingsDialog: React.FunctionComponent<{
         "BookSettings.ShowLanguageName"
     );
     const showTopicLabel = useL10n("Show Topic", "BookSettings.ShowTopic");
+    const showCreditsLabel = useL10n(
+        "Show Credits",
+        "BookSettings.ShowCredits"
+    );
     const frontAndBackMatterLabel = useL10n(
         "Front & Back Matter",
         "BookSettings.FrontAndBackMatter"
@@ -473,6 +477,13 @@ export const BookSettingsDialog: React.FunctionComponent<{
                                     disabled={appearanceDisabled}
                                     {...getAdditionalProps<boolean>(
                                         `cover-topic-show`
+                                    )}
+                                />
+                                <ConfigrBoolean
+                                    label={showCreditsLabel}
+                                    disabled={appearanceDisabled}
+                                    {...getAdditionalProps<boolean>(
+                                        `cover-creditsRow-show`
                                     )}
                                 />
                             </ConfigrSubgroup>
