cb97b219ea29a657fec83c67ea25990287f22009
Hatton <hattonjohn@gmail.com>
2026-01-30 11:01:07 -0700

    Fix BL-15558 preserve bloompub font simulation
    Also, give the user a chance to read notes like ones about font embedding.

--- src/BloomBrowserUI/publish/commonPublish/PublishProgressDialog.tsx
+++ src/BloomBrowserUI/publish/commonPublish/PublishProgressDialog.tsx
@@ -33,6 +33,8 @@ export const PublishProgressDialog: React.FunctionComponent<{
     const [accumulatedMessages, setAccumulatedMessages] = useState("");
 
     const [errorEncountered, setErrorEncountered] = useState(false);
+    const [interestingMessageEncountered, setInterestingMessageEncountered] =
+        useState(false);
 
     const {
         setProgressState: setProgressStateProp,
@@ -47,6 +49,7 @@ export const PublishProgressDialog: React.FunctionComponent<{
         setAccumulatedMessages("");
         setInstructionMessage(undefined);
         setErrorEncountered(false);
+        setInterestingMessageEncountered(false);
     }, [setProgressStateProp, setClosePendingProp]);
 
     //Note, originally this was just a function, closeIfNoError().
@@ -55,9 +58,9 @@ export const PublishProgressDialog: React.FunctionComponent<{
     // update we notice that and see about closing.
     React.useEffect(() => {
         if (props.closePending) {
-            if (errorEncountered) {
+            if (errorEncountered || interestingMessageEncountered) {
                 setProgressStateProp(() =>
-                    errorEncountered
+                    errorEncountered || interestingMessageEncountered
                         ? ProgressState.Done
                         : ProgressState.Closed,
                 );
@@ -75,11 +78,15 @@ export const PublishProgressDialog: React.FunctionComponent<{
         }
     }, [
         props.closePending,
+        errorEncountered,
         setProgressStateProp,
         setClosePendingProp,
         closeAndResetDialog,
+        interestingMessageEncountered,
     ]);
 
+    // Kick off the server-side task only after the websocket is ready,
+    // so we don't miss early progress messages.
     React.useEffect(() => {
         props.setProgressState(ProgressState.Working);
         // we need to be ready to listen to progress messages from the server,
@@ -121,11 +128,15 @@ export const PublishProgressDialog: React.FunctionComponent<{
                     case "Error":
                     case "Warning":
                         setErrorEncountered(true);
+                        setInterestingMessageEncountered(true);
                     // deliberately fall through
                     case "Progress":
 
                     // eslint-disable-next-line no-fallthrough
                     case "Note":
+                        if (progressEvent.progressKind === "Note") {
+                            setInterestingMessageEncountered(true);
+                        }
                         setAccumulatedMessages(
                             (oldMessages) => oldMessages + html,
                         );
